5790bdef52b63e72ba2aa2bb7b1b313f91fdb4e8
AdelinoJnr <adelinojunior96@gmail.com>
2021-08-30 20:12:20 -0300

    [feat: requirement 2]

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const ERROR_404 = {
+  message: 'Pessoa palestrante não encontrada',
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -20,10 +23,21 @@ app.listen(PORT, () => {
 // Requisito 1
 app.get('/talker', async (_req, res) => {
   try {
-    const request = await fs.readFile('./talker.json', 'utf-8');
-    const talkers = JSON.parse(request);
-    res.status(200).json(talkers);
+    const request = await fs.readFile('./talker.json', 'utf-8').then((r) => JSON.parse(r));
+    // const talkers = JSON.parse(request);
+    res.status(200).json(request);
   } catch (_err) {
     res.status(200).json([]);
   }
+});
+
+// Requisito 2
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const request = await fs.readFile('./talker.json', 'utf-8').then((r) => JSON.parse(r));
+  const findTalker = request.find((e) => e.id === Number(id));
+
+  if (!findTalker) return res.status(404).json(ERROR_404);
+
+  res.status(200).json(findTalker);
 });
