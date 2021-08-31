034b543abdd68099acc28b6c2298db652f4ab686
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-08-31 17:20:41 -0300

    feat: created endpoint GET /talker/:id

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.get('/talker', async (req, res, next) => {
   }
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const content = await fs.readFile('./talker.json');
+  const talkers = JSON.parse(content);
+  const { id } = req.params;
+  const talker = talkers.find((t) => t.id === parseInt(id.substring(id.length - 1), 10));
+  // https://stackoverflow.com/questions/7818903/jslint-says-missing-radix-parameter
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(200).json(talker);
+});
+
 app.use((err, _req, res, _next) => {
   console.log('Passou pelo middleware');
   res.status(400).json({ message: err });
