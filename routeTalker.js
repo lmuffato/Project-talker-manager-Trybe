fde51b92c888c241a37e0e1089b5cd045b60bde5
Hernanne Domiciano <hernannedomiciano@gmail.com>
2021-08-31 18:23:31 -0300

    feat: req2, fix: talker.json

--- routeTalker.js
+++ routeTalker.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const fs = require('fs').promises;
 
-const router = express.Router();
+const router = express.Router({
+  mergeParams: true,
+});
 
 const peopleRegister = () => fs.readFile('./talker.json', 'utf-8').then((data) => JSON.parse(data));
 
@@ -9,6 +11,14 @@ router.get('/', async (_req, res) => {
   const talkers = await peopleRegister();
   if (!talkers) return res.status(200).json([]);
   res.status(200).json(talkers);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await peopleRegister();
+  const filterId = talkers.find((r) => r.id === parseInt(id, 10));
+  if (!filterId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(200).json(filterId);
 });
 
 module.exports = router;
