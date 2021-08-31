d7a96d26db2f09694d7b7bfd684233411a7b953b
Nathalia Zebral <natzebral@gmail.com>
2021-08-31 18:15:10 -0300

    iniciando o projeto

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const talker = require('./talker');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,7 +9,8 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funcionar
+
+// não remova esse endpoint, eh para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
