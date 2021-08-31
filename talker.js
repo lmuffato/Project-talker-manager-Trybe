33011f8c266801d65c428583b52cddbdf824f243
Viegas <mvrdgs@gmail.com>
2021-08-31 15:53:14 -0300

    created talker/:id get route

--- talker.js
+++ talker.js
@@ -3,13 +3,35 @@ const { readFileSync } = require('fs');
 
 const router = express.Router();
 
+const readTalkerFile = () => {
+  const talkersList = readFileSync('./talker.json');
+  const data = JSON.parse(talkersList);
+
+  return data;
+};
+
 router.get('/', (_req, res) => {
   try {
-    const talkersList = readFileSync('./talker.json');
-    const data = JSON.parse(talkersList);
-    res.status(200).json(data);
+    const talkers = readTalkerFile();
+    return res.status(200).json(talkers);
   } catch (err) {
-    res.status(500).send(err.message);
+    return res.status(500).send(err.message);
+  }
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const talkers = readTalkerFile();
+    
+    const talkerById = talkers.find((talker) => talker.id === Number(id));
+    
+    if (!talkerById) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  
+    return res.status(200).json(talkerById);
+  } catch (err) {
+    return res.status(500).send(err.message);
   }
 });
 
