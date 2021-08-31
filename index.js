0e94dfb081273bb0da0664c5bba6e993755e96c1
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-08-31 16:16:13 -0300

    requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+
+const read = async () => {
+const data = await fs.readFile('./talker.json', 'utf8').catch(() => []);
+return JSON.parse(data);
+};  
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,6 +18,13 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.listen(PORT, () => {
+app.get('/talker', async (_req, res) => {
+  const data = await read();
+  if (data.length === 0) return res.status(HTTP_OK_STATUS).json([]);
+return res.status(HTTP_OK_STATUS).json(data);
+});
+
+app.listen(PORT, async () => {
+  // console.log(await read());
   console.log('Online');
 });
