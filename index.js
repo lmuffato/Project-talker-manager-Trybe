c28be3d9e74cfd98ad104629fa682efad2ecae67
RenzoSev <sevilharenzo@gmail.com>
2021-08-31 10:12:14 -0300

    feat: index.js

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const talker = require('./routes/talker');
+const login = require('./routes/login');
+
+const { PORT, https } = require('./utils/infos');
+
+const { HTTP_OK_STATUS } = https;
+
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK_STATUS = 200;
-const PORT = '3000';
-
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.use('/talker', talker);
+
+app.use('/login', login);
 
 app.listen(PORT, () => {
   console.log('Online');
