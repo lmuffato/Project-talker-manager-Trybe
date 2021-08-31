4372bfb565ce0a1265a00f483a9895cd0b4ab645
Stanrley Barbosa <3011stan@gmail.com>
2021-08-31 19:34:51 -0300

    Req 1 and 2 done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
@@ -22,7 +23,48 @@ app.get('/talker', async (_req, res) => {
   if (!talker) return res.status(HTTP_OK_STATUS).send([]);
   res.status(HTTP_OK_STATUS).send(talker);
 });
-// readFileCustom();
+// req 2
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkerArr = await readFileCustom();
+  const talkerIndex = talkerArr.findIndex((t) => t.id === +id);
+  if (talkerIndex === -1) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' }); 
+  }
+  res.status(HTTP_OK_STATUS).json(talkerArr[talkerIndex]);
+});
+// req 3
+const validateEmail = (email) => {
+  const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+  return (emailPattern.test(email));
+};
+
+const validateEmailMiddleware = (req, res, next) => {
+  const { email } = req.body;
+  if (!email || email === '') { // tratando email vazio ou inexistente
+    return res.status(400).send({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!validateEmail(email)) { // tratando e-mail inválido
+    return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+};
+
+const validatePasswordMiddleware = (req, res, next) => {
+  const { password } = req.body;
+  if (!password || password === '') {
+    return res.status(400).send({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
+app.post('/login', validateEmailMiddleware, validatePasswordMiddleware, (req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  res.status(200).send({ token });
+});
 // FINAL MINHAS IMPLEMENTAÇÕES
 
 // não remova esse endpoint, e para o avaliador funcionar
