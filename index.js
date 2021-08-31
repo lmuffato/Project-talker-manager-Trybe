7cc706a1a7e10720a269136076d879efbd5d8793
Jodiel Briesemeister <jodielll29@gmail.com>
2021-08-31 03:25:17 -0300

    feat: req04

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const generateToken = require('./utils/helpers');
-const { isValidEmail, isValidPassword } = require('./middlewares/validations');
+const { 
+  isValidEmail, 
+  isValidPassword,
+
+  isValidToken,
+  isValidName,
+  isValidAge,
+
+  isValidTalk,
+  isValidDate,
+  isValidRate,
+} = require('./middlewares/validations');
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,22 +30,51 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 
-app.get('/talker', async (_req, res) => {
+const readTalker = async () => {
   const talker = await fs.readFile('./talker.json', 'utf-8');
   const result = await JSON.parse(talker);
-  res.status(200).json(result);
+  return result;
+};
+
+app.get('/talker', async (_req, res) => {
+  const talker = await readTalker();
+  res.status(200).json(talker);
 });
 
 app.get('/talker/:id', async (req, res) => {
-  const { id } = req.params;
-  const talker = await fs.readFile('./talker.json', 'utf-8');
-  const result = await JSON.parse(talker);
-  const speaker = result.find((obj) => obj.id === parseInt(id, 0));
-  if (!speaker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-
-  res.status(200).json(speaker);
+    const { id } = req.params;
+    const talker = await readTalker();
+    const speaker = talker.find((obj) => obj.id === parseInt(id, 0));
+    if (!speaker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    res.status(200).json(speaker);
 });
 
 app.post('/login', isValidEmail, isValidPassword, (_req, res) => {
   res.status(200).json({ token: generateToken(16) });
 });
+
+app.post(
+  '/talker',
+  isValidToken,
+  isValidName,
+  isValidAge,
+
+  isValidTalk,
+  isValidDate,
+  isValidRate,
+  async (req, res) => {
+      const { name, age, talk } = req.body;
+      const talker = await readTalker();
+      const id = talker.length + 1;
+      const { watchedAt, rate } = talk;
+      const obj = {
+        name, 
+        age,
+        id,
+        talk: { watchedAt, rate },
+      };
+      talker.push(obj);
+      await fs.writeFile('talker.json', JSON.stringify(talker));
+      res.status(201).json(obj);
+  },
+);
--- middlewares/validations.js
+++ middlewares/validations.js
@@ -26,7 +26,104 @@ const isValidPassword = (req, res, next) => {
   next();
 };
 
+const isValidToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  const tokenRegex = /^[a-zA-Z0-9]{16}$/;
+
+  if (!token || token === '') {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+
+  if (!(token.match(tokenRegex))) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+
+  next();
+};
+
+const isValidName = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const isValidAge = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+
+  if ((parseInt(age, 0)) < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+const isValidTalk = (req, res, next) => {
+  const msg = 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios';
+  const { talk } = req.body;
+
+  if (!talk) {
+    return res.status(400).json(
+      { message: msg },
+    );
+  }
+
+  const { watchedAt, rate } = talk;
+
+  if (!watchedAt || !rate) {
+    return res.status(400).json(
+      { message: msg },
+    );
+  }
+
+  next();
+};
+
+const isValidDate = (req, res, next) => {
+  const { talk } = req.body;
+  const { watchedAt } = talk;
+
+  const dateRegex = /^\d{2}([///])\d{2}\1\d{4}$/;
+
+  if (!(watchedAt.match(dateRegex))) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  next();
+};
+
+const isValidRate = (req, res, next) => {
+  const { talk } = req.body;
+  const { rate } = talk;
+  const parseRate = parseInt(rate, 0);
+
+  if (!(Number.isInteger(parseRate)) || rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  next();
+};
+
 module.exports = {
   isValidEmail,
   isValidPassword,
+
+  isValidToken,
+  isValidName,
+  isValidAge,
+
+  isValidTalk,
+  isValidDate,
+  isValidRate,
 };
