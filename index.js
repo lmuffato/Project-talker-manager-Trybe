9c3c47a91c128022be18927e7297e10666e6b826
Lucas André <lucas.cancado89@gmail.com>
2021-08-31 14:18:51 -0300

    Requirement 2: done

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+const talkers = require('./talker.json');
+
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
@@ -14,4 +16,21 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', (req, res) => {
+  if (!talkers || talkers === []) return res.status(200).json([]);
+
+  res.status(200).json(talkers);
+});
+
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  const myTalker = talkers.find((t) => t.id === parseInt(id));
+  if (!myTalker) return res.status(404).json({ "message": "Pessoa palestrante não encontrada" });
+  res.status(200).json(myTalker);
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
 });
