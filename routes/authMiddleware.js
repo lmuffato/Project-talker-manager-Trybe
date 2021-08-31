c129ab2f976c6d1093d2a83d8f3697f5330288ae
Orlando Flores <orlando_flores@id.uff.br>
2021-08-31 18:55:58 -0300

    fix: condição de validação

--- routes/authMiddleware.js
+++ routes/authMiddleware.js
@@ -33,7 +33,7 @@ function validateAge(req, res, next) {
 
 function validateTalk(req, res, next) {
   const { talk } = req.body;
-  if (!talk || !talk.watchedAt || !talk.rate) {
+  if (!talk || !talk.watchedAt || (!talk.rate && talk.rate !== 0)) {
     return res.status(400).json({
       message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
     });
