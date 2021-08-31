9d867f7ea0f92d7cb2aad12823d15fb10332cb21
Alexandre Oliveira <alexandremolive@gmail.com>
2021-08-31 18:07:33 -0300

    feat req 3

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const Chance = require('chance');
 
+const chance = new Chance();
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_ERROR_STATUS = 404;
 const HTTP_OK_STATUS = 200;
+const HTTP_NOK_STATUS = 400;
 const PORT = '3000';
+const MIN_PASSWORD = 5;
 // não remova esse endpoint, e para o avaliador funcionar
 
 app.get('/', (_request, response) => {
@@ -35,10 +39,50 @@ app.get('/talker/:id', async (req, res) => {
         { message: 'Pessoa palestrante não encontrada' },
 );
     }
-    
     res.status(HTTP_OK_STATUS).json(...selectedId);
+});
+
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+
+  if (!email || !email.length) {
+    return res.status(HTTP_NOK_STATUS).json({
+      message: 'O campo "email" é obrigatório',
+    });
+  }
+  if (!email.match(/\S+@\S+\.\S+/)) {
+    return res.status(HTTP_NOK_STATUS).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+
+  if (!password || !password.length) {
+    return res.status(HTTP_NOK_STATUS).json({
+      message: 'O campo "password" é obrigatório',
+    });
+  }
+  if (password.length <= MIN_PASSWORD) {
+    return res.status(HTTP_NOK_STATUS).json({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
+  }
+  next();
+};
+
+app.post('/login', validateEmail, validatePassword, (req, res) => {
+  req.token = chance.string({ length: 16 });
+
+  res.status(HTTP_OK_STATUS).json({ token: req.token });
 });
 
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+// Utilizada a biblioteca Chance para gerar o token aleatorio
+// https://chancejs.com/basics/string.html
