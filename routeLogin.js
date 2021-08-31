1fbf712e8520b0c61baadd2ccc74fa584bb171b6
Hernanne Domiciano <hernannedomiciano@gmail.com>
2021-08-31 20:16:36 -0300

    fix: req3 ACHEI O PROBLEMA

--- routeLogin.js
+++ routeLogin.js
@@ -6,11 +6,11 @@ const router = express.Router();
 const validateEmail = (req, res, next) => {
   const { email } = req.body;
   const emailRegex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
-  if (!email || email === '') {
-    return res.state(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
   if (!emailRegex.test(email)) {
-    return res.state(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   next();
 };
