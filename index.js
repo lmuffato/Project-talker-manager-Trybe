e460454154d7505db5fab0ac26619008c98e1ba5
Fernando Castello Branco <fernandocastellob@gmail.com>
2021-08-31 16:55:17 -0300

    feat:req01

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promisses;
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,13 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_request, response) => {
+  fs.readFile('./talker.json', 'utf8')
+  .then((content) => response.status(200).send(JSON.parse(content)))
+  .catch((err) => response.status(200)
+  .json({ message: `Erro ao buscar arquivo ${err.message}` }));
 });
 
 app.listen(PORT, () => {
