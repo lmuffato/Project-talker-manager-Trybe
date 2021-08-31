d5875f6e7d31a0a87caa1eb5e99eb5d86e9b7186
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-31 14:57:58 -0300

    Feat: Created endpoint GET /talker/:id

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const NOT_FOUND = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -27,6 +28,17 @@ app.get('/talker', async (_req, res) => {
     const talker = await readTalker();
     res.status(HTTP_OK_STATUS).json(talker);
   } catch (e) {
-    res.status(400).json({ message: e.message });
+    res.status(NOT_FOUND).json({ message: e.message });
   }
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+    const talker = await readTalker();
+    // https://www.w3schools.com/jsref/jsref_parseint.asp considerando decimal
+    const talkerById = talker.find((t) => t.id === +id);
+    if (talkerById === undefined) { 
+      return res.status(NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' }); 
+    }
+    res.status(HTTP_OK_STATUS).json(talkerById);
 });
