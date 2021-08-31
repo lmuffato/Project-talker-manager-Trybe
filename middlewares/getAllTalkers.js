f37b46b796b12a358c4798b17cef5d532e86c98a
MarcioFujita <hanker78@gmail.com>
2021-08-31 16:29:43 -0300

    Requisito 1 V2

--- middlewares/getAllTalkers.js
+++ middlewares/getAllTalkers.js
@@ -1,10 +1,11 @@
 const fs = require('fs').promises;
 
-const readTalkers = () => fs.readTalkers('./talker.json', 'utf-8').then((data) => JSON.parse(data));
+const readFile = () => fs.readFile('./talker.json', 'utf-8').then((data) => JSON.parse(data));
 
 const getAllTalkers = async (_req, res) => {
-  const talkersList = await readTalkers();
-  res.status(200).json(talkersList);
+  const talkersList = await readFile();
+  const result = await JSON.parse(talkersList);
+  res.status(200).json(result);
 };
 
 module.exports = getAllTalkers;
