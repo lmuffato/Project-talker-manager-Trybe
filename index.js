1542424fac55f95c09d6068885cd33092e413b7e
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-08-31 15:07:50 -0300

    fix: listening port changed to 3000

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = '3001';
+const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res, next) => {
+  try {
+    const content = await fs.readFile('./talker.json');
+    const talkers = JSON.parse(content);
+    res.status(200).json(talkers);
+  } catch (e) {
+    console.log('Tratamento no middleware específico');
+    next(e.message);
+  }
+});
+
+app.use((err, _req, res, _next) => {
+  console.log('Passou pelo middleware');
+  res.status(400).json({ message: err });
 });
 
 app.listen(PORT, () => {
