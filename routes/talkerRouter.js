5c355430f2e2d37901a692aa84f19ff77a4d5e28
Guilherme de Prais <guilherme.prais@gmail.com>
2021-08-31 11:35:36 -0300

    req 02

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -14,4 +14,17 @@ router.get('/', async (request, response) => {
   return response.status(200).json(talkers);
 });
 
+router.get('/:id', async (request, response) => {
+  const { id } = request.params;
+  const talkers = await readFileSync('./talker.json');
+
+  const talkerById = await talkers.find((talker) => talker.id === Number(id));
+
+  if (!talkerById) {
+    return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return response.status(200).json(talkerById);
+});
+
 module.exports = router;
