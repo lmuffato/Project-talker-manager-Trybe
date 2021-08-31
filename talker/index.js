91f4b547e77f769eb4251f64d5fbade9759dcf19
Orlando Flores <orlando_flores@id.uff.br>
2021-08-31 15:24:26 -0300

    feat: cria função de ler arquivo

--- talker/index.js
+++ talker/index.js
@@ -3,10 +3,19 @@ const { readFile } = require('fs').promises;
 
 const router = express.Router();
 
+const readTalkersList = async (fileName) => {
+  try {
+    const data = await readFile(fileName, 'utf-8');
+    const talkersList = await JSON.parse(data);
+    return talkersList;
+  } catch (e) {
+    return e;
+  }
+};
+
 router.get('/', async (_req, res) => {
   try {
-    const data = await readFile('./talker.json', 'utf-8');
-    const talkersList = await JSON.parse(data);
+    const talkersList = await readTalkersList('./talker.json');
     res.status(200).json(talkersList);
   } catch (e) {
     res.status(400).json({ message: e.message });
