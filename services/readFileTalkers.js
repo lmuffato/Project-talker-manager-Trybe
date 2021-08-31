8459261c6f3730d086bbdf945576747d2dfb75cd
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-31 17:58:21 -0300

    feat: criada a funçao readFileTalkers que le o arquivo talker.json

---
+++ services/readFileTalkers.js
@@ -0,0 +1,10 @@
+const { readFile } = require('fs').promises;
+
+async function readFileTalkers() {
+const getTalkers = await readFile('./talker.json', 'utf-8')
+.catch(() => { throw Error('Erro na leitura na leitura do arquivo talker.json'); });
+const talkersJSON = await JSON.parse(getTalkers);
+return talkersJSON;
+}
+
+module.exports = readFileTalkers;
