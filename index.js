1b1a02013b5f478ca80433d218285a27c516a561
maricegalla <maricegalla@gmail.com>
2021-08-30 23:10:09 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -29,3 +29,17 @@ app.get('/talker', async (_req, res) => {
     res.status(200).json([]);
   }
 });
+
+// 2 - Crie o endpoint GET /talker/:id
+
+app.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const content = await fs.readFile('./talker.json');
+    const talker = JSON.parse(content);
+    const talkerId = talker.findIndex((t) => t.id === +id);
+    if (talkerId === -1) {
+      res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    } else {
+      res.status(200).json(talker[talkerId]);
+    }
+});
