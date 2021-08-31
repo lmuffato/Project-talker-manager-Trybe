b93b884e91054f946ddeb155b94f4a405bfdf6af
Viegas <mvrdgs@gmail.com>
2021-08-31 17:25:06 -0300

    created /talker post route

--- talker.js
+++ talker.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { readFileSync } = require('fs');
+const { readFileSync, writeFileSync } = require('fs');
 
 const router = express.Router();
 
@@ -8,6 +8,75 @@ const readTalkerFile = () => {
   const data = JSON.parse(talkersList);
 
   return data;
+};
+
+const authToken = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+
+  if (authorization.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+
+  next();
+};
+
+const authName = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const authAge = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age || typeof age !== 'number') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+const authTalk = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (!talk || !talk.watchedAt || !talk.rate) {
+    return res.status(400).json({ message: 'O campo "talk" é obrigatório e "watchedAt"'
+      + ' e "rate" não podem ser vazios' });
+  }
+
+  next();
+};
+
+const authWatchedAt = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+
+  const validadeDate = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
+
+  if (!watchedAt.match(validadeDate)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  next();
+};
+
+const authRate = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+
+  if (rate > 5 || rate < 1) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  next();
 };
 
 router.get('/', (_req, res) => {
@@ -33,6 +102,25 @@ router.get('/:id', (req, res) => {
   } catch (err) {
     return res.status(500).send(err.message);
   }
+});
+
+router.post('/', authToken, authName, authAge, authTalk, authWatchedAt, authRate, (req, res) => {
+  const { name, age, talk } = req.body;
+
+  const talkers = readTalkerFile();
+
+  const talkerRegistered = {
+    id: talkers.length + 1,
+    name,
+    age,
+    talk,
+  };
+
+  talkers.push(talkerRegistered);
+
+  writeFileSync('./talker.json', JSON.stringify(talkers));
+
+  return res.status(201).json(talkerRegistered);
 });
 
 module.exports = router;
