2a7dbefbb9c8297decd17eef5567cb09ae2f8ba7
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-31 15:38:42 -0300

    Feat: Created endpoint POST /login

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const NOT_FOUND = 404;
+const BAD_REQUEST = 400;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -41,4 +43,38 @@ app.get('/talker/:id', async (req, res) => {
       return res.status(NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' }); 
     }
     res.status(HTTP_OK_STATUS).json(talkerById);
+});
+
+const validateEmailMiddleware = (req, res, next) => {
+  const { email } = req.body;
+  // https://stackoverflow.com/questions/201323/how-to-validate-an-email-address-using-a-regular-expression onde peguei regex
+  // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript onde aprendi a usar o .test
+  if (email === '' || email === undefined) { 
+    return res.status(BAD_REQUEST)
+    .json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!/^\S+@\S+\.\S+$/.test(email)) {
+    return res.status(BAD_REQUEST)
+    .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+};
+
+const validatePasswordMiddleware = (req, res, next) => {
+  const { password } = req.body;
+  if (password === '' || password === undefined) {
+    return res.status(BAD_REQUEST).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(BAD_REQUEST)
+      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
+app.post('/login', validateEmailMiddleware, validatePasswordMiddleware, (req, res) => {
+  // https://stackoverflow.com/questions/8855687/secure-random-token-in-node-js
+  const token = crypto.randomBytes(8).toString('hex');
+  console.log(token);
+  res.status(HTTP_OK_STATUS).json({ token });
 });
