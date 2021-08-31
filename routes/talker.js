5bdc065a28aa9b0441a5d0731bec2434deaf8162
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-08-31 20:57:15 -0300

    Requisito 4 - Refatorando a chamada do middlewares

--- routes/talker.js
+++ routes/talker.js
@@ -2,13 +2,13 @@ const express = require('express');
 const controller = require('../controllers');
 const middlewares = require('../middlewares');
 
+const middlewaresCreate = [middlewares.checkToken, middlewares.checkName, middlewares.checkTalk,
+  middlewares.checkAge, middlewares.checkRate, middlewares.checkDate];
+
 const route = express.Router();
 
 route.get('/talker', middlewares.checkAll, controller.getAll);
 route.get('/talker/:id', middlewares.getById, controller.getById);
-// Existe um jeito bonitão de refatorar essa chamada de middleware, o Renatão fez isso na aula ao vivo ... ver lá depois
-route.post('/talker', middlewares.checkToken, middlewares.checkName, middlewares.checkTalk,
-middlewares.checkAge, middlewares.checkRate, middlewares.checkDate, 
-controller.createTalker);
+route.post('/talker', middlewaresCreate, controller.createTalker);
 
 module.exports = route;
