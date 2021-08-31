1c13e50d192fa2c275c69e49a459a5eb95e10824
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-31 20:43:46 -0300

    feat: requisito 3.2 - criada middleware validaEmail

--- middlewares/login.js
+++ middlewares/login.js
@@ -4,3 +4,18 @@ function geraToken(_req, res) {
   const token = randonCode();
   res.status(200).json({ token });
 }
+
+/* regex de validação de email adquirido do site abaixo:
+https://stackoverflow.com/questions/61830906/regular-expression-s-s-in-javascript-not-working */
+const emailRegex = /\S+@\S+\.\S+/;
+
+function validaEmail(req, res) {
+  const { email } = req.body;
+  if (email === undefined) {
+     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+  }
+}
