3305b4e43e9c7c7112fecc1f2ef7423205ae3b6c
Joao Nasc <oi@joaonasc.dev>
2021-08-31 19:39:19 -0300

    fix: add getAllTalkers for less typing

--- controllers/talker.js
+++ controllers/talker.js
@@ -1,9 +1,11 @@
 const readFile = require('../utils/readFile');
 
+const getAllTalkers = async () => readFile('./talker.json');
+
 module.exports = {
   async getTalkers(req, res) {
     try {
-      const talkers = await readFile('./talker.json');
+      const talkers = await getAllTalkers();
       res.status(200).json(talkers);
     } catch (error) {
       res.status(400).send({ error: error.message });
