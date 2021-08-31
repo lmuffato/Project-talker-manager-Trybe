d5f7c285bb9db3835ab1be4e1e87a929dc101804
thalysmcarrara <thalysmatiascarrara@gmail.com>
2021-08-31 14:50:02 -0400

    refatorando requisito 1

--- routes/talkerRoutes.js
+++ routes/talkerRoutes.js
@@ -3,9 +3,13 @@ const fs = require('fs').promises;
 
 const router = express.Router();
 
+function getTalkers() {
+  return fs.readFile('./talker.json').then((result) => JSON.parse(result));
+}
+
 router.get('/', async (_req, res) => {
   try {
-    const talkers = await fs.readFile('./talker.json').then((result) => JSON.parse(result));
+    const talkers = await getTalkers();
     if (talkers.length === 0) return res.status(200).send([]);
     
     return res.status(200).send(talkers);
