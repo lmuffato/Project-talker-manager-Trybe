ca6b50781aea8fe14a6c675e2c88cd3ac0f716bf
Orlando Flores <orlando_flores@id.uff.br>
2021-08-31 16:34:32 -0300

    feat: requisito 3

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,35 @@
 const express = require('express');
-// const { readFile } = require('fs').promises;
+const createToken = require('../token');
 
 const router = express.Router();
+
+function validateEmail(req, res, next) {
+  const { email } = req.body;
+  if (!email || email === ' ') {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!(email.includes('@') && email.includes('.com'))) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+}
+
+function validatePassword(req, res, next) {
+  const { password } = req.body;
+  if (!password || password === ' ') {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+}
+
+const AuthMiddleware = [validateEmail, validatePassword];
+
+router.post('/', AuthMiddleware, (_req, res) => {
+  const token = createToken();
+  res.status(200).json({ token });
+});
 
 module.exports = router;
