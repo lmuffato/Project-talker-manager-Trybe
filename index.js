c5511117ac76c4c0b996f38225eb36642dc339ec
Eduardo Seije <eduardoseije@gmail.com>
2021-08-31 20:14:59 -0300

    Requisito 04 - criação da estrutura da rota post /talker. Criação do middleware que valida o name com condicionais de existência e tamanho da string.

--- index.js
+++ index.js
@@ -68,3 +68,19 @@ function tokenGenerator(_req, res) {
 
 app.post('/login', emailValidation, passwordValidation, tokenGenerator, (_req, _res) => {
 });
+
+function nameValidation(req, res, next) {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+}
+
+
+app.post('/talker', nameValidation, (req, res) => {
+  const { name, age, talk: {watchedAt, rate} } = req.body;
+});
