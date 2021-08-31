52455f928ece82171ea2cb10366214994bf58b3d
maricegalla <maricegalla@gmail.com>
2021-08-31 05:13:16 -0300

    Requisito 3

--- index.js
+++ index.js
@@ -43,3 +43,59 @@ app.get('/talker/:id', async (req, res) => {
       res.status(200).json(talker[talkerId]);
     }
 });
+
+// 3 - Crie o endpoint POST /login
+
+const verifyEmailExists = (req, res, next) => {
+  const { email } = req.body;
+  
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  } 
+    next();
+};
+
+const verifyEmailPattern = (req, res, next) => {
+  const { email } = req.body;
+  const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+  
+  if (emailPattern.test(email) === false) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+    next();
+};
+
+const verifyPasswordExists = (req, res, next) => {
+  const { password } = req.body;
+  
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+    next();
+};
+
+const verifyPasswordLenght = (req, res, next) => {
+  const { password } = req.body;
+  
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  } 
+    next();
+};
+
+const generateToken = () => {
+  const length = 16;
+  const a = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.split('');
+  const b = [];  
+  for (let i = 0; i < length; i += 1) {
+      const j = (Math.random() * (a.length - 1)).toFixed(0);
+      b[i] = a[j];
+  }
+  return b.join('');
+};
+
+app.post('/login', 
+verifyEmailExists, verifyEmailPattern, 
+verifyPasswordExists, verifyPasswordLenght, (_req, res) => {
+  res.status(200).json({ token: generateToken() });
+});
