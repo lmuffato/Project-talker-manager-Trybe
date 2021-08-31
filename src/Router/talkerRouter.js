f3f91dd500c38ef28228cbbc3b1dba57e2bfedbd
Maria Baeta <m.martinsdg@gmail.com>
2021-08-31 17:14:09 -0300

    feat: :sparkles: Requisito 2 - Get Talker by Id
    Retorna a pessoa palestramte cadastrada que o id sejá igual ao informado.

--- src/Router/talkerRouter.js
+++ src/Router/talkerRouter.js
@@ -1,16 +1,25 @@
 const { Router } = require('express');
-const fs = require('fs/promises');
+const fs = require('fs').promises;
 
 const router = Router();
 
 router.get('/', async (_req, res) => {
   try {
     const data = await fs.readFile('talker.json', 'utf-8');
-    const talker = JSON.parse(data);
-    res.status(200).json(talker);
+    const talkers = JSON.parse(data);
+    res.status(200).json(talkers);
   } catch (e) {
     console.error(`Erro: ${e}`);
   }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await fs.readFile('talker.json', 'utf-8');
+  const talkers = JSON.parse(data);
+  const talker = talkers.find((t) => t.id === parseInt(id, 0));
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(200).json(talker);
 });
 
 module.exports = router;
