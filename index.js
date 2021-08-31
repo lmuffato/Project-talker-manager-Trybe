dd786df81d321dffde6609c630b62d756600ec7e
w-cypriano <profwellington17@gmail.com>
2021-08-31 16:48:08 -0300

    requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,16 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  try {
+      const response = await fs.readFile('./talker.json', 'utf-8');
+      console.log(response);
+      res.status(200).json(JSON.parse(response));
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
 
 app.listen(PORT, () => {
