96f3607f893b5b96b4cec046ac8d3f143ea46524
Alexandre Oliveira <alexandremolive@gmail.com>
2021-08-31 16:48:10 -0300

    feat req 2

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs').promises;
 const app = express();
 app.use(bodyParser.json());
 
+const HTTP_ERROR_STATUS = 404;
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
@@ -21,6 +22,21 @@ app.get('/talker', async (_req, res) => {
   } catch (e) {
     res.status(HTTP_OK_STATUS).json([]);
   }
+});
+
+app.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const content = await fs.readFile('./talker.json');
+    const data = JSON.parse(content);
+    const selectedId = data.filter((d) => d.id === parseInt(id, 10));
+    
+    if (!selectedId.length) {
+      return res.status(HTTP_ERROR_STATUS).json(
+        { message: 'Pessoa palestrante não encontrada' },
+);
+    }
+    
+    res.status(HTTP_OK_STATUS).json(...selectedId);
 });
 
 app.listen(PORT, () => {
