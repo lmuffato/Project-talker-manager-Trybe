e9f73806f113703b93e07dc20b0e2bb9583d38d5
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-31 15:16:19 -0300

    Req 1 - Cria endpoint /talkers e retorna array de talkers

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -6,6 +7,14 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+const talkers = JSON.parse(fs.readFileSync('talker.json', 'utf-8'));
+
+app.get('/talker', (_req, res) => {
+  if (talkers === '' || talkers.length === 0) return res.status(200).json([]);
+
+  res.status(200).json(talkers);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
