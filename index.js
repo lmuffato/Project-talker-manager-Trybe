84e4e218dc902fa625b16844a78b55d089f9e375
Orlando Flores <orlando_flores@id.uff.br>
2021-08-31 14:57:35 -0300

    feat: importa o talker router no app

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+const talkerRouter = require('./talker');
+
+app.use('/talker', talkerRouter);
+
 app.listen(PORT, () => {
   console.log('Online');
 });
