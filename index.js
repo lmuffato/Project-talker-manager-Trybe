3313397d1c9a0fd7cbb333e781764a0307703b32
jonathan <jonathanfernandesjf98@gmail.com>
2021-08-31 15:07:31 -0300

    criacao do endpoint /talker

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const talkers = [
+  {
+    name: 'Henrique Albuquerque',
+    age: 62,
+    id: 1,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+  {
+    name: 'Heloísa Albuquerque',
+    age: 67,
+    id: 2,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+  {
+    name: 'Ricardo Xavier Filho',
+    age: 33,
+    id: 3,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+  {
+    name: 'Marcos Costa',
+    age: 24,
+    id: 4,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+];
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -14,4 +41,9 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', (_req, res) => {
+  if (!talkers) return res.status(200).json([]);
+  res.status(200).json(talkers);
 });
