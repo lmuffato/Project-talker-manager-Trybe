cdc1e9ae580d7263a1b998c4055abb7611d4ed64
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-31 18:27:18 -0300

    feat: requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const getAllTalkers = require('./middlewares/getAllTalkers');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,3 +16,5 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+app.get('/user', getAllTalkers);
