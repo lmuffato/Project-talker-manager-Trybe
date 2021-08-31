338845584fa070f286113218326538f6905f3a1d
Sergio Martins <sergiomos2010@gmail.com>
2021-08-31 16:14:53 -0300

    Feat: create generate random token

---
+++ utils/generateToken.js
@@ -0,0 +1,16 @@
+// Referencia: https://stackoverflow.com/questions/8532406/create-a-random-token-in-javascript-based-on-user-details
+
+const generateRandomString = () => Math.random().toString(36).substr(2);
+
+const generateToken = () => {
+  const tokenLength = 16;
+  let token = '';
+  
+  do {
+    token += generateRandomString();
+  } while (token.length < tokenLength);
+
+  return token.substr(0, tokenLength);
+};
+
+module.exports = generateToken();
