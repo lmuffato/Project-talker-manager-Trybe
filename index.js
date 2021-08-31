5afea356fd1bdac0a6316765c4ae3920d2267f73
Eduardo Seije <eduardoseije@gmail.com>
2021-08-31 15:04:52 -0300

    Requisito 1  - criando o endpoit /talker - Criação da função readFile que fará a leitura do arquivo json utilizando .then. Adição do método .promises ao fs. Criação da rota GET /talker (assíncrona) que retorna o status 200 e a lista de palestrantes.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
+const fs = require('fs').promises;
+const { json } = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const talkers = "talker.json"
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -15,4 +17,14 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+const readFile = () => fs.readFile(talkers, 'utf8')
+  .then((data) => {
+    return JSON.parse(data);
+  });
+
+app.get('/talker', async (_req, res) => {
+  const talkersList = await readFile();
+  return res.status(HTTP_OK_STATUS).json(talkersList);
 });
