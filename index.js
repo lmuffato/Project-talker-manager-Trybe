3bf5d330e6e6f22282d9c26c687566ec7f728b06
mallmann02 <leomallmann020302@gmail.com>
2021-08-31 16:10:05 -0300

    feat(routes): create endpoint /talker

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const data = require('./talker.json');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,10 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (req, res) => {
+  return res.status(HTTP_OK_STATUS).json(data || []);
 });
 
 app.listen(PORT, () => {
