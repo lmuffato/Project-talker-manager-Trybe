2eaef3c2b1bf3e1196d0f143dd353fc364813c12
lucas-paz <lucas.hpaz@outlook.com>
2021-08-31 18:11:14 -0300

    fix: lint

--- Routes/talker.js
+++ Routes/talker.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { readFile, writeFile } = require('fs').promises;
 const authMiddlewares = require('./middlewares');
 
+const TALKER_JSON = 'talker.json';
+
 const router = express.Router();
 const [validateToken] = authMiddlewares;
 
@@ -13,7 +15,7 @@ const getFileData = async (file) => {
 
 router.get('/', async (_req, res) => {
   try {
-    const talkers = await getFileData('talker.json');
+    const talkers = await getFileData(TALKER_JSON);
     res.status(200).json(talkers);
   } catch (error) {
     res.status(200).json([]);
@@ -23,7 +25,7 @@ router.get('/', async (_req, res) => {
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   try {
-    const talkers = await getFileData('talker.json');
+    const talkers = await getFileData(TALKER_JSON);
     const talker = talkers.find((person) => person.id === parseInt(id, 10));
     if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
     res.status(200).json(talker);
@@ -36,9 +38,9 @@ router.post('/', authMiddlewares, async (req, res) => {
   const { name, age, talk } = req.body;
 
   try {
-    const talkers = await getFileData('talker.json');
+    const talkers = await getFileData(TALKER_JSON);
     const newTalker = { name, age, talk, id: (talkers.length + 1) };
-    await writeFile('talker.json', JSON.stringify([...talkers, newTalker]));
+    await writeFile(TALKER_JSON, JSON.stringify([...talkers, newTalker]));
     res.status(201).json(newTalker);
   } catch (error) {
     res.status(500).end();
@@ -48,9 +50,9 @@ router.post('/', authMiddlewares, async (req, res) => {
 router.delete('/:id', validateToken, async (req, res) => {
   const { id } = req.params;
   try {
-    const talkers = await getFileData('talker.json');
+    const talkers = await getFileData(TALKER_JSON);
     const newTalkers = talkers.filter((talker) => talker.id !== parseInt(id, 10));
-    await writeFile('talker.json', JSON.stringify(newTalkers), 'utf-8');
+    await writeFile(TALKER_JSON, JSON.stringify(newTalkers), 'utf-8');
     res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
   } catch (error) {
     res.status(500).end();
