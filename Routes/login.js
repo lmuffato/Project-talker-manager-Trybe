2337cdaa4c2cc4ef6570ccf88107c65d8df18f03
lucas-paz <lucas.hpaz@outlook.com>
2021-08-31 16:12:15 -0300

    cria os middlewares de verificacao do email e password

---
+++ Routes/login.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const router = express.Router();
+
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+  if (!email || email === '') {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (
+    !(email.includes('@') && email.includes('.com'))
+  ) return res.status(400).json({ massage: 'O "email" deve ter o formato "email@email.com"' });
+
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+  const MIN_LENGTH = 6;
+  if (!password || password === '') {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < MIN_LENGTH) {
+    return res.status(400).json({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
+  }
+
+  next();
+};
+
+router.post('/', (req, res, _next) => {
+  const { email, password } = req.body;
+});
+
+module.exports = router;
