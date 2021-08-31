c27c487f6a37482f2e7a9fa75f721f2d943651b0
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-31 17:48:19 -0300

    Req 4 - cria rota POST /talk com validacoes

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -1,11 +1,66 @@
 const express = require('express');
-const fs = require('fs').promises;
+const { readTalkers, writeTalker, isDateValid } = require('../utils/utils');
 
 const router = express.Router();
 
-const readTalkers = async () => {
-  const data = await fs.readFile(`${__dirname}/../talker.json`, 'utf-8');
-  return JSON.parse(data);
+const verifyToken = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+
+  if (authorization.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+
+  next();
+};
+
+const verifyTalkerName = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const verifyTalkerAge = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+const verifyTalkerTalk = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (!talk || !talk.watchedAt || !talk.rate) {
+    return res.status(400).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+
+  if (!isDateValid(talk.watchedAt)) {
+    return res.status(400).json({
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
+  next();
+};
+
+const verifyTalkerRate = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+  console.log('----------- RATE ----------', rate);
+  if (rate < 1 || rate > 5) {
+    return res.status(400).json({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
+  }
+
+  next();
 };
 
 router.get('/', async (_req, res) => {
@@ -23,5 +78,26 @@ router.get('/:id', async (req, res) => {
 
   res.status(200).json(talker);
 });
+
+router.post(
+  '/',
+  verifyToken,
+  verifyTalkerName,
+  verifyTalkerAge,
+  verifyTalkerTalk,
+  verifyTalkerRate,
+  async (req, res) => {
+    const { name, age, talk } = req.body;
+    const talkers = await readTalkers();
+
+    const lastId = talkers[talkers.length - 1].id;
+    const newId = lastId ? lastId + 1 : 1;
+    const newTalker = { id: newId, name, age, talk };
+    talkers.push(newTalker);
+
+    await writeTalker(talkers);
+    res.status(201).json(newTalker);
+  },
+);
 
 module.exports = router;
--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,15 @@
+const fs = require('fs').promises;
 const crypto = require('crypto');
+
+exports.readTalkers = async () => {
+  const data = await fs.readFile(`${__dirname}/../talker.json`, 'utf-8');
+  return JSON.parse(data);
+};
+
+exports.writeTalker = async (talkers) => {
+  const talkerStringified = JSON.stringify(talkers);
+  return fs.writeFile(`${__dirname}/../talker.json`, talkerStringified);
+};
 
 // Função verifyEmail consultada no meu próprio PR do projeto Trybe Wallet
 // Link: https://github.com/tryber/sd-010-a-project-trybewallet/pull/70
@@ -12,4 +23,18 @@ exports.verifyEmail = (email) => {
 exports.generateToken = async () => {
   const token = await crypto.randomBytes(8).toString('hex');
   return token;
+};
+
+// watchedAt: '42-20-3333'
+function checkDateNumbers(dateArr) {
+  if (+dateArr[0] < 1 || +dateArr[0] > 31) return false;
+  if (+dateArr[1] > 12) return false;
+  if (dateArr[2].length !== 4) return false;
+  return true;
+}
+
+exports.isDateValid = (date) => {
+  if (date[2] !== '/' || date[5] !== '/') return false;
+
+  return checkDateNumbers(date.split('/'));
 };
