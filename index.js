0a4b9d4b2f2b021de312a7b5043ea79cc6a492e6
Alexandre Oliveira <alexandremolive@gmail.com>
2021-08-31 15:48:17 -0300

    feat req 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,8 +8,19 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
+
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  try {
+    const content = await fs.readFile('./talker.json');
+    const data = JSON.parse(content);
+    res.status(HTTP_OK_STATUS).json(data);
+  } catch (e) {
+    res.status(HTTP_OK_STATUS).json([]);
+  }
 });
 
 app.listen(PORT, () => {
