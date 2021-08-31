2a0531cd20d373e68c587df09ed96a2e574e72ca
Marcus C. H. Filho <mchusemann@gmail.com>
2021-08-31 10:38:03 -0300

    feat: challenge2 - getTalkerById

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rescue = require('express-rescue');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
@@ -8,6 +9,23 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+// getTalkerById
+app.get(
+  '/talker/:id',
+  (request, response) => {
+    const { id } = request.params;
+    const data = fs.readFileSync('./talker.json', { encoding: 'utf-8' });
+    const jsonData = JSON.parse(data);
+    const talker = jsonData.find((person) => +id === person.id);
+    console.log(talker);
+    if (!talker) {
+      return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    response.status(200).json(talker);
+  },
+);
+
+// getAllTalkers
 app.get('/talker', (_request, response) => {
   const data = fs.readFileSync('./talker.json', { encoding: 'utf-8' });
   const jsonData = JSON.parse(data);
