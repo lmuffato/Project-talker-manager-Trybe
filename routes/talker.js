72b03b08b04b95e50ddad766779cc8ff9f660641
Sergio Martins <sergiomos2010@gmail.com>
2021-08-31 15:58:54 -0300

    Desafio 2: get talker by id

--- routes/talker.js
+++ routes/talker.js
@@ -4,14 +4,29 @@ const readFile = require('../utils/readFile');
 const router = express.Router();
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
+
+const TALKERS = readFile('talker.json');
 
 router.get('/', (_req, res) => {
-  const talkers = readFile('talker.json');
-  const hasTalkers = talkers || talkers.length > 0;
+  const hasTalkers = TALKERS || TALKERS.length > 0;
 
   if (!hasTalkers) return res.status(HTTP_OK_STATUS).json([]);
 
-  res.status(HTTP_OK_STATUS).json(talkers);
+  res.status(HTTP_OK_STATUS).json(TALKERS);
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const foundTalker = TALKERS.find((t) => t.id === parseInt(id, 10));
+
+  if (!foundTalker) {
+    return res
+    .status(HTTP_NOT_FOUND_STATUS)
+    .json({ message: 'Pessoa palestrante não encontrada' }); 
+  }
+
+  res.status(HTTP_OK_STATUS).json(foundTalker);
 });
 
 module.exports = router;
