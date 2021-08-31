42c383ffc64a88e66b90600ed13bb1820cc67905
maricegalla <maricegalla@gmail.com>
2021-08-31 15:46:59 -0300

    Requisito 5 erro validação ultimo teste

--- index.js
+++ index.js
@@ -164,16 +164,19 @@ findAge, checkAge, findTalk, checkRate, checkTalk, checkWhachedAt,
  async (req, res) => {
   const { id } = req.params;
   const { name, age, talk: { watchedAt, rate } } = req.body;
-  // const content = await fs.readFile('./talker.json');
-  // const talker = JSON.parse(content);
+  const content = await fs.readFile('./talker.json');
+  const talker = JSON.parse(content);
+  const otherTalker = talker.filter((t) => t.id !== +id);
   const editedTalker = {
+    id,
     name,
     age,
-    id,
     talk: {
-      rate,
       watchedAt,
+      rate,
     },
   };
-  res.status(201).json(editedTalker);
+  otherTalker.push(editedTalker);
+  await fs.writeFile('./talker.json', JSON.stringify(otherTalker));
+  res.status(200).json(editedTalker);
 });
