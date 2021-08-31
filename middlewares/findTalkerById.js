44a6aeea74cd9e2472db0c473af6f9f9f0fa7d2e
Flavio Franco <flavio.win@gmail.com>
2021-08-31 15:22:00 -0300

    requisito 2

--- middlewares/findTalkerById.js
+++ middlewares/findTalkerById.js
@@ -2,13 +2,15 @@ const fs = require('fs');
 
 const findTalkerById = (req, res) => {
   const { id } = req.params;
-  try {
-    const object = JSON.parse(fs.readFileSync('./talker.json', 'UTF-8'));
-    const talker = object.find((tk) => tk.id === +id);
-    return res.status(200).json(talker);
-  } catch (e) {
+ 
+  const object = JSON.parse(fs.readFileSync('./talker.json', 'UTF-8'));
+  const talker = object.find((tk) => tk.id === +id);
+
+  if (!talker) {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
+
+  return res.status(200).json(talker);  
 };
 
 module.exports = findTalkerById;
