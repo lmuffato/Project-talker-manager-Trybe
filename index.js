bdfa4dc55f9463d35cb0fad8e015663c477159c8
Jodiel Briesemeister <jodielll29@gmail.com>
2021-08-31 00:34:59 -0300

    feat: req01

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +15,10 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (_req, res) => {
+  const talker = await fs.readFile('./talker.json', 'utf-8');
+  const result = await JSON.parse(talker);
+  res.status(200).json(result);
 });
