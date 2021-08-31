ebc6805d4d35666c60e024f2a1e42133736636da
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-31 18:03:02 -0300

    feat: criado o middleware getAllTalkers

---
+++ middlewares/getAllTalkers.js
@@ -0,0 +1,12 @@
+const readFileTalker = require('../services/readFileTalkers.js');
+
+async function getAllTalkers(_req, res, next) {
+try {
+const getTalkers = await readFileTalker();
+res.status(200).json(getTalkers);
+} catch (e) {
+next(e);
+}
+}
+
+module.exports = getAllTalkers;
