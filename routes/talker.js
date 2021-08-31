15e9f1dfeef856f5a957e810bfc01f5b520b1569
Orlando Flores <orlando_flores@id.uff.br>
2021-08-31 17:16:17 -0300

    feat: termina a rota post /talker

--- routes/talker.js
+++ routes/talker.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { readFile } = require('fs').promises;
+const { readFile, writeFile } = require('fs').promises;
+const AuthMiddleware = require('./authMiddleware');
 
 const router = express.Router();
 
@@ -36,80 +37,16 @@ router.get('/:id', async (req, res) => {
   res.status(200).json(talker);
 });
 
-function validateToken(req, res, next) {
-  const { authorization } = req.headers;
-  if (!authorization || authorization === ' ') {
-    return res.status(401).json({ message: 'token não encontrado' });
+router.post('/talker', AuthMiddleware, async (req, res) => {
+  const { name, age, talk } = req.body;
+  try {
+    const talkersList = await readTalkersList('./talker.json');
+    const newTalker = { name, age, talk, id: `${talkersList.length + 1}` };
+    const newTalkersList = [...talkersList, newTalker];
+    await writeFile('./talker.json', JSON.stringify(newTalkersList), 'utf-8');
+    return res.status(200).json(newTalker);
+  } catch (e) {
+    res.status(400).json({ message: e.message });
   }
-  if (authorization.length !== 16) {
-    return res.status(401).json({ message: 'token inválido' });
-  }
-  next();
-}
-
-function validateName(req, res, next) {
-  const { name } = req.body;
-  if (!name || name === ' ') {
-    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
-  }
-  if (name.length < 3) {
-    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
-  }
-  next();
-}
-
-function validateAge(req, res, next) {
-  const { age } = req.body;
-  if (!age || age === ' ') {
-    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
-  }
-  if (age < 18) {
-    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
-  }
-  next();
-}
-
-function validateTalk(req, res, next) {
-  const { talk } = req.body;
-  const { watchedAt, rate } = talk;
-  if (!talk || !watchedAt || !rate) {
-    return res.status(400).json({
-      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
-    });
-  }
-  next();
-}
-
-function validateWatchedAt(req, res, next) {
-  const { talk: { watchedAt } } = req.body;
-  const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
-
-  if (!dateRegex.test(watchedAt)) {
-    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
-  }
-
-  next();
-}
-
-function validateRate(req, res, next) {
-  const { talk: { rate } } = req.body;
-
-  if (rate < 1 || rate > 5) {
-    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-  }
-  next();
-}
-
-const AuthMiddleware = [
-  validateToken,
-  validateName,
-  validateAge,
-  validateTalk,
-  validateWatchedAt,
-  validateRate,
-];
-
-router.post('/talker', AuthMiddleware, (_req, _res) => {
-
 });
 module.exports = router;
