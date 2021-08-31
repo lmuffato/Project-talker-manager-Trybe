5a74f1f9e99a70085e981a133d2acd129952dceb
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-08-31 19:58:28 -0400

    feat: :sparkles: Requisito 1
    Passou no teste getAllTalkers

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
+const { readFile } = require('fs').promises;
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -14,4 +15,16 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+// 1. Crie o endpoint GET /talker
+
+app.get('/talker', async (_req, res) => {
+  try {
+    const data = await readFile('./talker.json');
+    const talkers = JSON.parse(data);
+    res.status(200).json(talkers);
+  } catch (e) {
+    res.status(200).json([]);
+  }
 });
