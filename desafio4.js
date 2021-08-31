0d6037f117981f0718a2cbfefe197d2d0b5d70ce
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-08-31 20:03:12 -0300

    Desafio 4 - Nome Verificado

--- desafio4.js
+++ desafio4.js
@@ -8,11 +8,23 @@ const validateToken = (authorization, res) => {
     return false;
 };
 
+const validateName = (name, res) => {
+    if (!name || name === '') {
+        return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+    }
+    if (name.length < 3) {
+        return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+    }
+    return false;
+};
+
 const createTalker = (req, res) => {
     const { authorization } = req.headers;
     const talker = req.body;
+    const { name } = req.body;
 
     if (validateToken(authorization, res)) return;
+    if (validateName(name, res)) return;
 
     res.status(201).json(talker);
 };
