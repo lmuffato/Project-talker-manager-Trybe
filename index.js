cd3b719a5b4f4b8d794f56fcc0eb6ca9d2866a4e
Hudson Carlos <hc88806@gmail.com>
2021-08-31 17:23:01 -0400

    router '/talker' done

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
+
+app.get('/talker', async (_req, res) => {
+
+  const talkers = await fs.readFile('./talker.json', 'utf8');
+
+  return res.status(200).json(JSON.parse(talkers));
+});
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
