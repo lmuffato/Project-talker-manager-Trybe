e7c3f368176ef12cea3be7ee6c0a14d6bfdfb5e0
lucas-paz <lucas.hpaz@outlook.com>
2021-08-31 15:43:37 -0300

    requisito 2

--- talkerRouter/index.js
+++ talkerRouter/index.js
@@ -3,13 +3,30 @@ const { readFile } = require('fs').promises;
 
 const router = express.Router();
 
+const getFileData = async (file) => {
+  const data = await readFile(file, 'utf-8');
+  const results = JSON.parse(data);
+  return results;
+};
+
 router.get('/', async (_req, res) => {
   try {
-    const data = await readFile('talker.json', 'utf-8');
-    const talkers = JSON.parse(data);
-    return res.status(200).json(talkers);
+    const talkers = await getFileData('talker.json');
+    res.status(200).json(talkers);
   } catch (error) {
-    return res.status(200).json([]);
+    res.status(200).json([]);
+  }  
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const talkers = await getFileData('talker.json');
+    const talker = talkers.find((person) => person.id === parseInt(id, 10));
+    if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    res.status(200).json(talker);
+  } catch (error) {
+    res.status(500).end();
   }
 });
 
