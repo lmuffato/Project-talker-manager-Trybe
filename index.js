236adfd8231f14add4a045c24bc4b755dcfcc40d
Hudson Carlos <hc88806@gmail.com>
2021-08-31 17:38:47 -0400

    router '/talker:id' done

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ app.get('/talker', async (_req, res) => {
   return res.status(200).json(JSON.parse(talkers));
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const talkers = await fs.readFile('./talker.json', 'utf8');
+  const talker = JSON.parse(talkers)
+    .filter(talker => talker.id === Number(id));
+
+  if (talker.length < 1) return res.status(404).json({
+    message: "Pessoa palestrante não encontrada"
+  });
+
+  res.status(200).json(talker[0]);
+});
+
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
