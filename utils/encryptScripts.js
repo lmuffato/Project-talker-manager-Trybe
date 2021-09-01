0d227ef678386d936364eeca642e3fb6cd635f1d
gabrieldeori <deori.pessoal@gmail.com>
2021-09-01 09:51:06 -0300

    Adding reference to crypto, stackoverflow question 8855687

--- utils/encryptScripts.js
+++ utils/encryptScripts.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+// https://stackoverflow.com/questions/8855687/secure-random-token-in-node-js
 
 function encryptToString(numberOfBytes) {
   return crypto.randomBytes(numberOfBytes).toString('hex');
