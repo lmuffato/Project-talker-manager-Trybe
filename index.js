f5f0ccd0edae71fecf5a2fd91960306b6956037b
Marina Alane <marina.alane@outlook.com>
2021-09-01 17:03:53 -0300

    requisito 2 - mostrar pessoas palestrantes individualmente

--- index.js
+++ index.js
@@ -13,10 +13,24 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// Requisito 1
+
 app.get('/talker', async (_request, response) => {
   const talkers = await fs.readFile('./talker.json', 'utf-8');
   const resul = await JSON.parse(talkers);
   response.status(200).json(resul);
+});
+
+// Requisito 2
+
+app.get('/talker/:id', async (request, response) => {
+  const { id } = request.params;
+  const talkers = await fs.readFile('./talker.json', 'utf-8');
+  const resul = await JSON.parse(talkers);
+  const talkerId = resul.find((talker) => talker.id === +id);
+  // console.log(talkerId);
+  if (!talkerId) response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  response.status(200).json(talkerId);
 });
 
 app.listen(PORT, () => {
