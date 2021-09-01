1d15f3eab9f806de2fb78ea3835e7b0279a422fb
gabrieldeori <deori.pessoal@gmail.com>
2021-09-01 13:53:34 -0300

    FIX LINT: reduce talkerToAdd, no enter between params

--- middlewares/editTalkers.js
+++ middlewares/editTalkers.js
@@ -3,29 +3,15 @@ const { writeAnObjectIntoAJSONFile } = require('../utils/write');
 const { returnArrayDifferentID } = require('../utils/talkerFilters');
 
 async function editTalkers(request, response) {
-  try {
     const { id } = request.params;
     const { name, age, talk } = request.body;
     const talkerDBPath = './talker.json';
     const talkerDB = await readJsonReturnArray(talkerDBPath);
     const newTalkersList = returnArrayDifferentID(talkerDB);
-
-    const talkerToAdd = {
-      name,
-      age,
-      talk,
-      id,
-    };
-
+    const talkerToAdd = { name, age, talk, id };
     newTalkersList.push(talkerToAdd);
-
     await writeAnObjectIntoAJSONFile(talkerDB);
     return response.status(201).json(talkerToAdd);
-
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
 }
 
 module.exports = editTalkers;
