3f4ea565fa2a963d1a51295b0980b57bb3f1353c
gabrieldeori <deori.pessoal@gmail.com>
2021-09-01 15:46:53 -0300

    Missing semicolon on deleteTalker

--- middlewares/deleteTalker.js
+++ middlewares/deleteTalker.js
@@ -7,7 +7,7 @@ async function deleteTalker(request, response) {
   const talkerDBPath = './talker.json';
   const talkerDB = await readJsonReturnArray(talkerDBPath);
   const newTalkersList = returnArrayDifferentID(talkerDB, id);
-  const message = { message: 'Pessoa palestrante deletada com sucesso' }
+  const message = { message: 'Pessoa palestrante deletada com sucesso' };
   await writeAnObjectIntoAJSONFile(newTalkersList);
   return response.status(200).send(message);
 }
