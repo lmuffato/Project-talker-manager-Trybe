df0a59be1ad8dd988ba106e498df47cf2757768e
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-09-01 10:09:06 -0300

    Requirement 01: get path /talker and put file talker.json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,16 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  try {
+    const file = await fs.readFile('talker.json');
+    const arrayOfObejct = JSON.parse(file);
+    res.status(200).json(arrayOfObejct);
+  } catch (err) {
+    return res.status(200).json([]);
+  }
 });
 
 app.listen(PORT, () => {
