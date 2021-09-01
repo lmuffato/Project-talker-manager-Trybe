aad04d1077d8e70bb0fe154aa815b2dc128e24b4
Stanrley Barbosa <3011stan@gmail.com>
2021-08-31 22:19:38 -0300

    Req 4 done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const crypto = require('crypto');
+const { writeFileSync } = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -64,6 +65,74 @@ const validatePasswordMiddleware = (req, res, next) => {
 app.post('/login', validateEmailMiddleware, validatePasswordMiddleware, (req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
   res.status(200).send({ token });
+});
+// req 4
+const validateToken = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).send({ message: 'Token não encontrado' });
+  }
+  if (authorization.length !== 16) {
+    return res.status(401).send({ message: 'Token inválido' });
+  }
+  next();
+};
+const validateName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name || name === '') {
+    return res.status(400).send({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+const validateAge = (req, res, next) => {
+  const { age } = req.body;
+  if (!age || age === '') {
+    return res.status(400).send({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).send({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+const validateTalk = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk || !talk.watchedAt || !talk.rate) {
+    return res.status(400).json({ message: 'O campo "talk" é obrigatório e "watchedAt"'
+      + ' e "rate" não podem ser vazios' });
+  }
+  next();
+};
+
+const validateWatchedAt = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+  const re = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
+  if (!re.test(watchedAt)) {
+    return res.status(400).send({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+};
+
+const validateRate = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+  if (rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
+app.post('/talker', validateToken, validateName,
+validateAge, validateTalk, validateWatchedAt, validateRate, async (req, res) => {
+  const { name, age, talk } = req.body;
+  const talkers = await readFileCustom();
+  const newTalker = {
+    id: talkers.length + 1, name, age, talk,
+  };
+  talkers.push(newTalker);
+  writeFileSync('./talker.json', JSON.stringify(talkers));
+  return res.status(201).json(newTalker);
 });
 // FINAL MINHAS IMPLEMENTAÇÕES
 
