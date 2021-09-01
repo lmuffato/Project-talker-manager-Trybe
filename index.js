cb58c0c15d1dfc1e7e1393bdabc8273f6f28fc75
Patrick Morais <ppternunes@gmail.com>
2021-09-01 15:37:28 -0300

    req 2

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOTFOUND_STATUS = 404;
 const PORT = '3000';
+
+const messageNotFound = {
+  message: 'Pessoa palestrante não encontrada',
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -24,8 +29,19 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 
+// Requisito 1
 app.get('/talkers', async (_req, res) => {
   const dataTalkers = await readFile();
   if (dataTalkers.length > 0) return res.status(HTTP_OK_STATUS).json(dataTalkers);
   return res.status(HTTP_OK_STATUS).json([]);
+});
+
+app.get('/talkers/:id', async (req, res) => {
+  const { id } = req.params;
+  const dataTalkers = await readFile();
+  
+  const dataTalkerFiltered = dataTalkers.find((talker) => talker.id === id);
+
+  if (dataTalkerFiltered.length > 0) return res.status(HTTP_OK_STATUS).json(dataTalkerFiltered);
+  return res.status(HTTP_NOTFOUND_STATUS).json(messageNotFound);
 });
