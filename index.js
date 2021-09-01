b32547958788f5f67babc38e9753b6e32686c561
w-cypriano <profwellington17@gmail.com>
2021-09-01 17:52:40 -0300

    Requisito3

--- index.js
+++ index.js
@@ -59,11 +59,30 @@ app.get('/talker/:id', async (req, res) => {
 });
 
 // at3
-app.post('/login', (req, res) => { 
-  /* const { email, senha } = req.body; */
-
-const token = { token: '7mqaVRXJSp886CGr' };
-res.status(200).json(token); 
+app.post('/login', (req, res, next) => {
+    const { email } = req.body;
+    const re = /\S+@\S+\.\S+/;
+    const regex = re.test(email);
+    if (!email || email === '') {
+      return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    }
+    if (regex === false) {
+      return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    }
+      next();
+  },
+  (req, res, next) => {
+  const { password } = req.body;
+  if (!password || password === '') {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+  return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+    next();
+  },
+  (req, res) => {
+    res.status(200).json({ token: '7mqaVRXJSp886CGr' }); 
 });
 
 app.listen(PORT, () => {
