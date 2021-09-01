ae8b321d07f4592b8c0c5d990b9f609d5b9bb8c9
Micaela Alves <micaela.alves93@gmail.com>
2021-09-01 15:53:54 -0300

    estrutura inicial

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const talkerRouter = require('./middlewares/talkerRouter.js');
+const loginRouter = require('./middlewares/loginRouter.js');
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -11,6 +14,9 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.use('/talker', talkerRouter);
+app.use('/login', loginRouter);
 
 app.listen(PORT, () => {
   console.log('Online');
