a38b87124d8ad22190bf4987e984aef22d9c457e
Marina Alane <marina.alane@outlook.com>
2021-09-01 15:39:29 -0300

    lista pessoas palestrantes - requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,12 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talkers', async (_request, response) => {
+  const talkers = await fs.readFile('./talker.json', 'utf-8');
+  const resul = await JSON.parse(talkers);
+  response.status(200).json(resul);
 });
 
 app.listen(PORT, () => {
