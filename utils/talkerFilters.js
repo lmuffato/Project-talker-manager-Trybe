4424f657aa6ffded64257353b77c70283dec7abc
gabrieldeori <deori.pessoal@gmail.com>
2021-09-01 15:20:23 -0300

    Adding parseInt to filter

--- utils/talkerFilters.js
+++ utils/talkerFilters.js
@@ -1,9 +1,9 @@
 function returnTalkerByID(talkersArray, tid) {
-  return talkersArray.find(({ id }) => id === tid);
+  return talkersArray.find(({ id }) => parseInt(id, 10) === parseInt(tid, 10));
 }
 
 function returnArrayDifferentID(talkersArray, tid) {
-  return talkersArray.filter(({ id }) => id !== tid);
+  return talkersArray.filter(({ id }) => parseInt(id, 10) !== parseInt(tid, 10));
 }
 
 module.exports = {
