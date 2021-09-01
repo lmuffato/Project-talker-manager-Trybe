2e7e86801f8ee41e36ac82d00ed7533e0e82ed32
gabrieldeori <deori.pessoal@gmail.com>
2021-09-01 10:52:20 -0300

    FIX LINT: identation

--- middlewares/createTalker.js
+++ middlewares/createTalker.js
@@ -4,28 +4,28 @@ const {
   talkValidator,
 } = require('../utils/createTalkerValidator');
 
-function nameCheck (request, response, next) {
+function nameCheck(request, response, next) {
   const { name } = request.body;
   const validated = nameValidator(name);
-  if ( validated === 0) {
+  if (validated === 0) {
     return next();
   }
   return response.status(400).send(validated);
 }
 
-function ageCheck (request, response, next) {
+function ageCheck(request, response, next) {
   const { age } = request.body;
   const validated = ageValidator(age);
-  if ( validated === 0) {
+  if (validated === 0) {
     return next();
   }
   return response.status(400).send(validated);
 }
 
-function talkCheck (request, response, next) {
+function talkCheck(request, response, next) {
   const { talk } = request.body;
   const validated = talkValidator(talk);
-  if ( validated === 0) {
+  if (validated === 0) {
     return next();
   }
   return response.status(400).send(validated);
