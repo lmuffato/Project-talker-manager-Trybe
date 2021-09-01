6630e984458eeebd87236aa46454cb410ddfc453
gabrieldeori <deori.pessoal@gmail.com>
2021-09-01 13:07:22 -0300

    Adding write.js functions to utils

---
+++ utils/write.js
@@ -0,0 +1,15 @@
+const { writeFile } = require('fs').promises;
+
+async function writeAnObjectIntoAJSONFile(objectToWrite) {
+  try {
+    const readyToWrite = await JSON.stringify(objectToWrite);
+    await writeFile('talker.json', readyToWrite);
+    return 0;
+  } catch (err) {
+    return err;
+  }
+}
+
+module.exports = {
+  writeAnObjectIntoAJSONFile,
+};
