ed5393a3b12ce6f53f396760463e0728a2fa2257
gabrieldeori <deori.pessoal@gmail.com>
2021-09-01 09:22:47 -0300

    Adding try catch to readJsonReturnArray

--- utils/read.js
+++ utils/read.js
@@ -1,9 +1,14 @@
 const fs = require('fs').promises;
 
 async function readJsonReturnArray(file) {
+  try {
     const RAWDATA = await fs.readFile(file);
     const DATA = await JSON.parse(RAWDATA);
     return (DATA);
+  } catch (err) {
+    console.log(err);
+    return [{ message: err }];
+  }
 }
 
 module.exports = {
