f1b5cab12f3e54766e8992966c0789f47fd9723b
gabrieldeori <deori.pessoal@gmail.com>
2021-09-01 16:18:30 -0300

    Add function that returnArrayIncludesTerm

--- utils/talkerFilters.js
+++ utils/talkerFilters.js
@@ -6,7 +6,14 @@ function returnArrayDifferentID(talkersArray, tid) {
   return talkersArray.filter(({ id }) => parseInt(id, 10) !== parseInt(tid, 10));
 }
 
+function returnArrayIncludesTerm(talkersArray, term, key) {
+  return talkersArray.filter((talker) => {
+    return talker[key].includes(term);
+  });
+}
+
 module.exports = {
   returnTalkerByID,
   returnArrayDifferentID,
+  returnArrayIncludesTerm,
 };
