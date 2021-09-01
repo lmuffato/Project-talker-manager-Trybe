b461bfbe6a105be04af16e8de233f27690ea4f66
Joao Nasc <oi@joaonasc.dev>
2021-08-31 21:08:54 -0300

    feat: middleware to token generation

---
+++ middlewares/generate.js
@@ -0,0 +1,9 @@
+const crypto = require('crypto');
+
+module.exports = {
+  generateToken(req, res) {
+    // https://github.com/tryber/sd-010-a-project-talker-manager/blob/7fe006d5f1cca4997964694af17ef83bc50a3def/middlewares/login.js#L29
+    const token = crypto.randomBytes(8).toString('hex');
+    res.status(200).json({ token });
+  },
+};
