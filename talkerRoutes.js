4d5930d2b9f12bc01fb39520df06f04c76e4e00f
Felipe Muller <felipeavmuller@gmail.com>
2021-08-31 22:37:41 -0300

    Desafio 1 - Criado GET talker

---
+++ talkerRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const fs = require('fs').promises;
+const rescue = require('express-rescue');
+
+const router = express.Router();
+
+const getFile = async () => {
+  const result = await fs.readFile('./talker.json');
+
+  return JSON.parse(result);
+};
+
+router.get('/', rescue(async (_req, res) => {
+  const talker = await getFile();
+  if (!talker) return res.status(200).json([]);
+
+  res.status(200).json(talker);
+}));
+
+module.exports = router;
