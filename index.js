5edf6ba4c71336a27feffbfa95b155f1ed5db1ed
Bruno Mendes <mendessbruno@gmail.com>
2021-09-01 14:51:58 -0300

    alterado ordem dos app use no index para testar se passa avaliador github

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+app.use('/talker', talker);
+app.use('/login', login);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
-
-app.use('/talker', talker);
-app.use('/login', login);
 
 app.listen(PORT, () => {
   console.log('Online');
