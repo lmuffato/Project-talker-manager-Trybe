dd92cf26dca471a2695fcf11c6f212b83e9ee0fb
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-08-31 21:19:25 -0300

    Feat: (req-1) return an array with all registered speakers and If there is no registered speaker, the endpoint must return an empty array and the status 200

---
+++ getTalkers.js
@@ -0,0 +1,20 @@
+const { readFile } = require('fs').promises;
+
+// const fileTalkers = './talker.json';
+const HTTP_OK_STATUS = 200;
+
+const readFileTalker = () => readFile('./talker.json', 'utf-8').then((data) => JSON.parse(data));
+
+  // console.log(readFileTalker()); // promise
+
+const getTalkers = async (_req, res) => {
+  const talkers = await readFileTalker();
+
+  // console.log(talkers); // json
+
+  if (talkers === []) return res.status(200).json([]);
+
+  res.status(HTTP_OK_STATUS).json(talkers);
+};
+
+module.exports = getTalkers;
