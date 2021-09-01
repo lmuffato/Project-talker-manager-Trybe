39f63a0826ad9c1dd50f178eea7b32e0c611795c
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-01 15:57:16 -0300

    feat 3: create the middleware function where check if the email is correct and exist

---
+++ loginRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const router = express.Router();
+
+const generateRandomToken = () => crypto.randomBytes(8).toString('hex');
+
+const checkEmail = (req, res, next) => {
+  const { email } = req.body;
+  const checkedEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!checkedEmail) return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+
+  next();
+};
+
+router.post('/login', (req, res) => {
+  const {};
+});
+
+// Para validação de email utilizei a seguinte referência: https://www.w3resource.com/javascript/form/email-validation.php
+// Para saber a existência do Crypto para geração de número aleatórios foi no: https://stackoverflow.com/questions/8532406/create-a-random-token-in-javascript-based-on-user-details
