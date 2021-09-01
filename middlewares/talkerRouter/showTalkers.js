9df7af74add3fc4b6e61757ce4a2016da017c3c4
Micaela Alves <micaela.alves93@gmail.com>
2021-09-01 17:15:25 -0300

    fix nome das funcçoes

--- middlewares/talkerRouter/showTalkers.js
+++ middlewares/talkerRouter/showTalkers.js
@@ -2,9 +2,9 @@ const getTalkers = require('../../fs-utils/getTalkers');
 
 const HTTP_OK_STATUS = 200;
 
-const talker = async (req, res) => {
+const showTalkers = async (_req, res) => {
   const talkers = await getTalkers();
   return res.status(HTTP_OK_STATUS).json(talkers);
 };
 
-module.exports = talker;
+module.exports = showTalkers;
--- middlewares/talkerRouter/talkerRouter.js
+++ middlewares/talkerRouter/talkerRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const { showTalkers } = require('./showTalkers');
+const showTalkers = require('./showTalkers');
 
 /* router.get('/talker/search', );
 
