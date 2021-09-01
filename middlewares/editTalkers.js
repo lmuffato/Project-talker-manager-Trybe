92b41abf088da385ace217a075ce900b50421335
gabrieldeori <deori.pessoal@gmail.com>
2021-09-01 15:22:11 -0300

    Making id a Number()

--- middlewares/editTalkers.js
+++ middlewares/editTalkers.js
@@ -8,7 +8,7 @@ async function editTalkers(request, response) {
     const talkerDBPath = './talker.json';
     const talkerDB = await readJsonReturnArray(talkerDBPath);
     const newTalkersList = returnArrayDifferentID(talkerDB);
-    const talkerToAdd = { name, age, talk, id };
+    const talkerToAdd = { name, age, talk, id: Number(id) };
     newTalkersList.push(talkerToAdd);
     await writeAnObjectIntoAJSONFile(talkerDB);
     return response.status(200).json(talkerToAdd);
