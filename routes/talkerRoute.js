de657ceaccd5efb778a9db8e62c3ed2340352bca
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-08-31 22:51:34 -0300

    req 2 done

--- routes/talkerRoute.js
+++ routes/talkerRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const getTalkers = require('../services/getTalkers');
+const getTalkersById = require('../services/getTalkerById');
 
 const router = express.Router();
 
@@ -7,6 +8,18 @@ router.get('/', async (_req, res) => {
   const sendJson = await getTalkers();
   console.log(sendJson);
   return res.status(200).json(sendJson);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await getTalkersById(id);
+  console.log(talker);
+  if (!talker) {
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+  return res.status(200).json(talker);
 });
 
 module.exports = router;
