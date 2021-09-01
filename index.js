8d9641a950bfcc413851d0b24a8521b83f9a7edd
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-01 15:19:24 -0300

    fix 1: change the response of a variable to a function, because i need to re-use this operation of read archives with fs

--- index.js
+++ index.js
@@ -10,15 +10,28 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const getTalkers = async () => fs.readFile(arrayPeople, 'utf-8').then((data) => JSON.parse(data));
+
 // Requisito 1 - Crie o endpoint GET /talker
 
 app.get('/talker', async (_req, res) => {
-  const arrayToSend = await fs.readFile(arrayPeople, 'utf-8').then((data) => JSON.parse(data));
+  const arrayToSend = await getTalkers();
 
   // if (!arrayToSend) return res.status(HTTP_OK_STATUS).json([]);
 
   res.status(HTTP_OK_STATUS).json(arrayToSend);
 });
+
+// ----------------------------------------
+
+// Requisito 2 - Crie o endpoint GET /talker/:id
+
+// app.get('/talker/:id', (req, res) => {
+//   const { id } = req.params;
+
+// });
+
+// ----------------------------------------
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
