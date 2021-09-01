9a9d594e3591e8c660b999a91a95332066f32ece
biazidioti <bzferreira.bio@gmail.com>
2021-09-01 17:16:48 -0300

    PASSOU!

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ app.get('/talker', async (_request, response) => {
   response.status(200).json(parsedTalkers);
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await fs.readFile('./talker.json');
+  const parsedTalkers = JSON.parse(talkers);
+  
+  const talker = parsedTalkers.find((t) => t.id === parseInt(id, 10));
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(200).json(talker);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
