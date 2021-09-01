10190e7f8c57564c1140bf55738d6260a033460a
gabrieldeori <deori.pessoal@gmail.com>
2021-09-01 15:20:02 -0300

    Minnor Changes in Write

--- utils/write.js
+++ utils/write.js
@@ -3,7 +3,7 @@ const { writeFile } = require('fs').promises;
 async function writeAnObjectIntoAJSONFile(objectToWrite) {
   try {
     const readyToWrite = await JSON.stringify(objectToWrite);
-    await writeFile('talker.json', readyToWrite);
+    await writeFile('./talker.json', readyToWrite);
     return 0;
   } catch (err) {
     return err;
