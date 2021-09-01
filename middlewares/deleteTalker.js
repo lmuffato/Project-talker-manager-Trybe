2d4726677fb3aac3200117748d5e19a1807adef2
gabrieldeori <deori.pessoal@gmail.com>
2021-09-01 15:41:43 -0300

    Add a middleware to deleteTalker.js

---
+++ middlewares/deleteTalker.js
@@ -0,0 +1,10 @@
+function deleteTalker(request, response) {
+  const { id } = request.params;
+  const talkerDBPath = './talker.json';
+  const talkerDB = await readJsonReturnArray(talkerDBPath);
+  const newTalkersList = returnArrayDifferentID(talkerDB, id);
+  await writeAnObjectIntoAJSONFile(newTalkersList);
+  return response.status(200).json(newTalkersList);
+}
+
+module.exports = deleteTalker;
