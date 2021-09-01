71aabebbc4c1a0f4f2e34234bf263065fc44acbc
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-09-01 17:46:31 -0300

    refactor: req1 - endpoint get /talker - change names const status http and const of content fs.readfile

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -4,14 +4,14 @@ const router = express.Router();
 
 const readContentFile = require('../utils/readContentFile');
 
-const HTTP_OK_STATUS = 200;
-const HTTP_NOT_FOUND_STATUS = 404;
+const HTTP_OK = 200;
+const HTTP_NOT_FOUND = 404;
 
 const PATH = './talker.json';
 
 router.get('/', async (_req, res) => {
-  const content = await readContentFile(PATH) || [];
-  res.status(HTTP_OK_STATUS).json(content);
+  const talkers = await readContentFile(PATH) || [];
+  res.status(HTTP_OK).json(talkers);
 });
 
 router.get('/:id', async (req, res) => {
@@ -22,11 +22,11 @@ router.get('/:id', async (req, res) => {
 
   if (!talkerFilteredById) {
     return res
-    .status(HTTP_NOT_FOUND_STATUS)
+    .status(HTTP_NOT_FOUND)
     .json({ message: 'Pessoa palestrante não encontrada' }); 
   }
 
-  res.status(HTTP_OK_STATUS).json(talkerFilteredById);
+  res.status(HTTP_OK).json(talkerFilteredById);
 });
 
 module.exports = router;
