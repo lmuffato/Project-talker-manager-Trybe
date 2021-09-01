203eae29bceb52a22cd7654be9e49eb919b3206f
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-01 15:10:43 -0300

    feat 1: Create a route to "/talker" where show a list of people if is populated or an empty array if not

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+
+const arrayPeople = './talker.json';
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+// Requisito 1 - Crie o endpoint GET /talker
+
+app.get('/talker', async (_req, res) => {
+  const arrayToSend = await fs.readFile(arrayPeople, 'utf-8').then((data) => JSON.parse(data));
+
+  // if (!arrayToSend) return res.status(HTTP_OK_STATUS).json([]);
+
+  res.status(HTTP_OK_STATUS).json(arrayToSend);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
