9112d84b351a6fce82c3694d3f334dbe554e4d5c
gabrieldeori <deori.pessoal@gmail.com>
2021-09-01 10:54:27 -0300

    Add to utils validateRate.js

---
+++ utils/validateRate.js
@@ -0,0 +1,8 @@
+function validateRate() {
+  if (rate < 1 || rate > 5) {
+    return { message: 'O campo "rate" deve ser um inteiro de 1 à 5' };
+  }
+  return 0;
+}
+
+module.exports = validateRate;
