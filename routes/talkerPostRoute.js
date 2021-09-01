746c52ff179ffb2b70c23e9c97b011dba32f32d7
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-09-01 20:00:37 -0300

    requisito 4 funcionando

--- routes/talkerPostRoute.js
+++ routes/talkerPostRoute.js
@@ -4,6 +4,8 @@ const filePath = ('talker.json');
 
 const saveTalkers = (talkers) => fs.writeFileSync(filePath, JSON.stringify(talkers, null, '\t'));
 
+const { readFile, writeFile } = require('fs').promises;
+
 const noToken = {
     message: 'Token não encontrado',
   };
@@ -69,9 +71,12 @@ const passwordValidation = (req, res, next) => {
   next();
 };
 
-// const validToken = (res, authorization) => {
-//     if (authorization.length !== 16) return res.status(400)
-// };
+const tokenValidation = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+  if (authorization.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+  next();
+};
 
 // Dica do token vista no repositório do Iago Ferreira
 const crypto = require('crypto');
@@ -88,48 +93,97 @@ const deleteTalker = (req, res) => {
     const defAnswer = answer.find(({ id }) => id !== parseInt(req.params.id, 10));
     saveTalkers(defAnswer);
     const { authorization } = req.headers;
-    if (!authorization) return res.status(401).send(noToken);
-    if (authorization.length !== 16) return res.status(401).send(invalidToken);
+    if (!authorization) return res.status(401).json(noToken);
+    if (authorization.length !== 16) return res.status(401).json(invalidToken);
     const message = { message: 'Pessoa palestrante deletada com sucesso' };
     return res.status(200).send(message);
 };
 
-const noName = {
-    message: 'O campo "name" é obrigatório',
-};
-
-const shortName = {
-    message: 'O "name" deve ter pelo menos 3 caracteres',
+  const registeringTalker = async (req, res) => {
+    const { name, age, talk } = req.body;
+    let talkers;
+    try {
+    talkers = JSON.parse(await readFile(filePath, 'utf-8'));
+    } catch (error) {
+    console.error(error);
+    }
+    const newTalker = {
+      name,
+      age,
+      talk,
+      id: talkers.length + 1,
+    };
+    talkers.push(newTalker);
+    await writeFile(filePath, JSON.stringify(talkers));
+    return res.status(201).json(newTalker);
+  };
+  
+  const invalidRate = { message: 'O campo "rate" deve ser um inteiro de 1 à 5' };
+  
+  const validatingRate = (req, res, next) => {
+    const { talk: { rate } } = req.body;
+    if (rate < 1 || rate > 5) {
+      return res.status(400).json(invalidRate);
+    }
+    next();
+  };
+  
+  const invalidDate = { message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' };
+  
+    const validatingDate = (req, res, next) => {
+      const { talk: { watchedAt } } = req.body;
+      const regex = /^\d{2}\/\d{2}\/\d{4}$/;
+      if (!regex.test(watchedAt)) {
+        return res.status(400).json(invalidDate);
+      }
+    next();
+    };
+  
+  const invalidTalk = {
+    message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
   };
 
-const validAge = {
-    message: 'O campo "age" é obrigatório',
+    const validatingTalk = (req, res, next) => {
+      const { talk } = req.body;
+        if (!talk || !talk.watchedAt || (!talk.rate && talk.rate !== 0)) {
+        return res.status(400).send(invalidTalk);
+      }
+      next();
+    };
+    
+    const validAge = {
+      message: 'O campo "age" é obrigatório',
+    };
+  
+  const minorAge = {
+      message: 'A pessoa palestrante deve ser maior de idade',
+    };
+  
+    const validatingAge = (req, res, next) => {
+      const { age } = req.body;
+      if (!age) return res.status(400).json(validAge);
+      if (age < 18) return res.status(400).json(minorAge);
+      next();
+    };
+  
+    const noName = {
+      message: 'O campo "name" é obrigatório',
   };
-
-const minorAge = {
-    message: 'A pessoa palestrante deve ser maior de idade',
+  
+  const shortName = {
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    };
+  const validatingName = (req, res, next) => {
+      const { name } = req.body;
+      if (!name) return res.status(400).json(noName);
+      if (name.length < 3) return res.status(400).json(shortName);
+      next();
   };
-
-const checkAge = (age, res) => {
-  if (!age) return res.status(400).send(validAge);
-  if (age < 18) return res.status(400).send(minorAge);
-};
-
-const addTalker = (req, res) => {
-    const { authorization } = req.headers;
-    const { name, age } = req.body;
-    if (!authorization) return res.status(401).send(noToken);
-    if (authorization.length !== 16) return res.status(401).send(invalidToken);
-    if (!name) return res.status(400).send(noName);
-    if (name.length < 3) return res.status(400).send(shortName);
-    checkAge(age, res);
-};
 
 const talkerPostRoute = (app) => {
     app.route('/talker')
-    .post((req, res) => {
-        addTalker(req, res);
-    });
+    .post(tokenValidation, validatingName, validatingAge, validatingTalk,
+      validatingDate, validatingRate, registeringTalker);
     app.route('/talker/:id')
     .delete((req, res) => {
       deleteTalker(req, res);
