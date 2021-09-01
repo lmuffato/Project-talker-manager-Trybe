2cc981b797ad96a642444963b43db5460d1155b7
gabrieldeori <deori.pessoal@gmail.com>
2021-09-01 16:25:00 -0300

    Add all logic of searchTerm

--- middlewares/searchTerm.js
+++ middlewares/searchTerm.js
@@ -1,6 +1,18 @@
+const { returnArrayIncludesTerm } = require('../utils/talkerFilters');
+
 function searchTerm(request, response) {
-  const query = request.query;
-  console.log(query);
+  try {
+    const { q } = request.query;
+    if (q) {
+      const talkerDBPath = './talker.json';
+      const talkerDB = await readJsonReturnArray(talkerDBPath);
+      const talkerKey = 'name';
+      const arrayOfTalkersThatIncludeTerm = returnArrayIncludesTerm(talkerDB, q, talkerKey);
+      return response.status(200).json(arrayOfTalkersThatIncludeTerm);
+    }
+  } catch (err) {
+    return { ERROR: err };
+  }
 }
 
 module.exports = searchTerm;
