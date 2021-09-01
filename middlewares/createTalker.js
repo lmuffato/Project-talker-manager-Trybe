1f85065f858f24982685dcddfa80e46d258109c4
AndreBarroso <andre_barroso17@hotmail.com>
2021-09-01 11:23:28 -0300

    feat: requisito 4.2 - criado midleware de validacao de token

--- middlewares/createTalker.js
+++ middlewares/createTalker.js
@@ -1,7 +1,7 @@
 const readFileTalker = require('../services/readFileTalker.js');
 const writeInTalker = require('../services/writeInTalker.js');
 
-async function createTalker(req, res, next) {
+async function createTalker(req, res) {
   const { name, age, watchedAt, rate } = req.body;
   const newData = {
     name,
@@ -14,7 +14,8 @@ async function createTalker(req, res, next) {
 
   const originalDatas = await readFileTalker();
   const updateDatas = originalDatas.push(newData);
-  writeInTalker(updateDatas);
+  await writeInTalker(updateDatas);
+  res.status(201).json(newData);
 }
 
 module.exports = createTalker;
