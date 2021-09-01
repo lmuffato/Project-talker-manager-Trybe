d984b4d4c8b51cdf4a704626ccb71a6c9f068cb7
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-09-01 09:50:36 -0400

    feat: req 1 - endpoint GET /talker

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fsAsync = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+async function showTalkers(_req, res) {
+  try {
+    const resp = await fsAsync.readFile('./talker.json', 'utf-8');
+    if (!resp) return res.status(500).json({ message: 'no data found' });
+    res.status(200).json(JSON.parse(resp));
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+}
+
+app.get('/talker', showTalkers);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
