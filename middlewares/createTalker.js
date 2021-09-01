97936a32002bbbe3cdd1e16e056199216f9e9c9b
AndreBarroso <andre_barroso17@hotmail.com>
2021-09-01 13:30:44 -0300

    feat: requisito 4

--- middlewares/createTalker.js
+++ middlewares/createTalker.js
@@ -3,14 +3,14 @@ const writeInTalker = require('../services/writeInTalker.js');
 
 async function createTalker(req, res) {
   const { name, age, talk } = req.body;
+  const originalDatas = await readFileTalker();
   const newData = {
+    id: originalDatas.length + 1,
     name,
     age, 
     talk,
   };
-
-  const originalDatas = await readFileTalker();
-  const updateDatas = originalDatas.push(newData);
+  const updateDatas = [...originalDatas, newData];
   await writeInTalker(updateDatas);
   res.status(201).json(newData);
 }
--- services/writeInTalker.js
+++ services/writeInTalker.js
@@ -1,10 +1,11 @@
 const { writeFile } = require('fs').promises;
 
 async function writeInTalker(input) {
+  // console.log('meu input: ', input);
   const intputJSON = JSON.stringify(input);
   try {
     await writeFile('./talker.json', intputJSON);
-    console.log('escreveu', intputJSON);
+    // console.log('escreveu', intputJSON);
   } catch (err) {
     console.err(`Erro ao escrever no arquivo testa: ${err.message}`);
   }
