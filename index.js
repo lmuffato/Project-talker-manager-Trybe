c990bc1206e3808895efe7ad04dacb41a4a94f54
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-09-01 16:40:30 -0300

    Req 3 teste

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const HTTP_OK_STATUS = 200;
 const HTTP_NOT_OK_STATUS = 404;
 const PORT = '3000';
 
+const crypto = require('crypto');
+const { validateEmail, validatePassword } = require('./validations/validationsPassAndEmail');
+
+function generateToken() {
+  return crypto.randomBytes(16).toString('hex');
+  // 1 Byte usa 2 caracteres no hexadecimal, por isso é normal vermos 32 bytes sendo representados como 64 bytes.
+}
+
 app.get('/talker/:id', (req, res) => {
   const { id } = req.params;
   const talkerData = fs.readFileSync('talker.json', 'utf-8');
@@ -29,6 +37,15 @@ app.get('/talker', (req, res) => {
     res.status(HTTP_OK_STATUS).json(JSON.parse([]));
   }
 });
+
+app.post('/login', validateEmail, validatePassword, (req, res) => {
+  const cryptoToken = generateToken();
+  return res.status(HTTP_OK_STATUS).json({ cryptoToken });
+});
+
+// https://stackoverflow.com/questions/17604866/difference-between-readfile-and-readfilesync
+// https://www.geeksforgeeks.org/node-js-crypto-randombytes-method/
+// https://stackoverflow.com/questions/55104802/nodejs-crypto-randombytes-to-string-hex-doubling-size
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
---
+++ validations/validationsPassAndEmail.js
@@ -0,0 +1,28 @@
+function validateEmail(req, res, next) {
+  const { email } = req.body;
+  const regex = /^w+@[a-zA-Z_]+?.[a-zA-Z]{2,3}$/;
+  
+  if (!email) {
+    return res.status(400).json({ message: 'O campo email deve ser obrigatorio' });
+  } if (!email.match(regex)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+}
+
+function validatePassword(req, res, next) {
+  const password = req.body;
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  } if (password.lenght < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+}
+
+module.exports = {
+  validateEmail,
+  validatePassword,
+};
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/match
+// https://stackoverflow.com/questions/940577/javascript-regular-expression-email-validation
