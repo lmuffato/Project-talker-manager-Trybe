f2473f7bb8f57f0effd973a086d239acf2abc53c
Patrick Morais <ppternunes@gmail.com>
2021-09-01 15:32:40 -0300

    req 1

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const fs = require('fs').promises;
+
+const readFile = async () => {
+  const file = await fs.readFile('./talker.json', 'utf-8');
+  const dataTalkers = JSON.parse(file);
+  return dataTalkers;
+};
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -14,4 +22,10 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talkers', async (_req, res) => {
+  const dataTalkers = await readFile();
+  if (dataTalkers.length > 0) return res.status(HTTP_OK_STATUS).json(dataTalkers);
+  return res.status(HTTP_OK_STATUS).json([]);
 });
