1ab66e709bbbc5f40bbb7b64b14c1cfb1853eef0
Felipe Muller <felipeavmuller@gmail.com>
2021-08-31 23:42:55 -0300

    Desafio 3 - Criado loginRouter com validacoes email/senha e POST login

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 const talkerRoutes = require('./talkerRoutes');
+const loginRouter = require('./loginRouter');
 
 app.use('/talker', talkerRoutes);
+app.use('/login', loginRouter);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
