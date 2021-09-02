8ffbae09540048885474f6f86e6520c62f6a41c0
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-09-02 13:29:17 -0300

    :sparkles: Requirement 04
    4 - Crie o endpoint POST /talker

--- talkerRouter.js
+++ talkerRouter.js
@@ -3,6 +3,79 @@ const express = require('express');
 
 const router = express.Router();
 
+const verificaToken = (token) => {
+  if (!token) {
+    const err = new Error('Token não encontrado'); 
+    err.code = 401;
+    throw err;
+  }
+
+  if (token === '' || token.length < 16) {
+    const err = new Error('Token inválido'); 
+    err.code = 401;
+    throw err;
+  }
+};
+
+const verificaNome = (name) => {
+  if (!name || name === '') {
+    const err = new Error('O campo "name" é obrigatório');
+    err.code = 400;
+    throw err;
+  }
+
+  if (name.length < 3) {
+    const err = new Error('O "name" deve ter pelo menos 3 caracteres');
+    err.code = 400;
+    throw err;
+  }
+};
+
+const verificaIdade = (age) => {
+  if (!age) {
+    const err = new Error('O campo "age" é obrigatório');
+    err.code = 400;
+    throw err;
+  }
+
+  if (age < 18) {
+    const err = new Error('A pessoa palestrante deve ser maior de idade');
+    err.code = 400;
+    throw err;
+  }
+};
+
+const verificaWatchedAt = (watchedAt) => {
+  if (!/^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/.test(watchedAt)) {
+    const err = new Error('O campo "watchedAt" deve ter o formato "dd/mm/aaaa"');
+    err.code = 400;
+    throw err;
+  }
+};
+
+const verificaRate = (rate) => {
+  if (rate < 1 || rate > 5) {
+    const err = new Error('O campo "rate" deve ser um inteiro de 1 à 5');
+    err.code = 400;
+    throw err;
+  }
+};
+
+const verificaTalk = (talk) => {
+  if (!talk || !talk.rate || !talk.watchedAt) {
+    const err = new Error(
+      'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    );
+    err.code = 400;
+    throw err;
+  }
+
+  verificaRate(talk.rate);
+  verificaWatchedAt(talk.watchedAt);
+};
+
+const getMaxId = (data) => data.reduce((acc, cur) => (cur.id > acc ? cur.id : acc), 0);
+
 router.get('/', (_req, res) => {
   fs.readFile('./talker.json')
     .then((result) => res.status(200).json(JSON.parse(result)))
@@ -21,6 +94,32 @@ router.get('/:id', (req, res) => {
       }
     })
     .catch((err) => res.status(404).json({ message: err.message }));
+});
+
+router.post('/', (req, res) => {
+  const { name, age, talk } = req.body;
+  fs.readFile('./talker.json')
+    .then((result) => JSON.parse(result))
+    .then((data) => {
+      const { authorization } = req.headers;
+      verificaToken(authorization);
+      verificaNome(name);
+      verificaIdade(age);
+      verificaTalk(talk);
+      const newEntry = { id: getMaxId(data) + 1, name, age, talk };
+      fs.writeFile('./talker.json', JSON.stringify([...data, newEntry]))
+        .then(() => console.log('ok'));
+      res.status(201).json(newEntry);
+    })
+    .catch((err) => res.status(err.code).json({ message: err.message }));
+});
+
+router.use((err, _req, res, _next) => {
+  if (err.code && err.status) {
+    return res.status(err.status).json({ message: err.message, code: err.code });
+  }
+
+  return res.status(500).json({ message: err.message });
 });
 
 module.exports = router;
