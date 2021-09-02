6f9849a05b5c36a29832e2af542566cf048c9588
Micaela Alves <micaela.alves93@gmail.com>
2021-09-01 23:41:00 -0300

    Req 5 - middleware que  editar um talker

---
+++ middlewares/editTalker.js
@@ -0,0 +1,24 @@
+const editTalkers = require('../fs-utils/editTalkers');
+const getTalkers = require('../fs-utils/getTalkers');
+
+const HTTP_OK_STATUS = 200;
+
+const editTalker = async (req, res) => {
+  const talkersList = await getTalkers();
+  const { name, age, talk } = req.body;
+  const { id } = req.params;
+  const talkerChanged = { 
+    name,
+    age,
+    id: Number(id),
+    talk };
+
+  talkersList.filter((talker) => Number(talker.id) !== Number(id));
+  talkersList.push(talkerChanged);
+  
+  await editTalkers(talkersList);
+  
+  return res.status(HTTP_OK_STATUS).json(talkerChanged);
+};
+
+module.exports = editTalker;
