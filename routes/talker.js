5c90c429f281ea42aa24632b4dda39539d40e44b
Joao Nasc <oi@joaonasc.dev>
2021-09-01 21:13:21 -0300

    feat: route to create talker

--- routes/talker.js
+++ routes/talker.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
-const { getTalkers, getTalker } = require('../controllers/talker');
+const { getTalkers, getTalker, postTalker } = require('../controllers/talker');
+const { tokenAuthentication } = require('../middlewares/authentication');
+const { validateName, validateAge, validateTalk } = require('../middlewares/validation');
 
 const routes = Router();
 
 routes.get('/', getTalkers);
 routes.get('/:id', getTalker);
+routes.post('/', tokenAuthentication, validateName, validateAge, validateTalk, postTalker);
 
 module.exports = routes;
