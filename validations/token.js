1271e7f0958a44c9915ff5a2499ba6d8a29f652f
Lucas Lara <lucasml@live.com>
2021-09-02 13:22:52 -0300

    token pattern validation use

--- validations/token.js
+++ validations/token.js
@@ -2,11 +2,11 @@ const { StatusCodes } = require('http-status-codes');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  // const isValid = /^[\w]{16}$/i;
+  const isValid = /^[\w]{16}$/i;
   if (!authorization) {
     return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Token não encontrado' });
   }
-  if (authorization.length !== 16) {
+  if (!isValid.test(authorization)) {
     return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Token inválido' });
   }
   next();
