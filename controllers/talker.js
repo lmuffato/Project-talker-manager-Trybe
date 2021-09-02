28b8429102cacad38fe8fdf61ae9c029a6c2f45e
Joao Nasc <oi@joaonasc.dev>
2021-09-01 21:13:00 -0300

    feat: post method for adding talker

--- controllers/talker.js
+++ controllers/talker.js
@@ -1,3 +1,4 @@
+const fs = require('fs').promises;
 const readFile = require('../utils/readFile');
 
 const getAllTalkers = async () => readFile('./talker.json');
@@ -21,6 +22,26 @@ module.exports = {
       res.status(200).json(talker);
     } catch (error) {
       res.status(404).json({ message: error.message });
+    }
+  },
+
+  async postTalker(req, res) {
+    try {
+      let highestId = 0;
+      const talkers = await getAllTalkers();
+      talkers.forEach((talker) => {
+        if (talker.id > highestId) highestId = talker.id;
+      });
+      const id = highestId + 1;
+      const talker = {
+        ...req.body,
+        id,
+      };
+      talkers.push(talker);
+      await fs.writeFile('./talker.json', JSON.stringify(talkers));
+      res.status(201).json(talker);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
     }
   },
 };
