35c0449136f0fd53fa209544e9822f4946b74e26
Joao Nasc <oi@joaonasc.dev>
2021-09-01 21:08:08 -0300

    fix: destructure authorization instead of token

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,10 +1,10 @@
 module.exports = {
   tokenAuthentication(req, res, next) {
-    const { token } = req.headers;
+    const { authorization } = req.headers;
 
     try {
-      if (!token) throw new Error('Token não encontrado');
-      const validToken = typeof token === 'string' && token.length === 16;
+      if (!authorization) throw new Error('Token não encontrado');
+      const validToken = typeof authorization === 'string' && authorization.length === 16;
       if (!validToken) throw new Error('Token inválido');
     } catch (error) {
       res.status(401).json({ message: error.message });
