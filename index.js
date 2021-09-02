bec9aa5c59e0956a196c1572c04a8479773ede2d
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-02 12:09:51 -0300

    feat 7: create the endpoint GET with Query String for search terms on talkers list, and re-arrange the order of endpoints

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ const PORT = '3000';
 
 const getTalkers = async () => fs.readFile(arrayPeople, 'utf-8').then((data) => JSON.parse(data));
 
+// Requisito 7 - Crie o endpoint GET /talker/search?q=searchTerm
+
+app.get('/talker/search', validateToken, async (req, res) => {
+  const { q } = req.query;
+  const talkersArray = await getTalkers();
+  const filterTalkers = talkersArray.filter((talker) => talker.name.includes(q));
+
+  if (!q) return res.status(202).json(talkersArray);
+
+  res.status(HTTP_OK_STATUS).json(filterTalkers);
+});
+
+// ----------------------------------------
+
 // Requisito 1 - Crie o endpoint GET /talker
 
 app.get('/talker', async (_req, res) => {
@@ -25,20 +39,6 @@ app.get('/talker', async (_req, res) => {
   // if (!arrayToSend) return res.status(HTTP_OK_STATUS).json([]);
 
   res.status(HTTP_OK_STATUS).json(arrayToSend);
-});
-
-// ----------------------------------------
-
-// Requisito 2 - Crie o endpoint GET /talker/:id
-
-app.get('/talker/:id', async (req, res) => {
-  const { id } = req.params;
-  const talkersArray = await getTalkers();
-  const filterTalker = talkersArray.find((talker) => talker.id === Number(id));
-
-  if (!filterTalker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-
-  res.status(HTTP_OK_STATUS).json(filterTalker);
 });
 
 // ----------------------------------------
@@ -67,6 +67,20 @@ app.post('/talker', validateToken, talkerValidation, async (req, res) => {
   await fs.writeFile('./talker.json', JSON.stringify(arrayToSend));
 
   res.status(201).json(aceptedPerson);
+});
+
+// ----------------------------------------
+
+// Requisito 2 - Crie o endpoint GET /talker/:id
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkersArray = await getTalkers();
+  const filterTalker = talkersArray.find((talker) => talker.id === Number(id));
+
+  if (!filterTalker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(HTTP_OK_STATUS).json(filterTalker);
 });
 
 // ----------------------------------------
