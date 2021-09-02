48f9e5ee2bfe3fbd56623a6b201d3ab3c7e3d277
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-09-01 22:14:00 -0300

    Requisito 3: 3 item: campo password validado

--- middlewares/loginValidations.js
+++ middlewares/loginValidations.js
@@ -24,4 +24,17 @@ const verifiedEmail = (req, res, next) => {
     next();
 };
 
-module.exports = { verifiedToken, verifiedEmail };
+const verifiedPassword = (req, res, next) => {
+    const { password } = req.body;
+
+    if (!password) {
+        return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    }
+
+    if (password.length < 6) {
+        return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    }
+    next();
+};
+
+module.exports = { verifiedToken, verifiedEmail, verifiedPassword };
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 const rescue = require('express-rescue');
-const { verifiedToken, verifiedEmail } = require('../middlewares/loginValidations');
+const { 
+    verifiedToken, 
+    verifiedEmail, 
+    verifiedPassword } = require('../middlewares/loginValidations');
 
-router.post('/', verifiedToken, verifiedEmail, rescue((_req, res) => 
+router.post('/', verifiedToken, verifiedEmail, verifiedPassword, rescue((_req, res) => 
     res.status(200).json({ token: '7mqaVRXJSp886CGr' })));
 
 module.exports = router;
