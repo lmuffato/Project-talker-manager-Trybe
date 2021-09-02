1f7010e96ed12ee9c2d3eabb1d6c2b8f524fe405
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-09-01 21:00:17 -0300

    req 3 done

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const validateEmail = require('../middlewares/emailAuthMiddle');
+const validatePassword = require('../middlewares/passwordAuthMiddle');
+const tokenGeneretor = require('../services/tokenGenerator');
 
 const router = express.Router();
+
+router.post('/', validateEmail, validatePassword, (req, res) => {
+  const token = tokenGeneretor();
+  return res.status(200).json({ token });
+});
 
 module.exports = router;
