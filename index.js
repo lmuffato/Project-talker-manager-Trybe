d808523b14df23c2194cc5860cbc4ede79ef16dc
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-09-02 19:40:34 -0300

    requisito 1 e 2

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+// const crypto = require('crypto');
 
-const { validateToken, validateEmail, validatePassword } = require('./validation.js');
+// crypto.randomBytes(16).toString('hex');
+
+// const { validateToken, validateEmail, validatePassword } = require('./validation.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,9 +16,8 @@ const PORT = '3000';
 // requisito 1
 app.get('/talker', async (_req, res) => {
   const data = await fs.readFile('./talker.json', 'utf8');
-
   const talker = JSON.parse(data);
-  console.log(talker);
+  // o readFile não sabe qual tipo de arquivo está lendo então preciso transformar esse dado em json
 
   res.status(HTTP_OK_STATUS).json(talker);
 });
@@ -25,29 +27,27 @@ app.get('/talker/:id', async (req, res) => {
   const { id } = req.params.id;
   
   const data = await fs.readFile('./talker.json', 'utf8');
-  const dataId = data.find((item) => item.id === Number(id));
+  const talker = JSON.parse(data);
 
-  if (!dataId) {
-    return res.status(404).json({ message: 'Book not found!' });
+  const talkerId = talker.find((item) => item.id === Number(id));
+  console.log(talkerId);
+
+  if (!talkerId) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
 
-  res.status(HTTP_OK_STATUS).json(dataId);
+  res.status(HTTP_OK_STATUS).json(talkerId);
 });
 
 // requisito 3
-app.post('/login', validateToken, validateEmail, validatePassword, (req, res) => {
+// app.post('/login', validateEmail, validatePassword, (req, res) => {
   
-});
+// });
 
-// requisito 4
-app.post('/login', validateToken, validateEmail, validatePassword, (req, res) => {
+// // requisito 4
+// app.post('/login', validateToken, validateEmail, validatePassword, (req, res) => {
   
-});
-
-
-
-
-
+// });
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
