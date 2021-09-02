d76aca25cbc2bc13272b6ab7f596059791b7a35a
Maria Baeta <m.martinsdg@gmail.com>
2021-09-02 11:25:25 -0300

    feat: :sparkles: Requisito 5 -  Edit Talker

--- src/Validations/validations.js
+++ src/Validations/validations.js
@@ -44,7 +44,7 @@ const isValidAge = (req, res, next) => {
 
 const isValidTalk = (req, res, next) => {
   const { talk } = req.body;
-  if (!talk || !talk.watchedAt || !talk.rate) {
+  if (!talk || !talk.watchedAt || (!talk.rate && talk.rate !== 0)) {
     return res
       .status(400).json({
         message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
