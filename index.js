a8d9378d8ab0e419716b49b1995974f994f316f9
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-09-02 09:32:49 -0300

    feat: requisito 3

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// cria endpoint /talker
 app.get('/talker', async (_request, response) => {
   const talker = await fs.readFile('./talker.json', 'utf8');
   const result = await JSON.parse(talker);
   response.status(200).json(result);
 });
 
+// cria endpoint /talker/id que retorna o palestrante pelo id
 app.get('/talker/:id', async (request, response) => {
   const { id } = request.params;
   const talker = await fs.readFile('./talker.json', 'utf-8');
@@ -32,15 +34,52 @@ app.get('/talker/:id', async (request, response) => {
   response.status(200).json(pessoasPalestrantes);
 });
 
-const SECRET = 'xablau';
+// cria endpoint /login
+const checarEmail = (req, res, next) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  next();
+};
+const validarEmail = (req, res, next) => {
+  const { email } = req.body;
+  const emailValido = /\w+@\w+.\w+/g;
+  if (emailValido.test(email) === false) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+};
+const checarSenha = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  next();
+};
 
-app.post('/login', (request, response) => {
-  if (request.body.email === 'email@email.com'
-   && request.body.password === '123456') {
-    const token = response.sign({ userId: 1 }, SECRET, { expiresIn: 300 });
-    response.json({ auth: true, token });
-   }
-   response.status(401).end();
+const validarSenha = (req, res, next) => {
+  const { password } = req.body;
+  const senhaValida = /^.{6,}$/g;
+  if (senhaValida.test(password) === false) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
+// gerador de token https://qastack.com.br/programming/1349404/generate-random-string-characters-in-javascript
+const randomToken = () => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const characterLength = characters.length;
+  for (let i = 0; i < 16; i += 1) {
+    result += characters.charAt(Math.floor(Math.random() * characterLength));
+  }
+  return result;
+};
+
+app.post('/login', checarEmail, validarEmail, checarSenha, validarSenha, (_req, res) => {
+  res.status(200).json({ token: randomToken() });
 });
 
 app.listen(PORT, () => {
