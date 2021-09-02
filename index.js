98cb2d660c05be318b644d9089956e7338a2ba26
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-09-02 19:22:49 -0300

    requisito 1

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+
+const { validateToken, validateEmail, validatePassword } = require('./validation.js');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+// requisito 1
+app.get('/talker', async (_req, res) => {
+  const data = await fs.readFile('./talker.json', 'utf8');
+
+  const talker = JSON.parse(data);
+  console.log(talker);
+
+  res.status(HTTP_OK_STATUS).json(talker);
+});
+
+// requisito 2
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params.id;
+  
+  const data = await fs.readFile('./talker.json', 'utf8');
+  const dataId = data.find((item) => item.id === Number(id));
+
+  if (!dataId) {
+    return res.status(404).json({ message: 'Book not found!' });
+  }
+
+  res.status(HTTP_OK_STATUS).json(dataId);
+});
+
+// requisito 3
+app.post('/login', validateToken, validateEmail, validatePassword, (req, res) => {
+  
+});
+
+// requisito 4
+app.post('/login', validateToken, validateEmail, validatePassword, (req, res) => {
+  
+});
+
+
+
+
+
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
---
+++ validation.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+const validateToken = (req, res, next) => {
+ 
+  next();
+};
+
+function EmailIsValid(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+  const valid = EmailIsValid(email);
+
+  if (!email || email === '') {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  
+  if (!valid) {
+    return res.status(404).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+
+  if (!password || password === ' ') {
+    res.status().json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 6) {
+    res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  next();
+};
+
+module.exports = { validateToken, validateEmail, validatePassword };
