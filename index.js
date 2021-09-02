4fecdda7f3c51131ac33de416d8dc73e8df7100b
samuelmmjr <samuelmmjunior@gmail.com>
2021-09-02 10:19:31 -0300

    fix:req

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const fs = require('fs').promises;
+// const talker = require('./routes/talker');
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -11,6 +14,14 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', async (_req, res) => {
+    const talker = await fs.readFile('./talker.json', 'utf8');
+    const response = await JSON.parse(talker);
+    res.status(200).json(response);
+});
+
+// app.use('/talker', talker);
 
 app.listen(PORT, () => {
   console.log('Online');
---
+++ routes/talker.js
@@ -0,0 +1,9 @@
+// const fs = require('fs').promises;
+
+// app.get('/talker', async (_req, res) => {
+//     const talker = await fs.readFile('./talker.json', 'utf8');
+//     const response = await JSON.parse(talker);
+//     res.status(200).json(response);
+// });
+
+// export module talker;
