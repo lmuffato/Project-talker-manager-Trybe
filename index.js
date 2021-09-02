49c6cac01000aa0ca036c68ce5de4b5386ee90df
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-09-02 02:12:50 -0300

    refactor: change const http_ok_status

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const talkerRouter = require('./routers/talkerRouter');
 const loginRouter = require('./routers/loginRouter');
+const { HTTP_OK_STATUS } = require('./utils/statusHTTP');
 
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
