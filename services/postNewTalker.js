20b1bdb6aa27e9cc23581c995217938b1b4cd779
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-09-01 22:56:41 -0300

    add postTalker file

--- services/postNewTalker.js
+++ services/postNewTalker.js
@@ -1,17 +1,6 @@
 const getTalkers = require('./getTalkers');
 const { writeFile } = require('fs').promises;
 
-const testTalker = {
-  id: 1,
-  name: "Danielle Santos",
-  age: 56,
-  talk: {
-    watchedAt: "22/10/2019",
-    rate: 5
-  }
-}
-
-
 const postNewTalker = async (talker) => {
   const oldArrTalkers = await getTalkers();
   const newTalker = { ...talker, id: oldArrTalkers.length + 1 }
@@ -20,7 +9,5 @@ const postNewTalker = async (talker) => {
   await writeFile('./talker.json', toWrite);
   return newTalker;
 };
-
-postNewTalker(testTalker);
 
 module.exports = postNewTalker;
