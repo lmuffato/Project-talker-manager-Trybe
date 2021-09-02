e8aa0616b3951a35cb810a80b85b633ef54e13c3
lucas-chave <lcchavesr150@gmail.com>
2021-09-02 11:42:26 -0300

    requirement 1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+
+const talker = './talker.json';
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,8 +15,11 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', async (_req, res) => {
+  const dataTalker = await fs.readFile(talker, 'utf8');
+  res.status(HTTP_OK_STATUS).json(JSON.parse(dataTalker));
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
-
-// iniciando o projeto;
