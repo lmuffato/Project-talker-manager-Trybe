0a634ea54deb2f06ea4a1ccd4c18271f89767c80
Micaela Alves <micaela.alves93@gmail.com>
2021-09-01 23:48:05 -0300

    Req 6 - middleware deleta talker

---
+++ middlewares/deleteTalker.js
@@ -0,0 +1,15 @@
+const editTalkers = require('../fs-utils/editTalkers');
+const getTalkers = require('../fs-utils/getTalkers');
+
+const HTTP_OK_STATUS = 200;
+
+const deleteTalker = async (req, res) => {
+  const { id: idDelete } = req.params;
+  const talkersList = await getTalkers();
+  const newTalkerList = talkersList
+    .filter(({ id }) => Number(id) !== Number(idDelete));
+  await editTalkers(newTalkerList);
+  return res.status(HTTP_OK_STATUS).json({ message: 'Pessoa palestrante deletada com sucesso' });
+};
+
+module.exports = deleteTalker;
