328b44fc62689108e2fe83002058d3dd90f1342d
Micaela Alves <micaela.alves93@gmail.com>
2021-09-01 22:45:45 -0300

    Requisito 3

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const showTalkers = require('./middlewares/showTalkers');
 const getTalkerById = require('./middlewares/getTalkerById');
+const login = require('./middlewares/login');
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,6 +19,8 @@ app.get('/', (_request, response) => {
 app.get('/talker/:id', getTalkerById);
 
 app.get('/talker', showTalkers);
+
+app.post('/login', login);
 
 app.listen(PORT, () => {
   console.log('Online');
