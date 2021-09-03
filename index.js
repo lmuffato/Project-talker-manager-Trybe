217d43c75e2f3e46ab538b5031fb80159c579b0b
Willian Alves <willian.alvesb65@gmail.com>
2021-09-02 22:19:10 -0300

    Feat: Add requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,14 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (req, res) => {
+  const talker = JSON.parse(fs.readFileSync('./talker.json'));
+  if (talker.length === 0) {
+    return res.status(HTTP_OK_STATUS).json([]);
+  } 
+    return res.status(HTTP_OK_STATUS).json(talker);
 });
 
 app.listen(PORT, () => {
