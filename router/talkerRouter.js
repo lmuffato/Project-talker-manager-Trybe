1fa72d0630febb39b86f336669ab759654a4521f
Alexandre Oliveira <alexandremolive@gmail.com>
2021-09-03 11:02:16 -0300

    feat req 7

--- router/talkerRouter.js
+++ router/talkerRouter.js
@@ -19,13 +19,28 @@ const HTTP_ERROR_STATUS = 404;
 const HTTP_OK_STATUS = 200;
 const CREATED_STATUS = 201;
 
-router.get('/', async (_req, res) => {
-  try {
+router.get('/search',
+  validateToken,
+  async (req, res) => {
+    const { q } = req.query;
+    if (!q) {
     const data = await readData();
     res.status(HTTP_OK_STATUS).json(data);
-  } catch (e) {
-    res.status(HTTP_OK_STATUS).json([]);
-  }
+    }
+    const data = await readData();
+    const querySearch = data.filter((d) => d.name.includes(q));
+    if (!querySearch) {
+      res.status(HTTP_OK_STATUS).json([]);
+    }
+    return res.status(HTTP_OK_STATUS).json(querySearch);
+  });
+
+router.get('/', async (_req, res) => {
+    const data = await readData();
+    if (!data) {
+      res.status(HTTP_OK_STATUS).json([]);
+    }
+    res.status(HTTP_OK_STATUS).json(data);
 });
 
 router.get('/:id', async (req, res) => {
@@ -48,13 +63,13 @@ router.post('/',
   validateTalk,
   validateDate,
   validateRate,
-  
+
   async (req, res) => {
     const { name, age, talk } = req.body;
     const { watchedAt, rate } = talk;
     const data = await readData();
 
-    if (data[data.length - 1]) {
+    if (data) {
       const { id: dataId } = data[data.length - 1];
       const id = parseInt(dataId, 10) + 1;
 
@@ -78,28 +93,28 @@ router.put('/:id',
   async (req, res) => {
     const { name, age, talk } = req.body;
     const { watchedAt, rate } = talk;
-    
+
     const { id: dataId } = req.params;
     const id = parseInt(dataId, 10);
     const data = await readData();
     const dataFiltered = data.filter((d) => d.id !== parseInt(id, 10));
-    
+
     dataFiltered.push({ id, name, age, talk: { watchedAt, rate } });
     await writeData(dataFiltered);
-    
+
     const newData = await readData();
 
     return res.status(HTTP_OK_STATUS).json(newData[newData.length - 1]);
   });
 
-  router.delete('/:id',
+router.delete('/:id',
   validateToken,
 
   async (req, res) => {
-    const { id } = req.params;    
+    const { id } = req.params;
     const data = await readData();
     const dataFiltered = data.filter((d) => d.id !== parseInt(id, 10));
-   
+
     await writeData(dataFiltered);
 
     return res.status(HTTP_OK_STATUS).json({ message: 'Pessoa palestrante deletada com sucesso' });
