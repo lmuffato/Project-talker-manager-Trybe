205cd20220cd2cf92f89e904ab3ddb99471d78cd
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-04 15:01:18 -0300

    nova funcao getTalkerById no model

--- model/talkerModel.js
+++ model/talkerModel.js
@@ -5,6 +5,14 @@ async function getAllTalkers() {
   return JSON.parse(allTalkers);
 }
 
+async function getTalkerById(talkerId) {
+  const allTalkers = await fs.readFile('./talker.json');
+  const allTalkersData = JSON.parse(allTalkers);
+  const talkerById = allTalkersData.find((t) => t.id === parseInt(talkerId, 10));
+  return talkerById;
+}
+
 module.exports = {
   getAllTalkers,
+  getTalkerById,
 };
