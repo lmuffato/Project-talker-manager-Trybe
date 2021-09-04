bea0ec014fa460cce439525ec9309fc0284f2ad6
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-04 15:52:05 -0300

    middlewares de validação

---
+++ utils/middlewares.js
@@ -0,0 +1,35 @@
+function validateEmail(req, res, next) {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({
+      message: 'O campo "email" é obrigatório',
+    });
+  }
+  const emailRegex = /[a-z0-9]+@[a-z0-9]+(\.com)$/gi;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }
+  next();
+}
+
+function validatePassword(req, res, next) {
+  const { password } = req.body;
+
+  if (!password) {
+    return res.status(400).json({
+      message: 'O campo "password" é obrigatório',
+    });
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
+  }
+
+  next();
+}
+
+module.exports = { validateEmail, validatePassword };
