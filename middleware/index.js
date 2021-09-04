85e155d1f8b8c9224bd6d45dd334c0c922260a80
Lucas Pedroso <lepsk8@gmail.com>
2021-09-03 21:08:03 -0300

    add index for /middlewares

---
+++ middleware/index.js
@@ -0,0 +1,17 @@
+const nameValidation = require('./nameValidation');
+const ageValidation = require('./ageValidation');
+const talkWatchedAtValidation = require('./talkWatchedAtValidation');
+const talkRateValidation = require('./talkRateValidation');
+const talkWatchedAtRegexValidation = require('./talkWatchedAtRegexValidation');
+const tokenVerification = require('./tokenVerification');
+const talkValidation = require('./talkValidation');
+
+module.exports = {
+  nameValidation,
+  ageValidation,
+  talkWatchedAtValidation,
+  talkRateValidation,
+  talkWatchedAtRegexValidation,
+  tokenVerification,
+  talkValidation,
+};
