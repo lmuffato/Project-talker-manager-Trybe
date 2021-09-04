d754680719094d35618b40111828eb5bc896e06b
Hudson Carlos <hc88806@gmail.com>
2021-09-04 13:18:50 -0400

    router /talker done

--- index.js
+++ index.js
@@ -79,6 +79,129 @@ app.post('/login', async (req, res) => {
   return res.status(200).json({ token });
 });
 
+const validationToken = (authorization) => {
+  const regexToken = /[A-Z0-9a-z]{16}/;
+
+  if (!authorization) {
+    return {
+      message: 'Token não encontrado',
+    };
+  }
+
+  if (!regexToken.test(authorization)) {
+    return { message: 'Token inválido' };
+  }
+
+  return true;
+};
+
+const validationName = (name) => {
+  if (!name || name === '') {
+    return {
+      message: 'O campo "name" é obrigatório',
+    };
+  }
+
+  if (name.length < 3) {
+    return {
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    };
+  }
+
+  return 'err';
+};
+
+const validetionAge = (age) => {
+  if (!age || age === '') {
+    return {
+      message: 'O campo "age" é obrigatório',
+    };
+  }
+
+  if (age < 18) {
+    return {
+      message: 'A pessoa palestrante deve ser maior de idade',
+    };
+  }
+
+  return true;
+};
+
+function extendValidations(watchedAt, rate) {
+  const regexData = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
+
+  if (rate > 5 || rate < 1) {
+    return {
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    };
+  }
+
+  if (!regexData.test(watchedAt)) {
+    return {
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+    };
+  }
+
+  return true;
+}
+
+const validetionTalk = (talk) => {
+  if (!talk) {
+    return {
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    };
+  }
+
+  const { watchedAt, rate } = talk;
+  if (!watchedAt || !rate) {
+    return {
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    };
+  }
+
+  const extend = extendValidations(watchedAt, rate);
+  if (extend.message) return extend;
+
+  return true;
+};
+
+function talkerExtend(req) {
+  const { name, age, talk } = req.body;
+  const { authorization } = req.headers;
+  const token = validationToken(authorization);
+  const resultName = validationName(name);
+  const resultAge = validetionAge(age);
+  const resultTalk = validetionTalk(talk);
+
+  if (token.message) return { status: 401, response: token };
+
+  if (resultName.message) return { status: 400, response: resultName };
+
+  if (resultAge.message) return { status: 400, response: resultAge };
+
+  if (resultTalk.message) return { status: 400, response: resultTalk };
+
+  return false;
+}
+
+app.post('/talker', async (req, res) => {
+  const talkers = await fs.readFile('./talker.json', 'utf8');
+
+  const validations = talkerExtend(req);
+
+  if (validations.status) {
+    const { status, response } = validations;
+    return res.status(status).json(response);
+  }
+
+  const newTalkers = JSON.parse(talkers);
+  newTalkers.push({ id: newTalkers.length + 1, ...req.body });
+
+  await fs.writeFile('./talker.json', JSON.stringify(newTalkers));
+
+  return res.status(201).json(newTalkers[newTalkers.length - 1]);
+});
+
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
