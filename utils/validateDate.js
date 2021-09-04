828758772b8bd4d4a0a5001edc1e73295222cea8
Ana Ventura <anaventura1811@gmail.com>
2021-09-04 10:10:17 -0300

    fix: remove código comentado e não utilizado

--- utils/validateDate.js
+++ utils/validateDate.js
@@ -3,7 +3,7 @@ const { BAD_REQUEST } = require('./httpStatus');
 const validateDate = (req, res, next) => {
   const { watchedAt } = req.body.talk;
   const isValidDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
-  // const testRegex = isValidDate.test(talk.watchedAt);
+  
   if (!watchedAt || !watchedAt === '') {
     return res.status(BAD_REQUEST).json(
       { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
