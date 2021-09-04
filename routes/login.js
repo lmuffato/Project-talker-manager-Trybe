39e37fe1cab6d628d44237cf1551593a56bb4a4f
Lucas Pedroso <lepsk8@gmail.com>
2021-09-03 21:08:18 -0300

    addd login route

---
+++ routes/login.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const fs = require('fs').promises;
+
+const router = express.Router();
+const crypto = require('crypto');
+
+const emailVerification = require('../middleware/EmailVerification');
+const passwordVerification = require('../middleware/PasswordVerification');
+
+router.post('/', emailVerification, passwordVerification, async (_req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  // const token = '0fd663466aaa5297';
+  await fs.writeFile('token.txt', token)
+    .catch((err) => console.error(err));
+  res.status(200).json({ token });
+});
+
+module.exports = router;
