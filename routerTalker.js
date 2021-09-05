6c1541559d4c9795a0abf0a8906e6f6eb7b75add
phlopes <phlopes93@gmail.com>
2021-09-05 17:52:21 -0300

    requirement 5 completed

--- routerTalker.js
+++ routerTalker.js
@@ -11,11 +11,13 @@ const {
   talkValidationMandatory,
 } = require('./MiddlewaresValidation');
 
+const talkerJSON = './talker.json';
+
 router.use(bodyParser.json());
 
 router.get('/', async (_req, res) => {
   try {
-    const talker = await fs.readFile('./talker.json', 'utf8');
+    const talker = await fs.readFile(talkerJSON, 'utf8');
     res.status(200).json(JSON.parse(talker));
   } catch (err) {
     res.status(500).send(err.message);
@@ -29,7 +31,7 @@ ageValidation,
 talkValidation, 
 talkValidationMandatory, 
 async (req, res) => {
-  const talker = await fs.readFile('./talker.json', 'utf8');
+  const talker = await fs.readFile(talkerJSON, 'utf8');
   const talkerParse = JSON.parse(talker);
   const { name, age, talk } = req.body;
 
@@ -41,19 +43,39 @@ async (req, res) => {
   };
   
   talkerParse.push(newPeople);
-  await fs.writeFile('./talker.json', JSON.stringify(talkerParse));
+  await fs.writeFile(talkerJSON, JSON.stringify(talkerParse));
 
   res.status(201).json(newPeople);
 }); 
 
 router.get('/:id', async (req, res) => {
-  const talker = await fs.readFile('./talker.json', 'utf8');
+  const talker = await fs.readFile(talkerJSON, 'utf8');
   const { id } = req.params;
   const people = JSON.parse(talker).find((p) => p.id === +id);
   
   if (!people) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 
   res.status(200).json(people);
+});
+
+router.put('/:id', validateToken, 
+nameValidation, 
+ageValidation, 
+talkValidation, 
+talkValidationMandatory,
+ async (req, res) => {
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+  
+  const talker = await fs.readFile(talkerJSON, 'utf8');
+  const talkerParse = JSON.parse(talker);
+
+  const editTalker = { name, age, talk, id: +id };
+  const talkerId = talkerParse.filter((p) => p.id === +id);
+  talkerId.push(editTalker);
+
+  await fs.writeFile('talker.json', JSON.stringify(talkerId));
+  return res.status(200).json(editTalker);
 });
     
 module.exports = router;
