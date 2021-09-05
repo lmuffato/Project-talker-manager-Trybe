a8c2597625d787d334625203ba1228a49ad3ce63
William <w.willers@outlook.com>
2021-09-05 18:46:18 -0400

    Requisito 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const talker = require('./talker.json');
 
 const app = express();
 app.use(bodyParser.json());
@@ -22,6 +23,21 @@ app.get('/talker', (req, res) => {
     const TALKER = JSON.parse(data);
     return res.status(HTTP_OK_STATUS).send(TALKER);
   });
+});
+
+// Resolvi fazer a anterior com readFile e essa importando o arquivo, 
+// apenas para mostrar que é possível das duas maneiras
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  const talkerById = talker.find((t) => t.id === parseInt(id, 8));
+
+  if (!talkerById) {
+ return res.status(404).json({
+    message: 'Pessoa palestrante não encontrada',
+  }); 
+}
+
+  res.status(200).json(talkerById);
 });
 
 app.listen(PORT, () => {
