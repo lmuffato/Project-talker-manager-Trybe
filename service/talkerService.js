bb648d220cd0ff48bfd2509c01173639404a6673
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-04 22:41:50 -0300

    eslint errors

--- service/talkerService.js
+++ service/talkerService.js
@@ -38,7 +38,7 @@ function validatePostTalkerTalk(req, res, next) {
   const { talk } = req.body;
   if (!talk.watchedAt || !talk.rate) {
     return res.status(400).json({
-      message: 'O campo \"talk\" é obrigatório e \"watchedAt\" e \"rate\" não podem ser vazios',
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
     });
   }
   next();
