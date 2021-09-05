134d9757ff1fa0382be697fdd301a83273338fbc
William <w.willers@outlook.com>
2021-09-05 18:19:11 -0400

    Correção lint para commit inicial

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const { response } = require('express');
 
 const app = express();
 app.use(bodyParser.json());
