c7adae9dcd00855c9aabef8c1ed7b65346ed2940
Rafael <rafaelsantanafernandes@gmail.com>
2021-09-05 01:43:12 -0300

    done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-app.get('/talker', talker);
+app.use('/talker', talker);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
--- talker.js
+++ talker.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const { readFile } = require('fs').promises;
+const fs = require('fs').promises;
 
 const router = express.Router();
 
-// const getData = async (path) => {
-//     const data = await readFile(path, 'utf-8');
-//     return JSON.parse(data);
-//   };
-
 router.get('/', async (_req, res) => {
     try {
-        const data = await readFile('./talker.json', 'utf-8');
+        const data = await fs.readFile('./talker.json', 'utf-8');
         // console.log(JSON.parse(data));
         return res.status(200).json(JSON.parse(data));
     } catch (error) {
