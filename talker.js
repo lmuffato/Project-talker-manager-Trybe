6edd07e891de82796273bc813388789a5f4737ad
Rafael <rafaelsantanafernandes@gmail.com>
2021-09-05 01:26:23 -0300

    initial

--- talker.js
+++ talker.js
@@ -3,20 +3,19 @@ const { readFile } = require('fs').promises;
 
 const router = express.Router();
 
-const getData = async (path) => {
-    const data = await readFile(path, 'utf-8');
-    return JSON.parse(data);
-  };
+// const getData = async (path) => {
+//     const data = await readFile(path, 'utf-8');
+//     return JSON.parse(data);
+//   };
 
 router.get('/', async (_req, res) => {
     try {
-        const talker = await getData('talker.json');
-        return res.status(200).json(talker);
+        const data = await readFile('./talker.json', 'utf-8');
+        // console.log(JSON.parse(data));
+        return res.status(200).json(JSON.parse(data));
     } catch (error) {
         return res.status(200).json([]);
     }
 });
 
-module.exports = {
-    router,
-};
+module.exports = router;
