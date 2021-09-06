6a930579c7369e4e2376c89e3af6e687bd66c87f
igson <igson660@gmail.com>
2021-09-06 11:12:14 -0500

    quesito 03

--- index.js
+++ index.js
@@ -51,6 +51,49 @@ app.get('/talker/:id', async (req, res) => {
   }
 });
 
+// Quesito 03
+// Referencia para a criação do tolken: https://nodejs.org/api/crypto.html#crypto_crypto_randombytes_size_callback
+
+const HTTP_BAD_REQUEST_STATUS = 400;
+
+const crypto = require('crypto');
+
+const tokenGenerator = () => crypto.randomBytes(8).toString('hex');
+
+const validationEmail = (email) => {
+  if (!email) return 'O campo "email" é obrigatório';
+  const verificationEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+  if (!verificationEmail) return 'O "email" deve ter o formato "email@email.com"';
+  return null;
+};
+
+const validationPassWord = (password) => {
+  if (!password) return 'O campo "password" é obrigatório';
+  const passWord = password.toString();
+  if (passWord.length < 6) return 'O "password" deve ter pelo menos 6 caracteres';
+  return null;
+};
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const verificationEmail = validationEmail(email);
+  const verificationPassword = validationPassWord(password);
+
+  try {
+    if (verificationEmail !== null) {
+      return res.status(HTTP_BAD_REQUEST_STATUS).send({ message: verificationEmail });
+    }
+
+    if (verificationPassword !== null) {
+      return res.status(HTTP_BAD_REQUEST_STATUS).send({ message: verificationPassword });
+    }
+
+    res.status(HTTP_OK_STATUS).json({ token: tokenGenerator() });
+  } catch (_e) {
+    return null;
+  }
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
