382854c4e9ae9966c78d9d0d67c66d2a8402b282
andy silva <andssilva.23@gmail.com>
2021-09-06 19:33:39 -0400

    feat(req 2.2): :rocket: return message if can't find the registered person

--- routerTalker.js
+++ routerTalker.js
@@ -4,6 +4,7 @@ const express = require('express');
 const talker = './talker.json';
 const router = express.Router();
 const HTTP_OK_STATUS = 200;
+const HTTP_ERROR_STATUS = 404;
 
 // requisito 1
 router.get('/', (_request, response) => {
@@ -16,8 +17,11 @@ router.get('/', (_request, response) => {
 router.get('/:id', (request, response) => {
   const { id } = request.params;
   const dataTalker = JSON.parse(fs.readFileSync(talker));
-
   const findTalker = dataTalker.find((elem) => elem.id === parseInt(id, 10));
+  if (!findTalker) {
+    return response.status(HTTP_ERROR_STATUS)
+      .json({ message: 'Pessoa palestrante não encontrada' });
+  }
   return response.status(HTTP_OK_STATUS).json(findTalker);
 });
 
