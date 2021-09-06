b45321bed4cf67e71c66c16541d35a6269ec1005
Rafael <rafaelsantanafernandes@gmail.com>
2021-09-06 00:28:13 -0300

    done

--- login.js
+++ login.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const crypto = require('crypto');
-const midd = require('./middleware');
 
 const router = express.Router();
 
-router.post('/', midd.emailValidate, midd.passwordValidate, async (_req, res) => {
-    try {
-        const token = crypto.randomBytes(8).toString('hex');
-        return res.status(200).json(token);
-    } catch (error) {
-        return res.status(500).json(error);
-    }
+const token = () => crypto.randomBytes(8).toString('hex');
+
+const emailValidate = (req, res, next) => {
+  const { email } = req.body;
+  if (!email || email === '') {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!(email.includes('@') && email.includes('.com'))) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+};
+
+const passwordValidate = (req, res, next) => {
+  const { password } = req.body;
+  const MIN_LENGTH = 6;
+  if (!password || password === '') {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < MIN_LENGTH) {
+    return res.status(400).json({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
+  }
+
+  next();
+};
+
+router.post('/', emailValidate, passwordValidate, (req, res, _next) => {
+  res.status(200).json({ token: token() });
 });
 
 module.exports = router;
