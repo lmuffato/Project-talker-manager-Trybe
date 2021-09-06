8bf30d3530088ed9a66cbb33ca3014f05625a90a
carlos25xrm <carlos25xrm@gmail.com>
2021-09-06 10:06:02 -0300

    Incluindo postLogin em endpoints > index.js

--- endpoints/index.js
+++ endpoints/index.js
@@ -1,7 +1,17 @@
+// Arquivo centralizador dos middlewares
+
 const getTalker = require('./getTalker');
 const getTalkerID = require('./getTalkerID');
+// tokenLogin, validarEmail e validarPassword estão entre {} SOMENTE
+// abaixo porque o arquivo postLogin exporta mais de um módulo.
+const { tokenLogin } = require('./postLogin');
+const { validarEmail } = require('./postLogin');
+const { validarPassword } = require('./postLogin');
 
 module.exports = {
   getTalker,
   getTalkerID,
+  tokenLogin,
+  validarEmail,
+  validarPassword,
 };
