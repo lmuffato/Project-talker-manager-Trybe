df595bbfba9a1670e1adc376197c828afb21423f
andy silva <andssilva.23@gmail.com>
2021-09-06 18:23:39 -0400

    feat(req 1.2): :rocket: check if the endpoint brings the empty array

--- routerTalker.js
+++ routerTalker.js
@@ -6,6 +6,9 @@ const dataTalker = JSON.parse(fs.readFileSync(talker, 'utf-8'));
 const router = express.Router();
 
 // requisito 1
-router.get('/', (_request, response) => response.status(200).json(dataTalker));
+router.get('/', (_request, response) => {
+  if (dataTalker.length === 0) return response.status(200).json([]);
+  return response.status(200).json(dataTalker);
+});
 
 module.exports = router;
