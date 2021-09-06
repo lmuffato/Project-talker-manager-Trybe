9c9e469c7354e81122244901143f080f9ce95cf7
andy silva <andssilva.23@gmail.com>
2021-09-06 18:26:11 -0400

    feat: :rocket: add the HTTP_OK_STATUS constant to the routerTalker file

--- routerTalker.js
+++ routerTalker.js
@@ -4,11 +4,12 @@ const express = require('express');
 const talker = 'talker.json';
 const dataTalker = JSON.parse(fs.readFileSync(talker, 'utf-8'));
 const router = express.Router();
+const HTTP_OK_STATUS = 200;
 
 // requisito 1
 router.get('/', (_request, response) => {
-  if (dataTalker.length === 0) return response.status(200).json([]);
-  return response.status(200).json(dataTalker);
+  if (dataTalker.length === 0) return response.status(HTTP_OK_STATUS).json([]);
+  return response.status(HTTP_OK_STATUS).json(dataTalker);
 });
 
 module.exports = router;
