56791299feeba5a170aaca634643d370f1f57e97
carlos25xrm <carlos25xrm@gmail.com>
2021-09-06 19:47:22 -0300

    Requisito 5

--- endpoints/postTalker.js
+++ endpoints/postTalker.js
@@ -54,14 +54,14 @@ const validarNota = (request, response, next) => {
   const { talk } = request.body;
   const { rate } = talk;
 
-  if (!rate) {
-    return response.status(400)
-    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
-  }
-
   if (rate < 1 || rate > 5) {
     return response.status(400)
     .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  if (!rate) {
+    return response.status(400)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
   }
 
   next();
