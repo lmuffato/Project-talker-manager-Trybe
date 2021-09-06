33ebb76258fb2dbb01eacdc7efa00dd320dff131
andy silva <andssilva.23@gmail.com>
2021-09-06 19:03:28 -0400

    fix: :bug: fix requirement 1

--- routerTalker.js
+++ routerTalker.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const express = require('express');
 
-const talker = 'talker.json';
-const dataTalker = JSON.parse(fs.readFileSync(talker, 'utf-8'));
+const talker = './talker.json';
 const router = express.Router();
 const HTTP_OK_STATUS = 200;
 
 // requisito 1
 router.get('/', (_request, response) => {
+  const dataTalker = JSON.parse(fs.readFileSync(talker));
   if (dataTalker.length === 0) return response.status(HTTP_OK_STATUS).json([]);
   return response.status(HTTP_OK_STATUS).json(dataTalker);
 });
