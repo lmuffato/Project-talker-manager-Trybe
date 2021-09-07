35525beb61064dac524bc75934b4d4a856b6d90a
Rafael <rafaelsantanafernandes@gmail.com>
2021-09-07 10:34:52 -0300

    done refactor again

--- login.js
+++ login.js
@@ -1,36 +1,12 @@
 const express = require('express');
 const crypto = require('crypto');
+const midd = require('./middleware');
 
 const router = express.Router();
 
 const token = () => crypto.randomBytes(8).toString('hex');
 
-const emailValidate = (req, res, next) => {
-  const { email } = req.body;
-  if (!email || email === '') {
-    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
-  }
-  if (!(email.includes('@') && email.includes('.com'))) {
-    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  }
-  next();
-};
-
-const passwordValidate = (req, res, next) => {
-  const { password } = req.body;
-  if (!password || password === '') {
-    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
-  }
-  if (password.length < 6) {
-    return res.status(400).json({
-      message: 'O "password" deve ter pelo menos 6 caracteres',
-    });
-  }
-
-  next();
-};
-
-router.post('/', emailValidate, passwordValidate, (req, res, _next) => {
+router.post('/', midd.emailValidate, midd.passwordValidate, (req, res, _next) => {
   try {
    return res.status(200).json({ token: token() });
   } catch (error) {
