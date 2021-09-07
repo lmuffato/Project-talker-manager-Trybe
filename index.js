feee61cbb1086a602fa0d683b7e39f7fa7da1865
rcalvs <rafaelcalvette@gmail.com>
2021-09-07 17:05:49 -0300

    Req 1

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+const talker = require('./controllers/talker');
+
+app.use('/talker', talker);
+
 app.listen(PORT, () => {
   console.log('Online');
 });
