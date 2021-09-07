966e670b40f97334ef0d72442b7ed84199d32136
Perycles reis <perycles_reis@hotmail.com>
2021-09-07 11:34:33 -0300

    requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,9 +8,19 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const talkerData = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_rej, res) => {
+  try {
+    return res.status(200).send(talkerData);
+  } catch (error) {
+    return res.status(200).send({ error });
+  }
 });
 
 app.listen(PORT, () => {
