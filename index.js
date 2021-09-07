75315d04fdaeee9e6c41d0fb17134c68003c4abb
vinicios <soicinivt@gmail.com>
2021-09-07 00:15:07 -0300

    Requisito 3.

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const HTTP_NOT_FOUND_STATUS = 404;
+const HTTP_BAD_REQUEST = 400;
+const HTTP_NOT_FOUND = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -26,12 +27,33 @@ app.get('/talker/:id', async (req, res) => {
   const talkers = await fs.readFile('talker.json');
   const [talker] = JSON.parse(talkers).filter(({ id: talkerID }) => talkerID === Number(id));
   if (!talker) {
-  return res.status(HTTP_NOT_FOUND_STATUS).json({
+  return res.status(HTTP_NOT_FOUND).json({
   message: 'Pessoa palestrante não encontrada',
 }); 
 }
   
   res.status(HTTP_OK_STATUS).json(talker);
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const validator = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const isValidEmail = validator.test(email);
+  // const isPasswordGTE6 = password.length >= 6;
+  if (!email) res.status(HTTP_BAD_REQUEST).json({ message: 'O campo "email" é obrigatório' });
+  if (!isValidEmail) {
+  res.status(HTTP_BAD_REQUEST).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    }); 
+}
+  if (!password) res.status(HTTP_BAD_REQUEST).json({ message: 'O campo "password" é obrigatório' });
+  if (/* !isPasswordGTE6 */ password.length < 6) {
+  res.status(HTTP_BAD_REQUEST).json({
+    message: 'O "password" deve ter pelo menos 6 caracteres',
+  }); 
+}
+  const token = '16caracteresaqui';
+  res.status(HTTP_OK_STATUS).json({ token });
 });
 
 app.listen(PORT, () => {
