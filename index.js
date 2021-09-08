50a0867ad3a054e071402f8a06b088d69a00373d
Perycles reis <perycles_reis@hotmail.com>
2021-09-08 14:04:11 -0300

    refatorando desafio 1

--- index.js
+++ index.js
@@ -8,19 +8,16 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-const talkerData = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', (_rej, res) => {
-  try {
-    return res.status(HTTP_OK_STATUS).send(talkerData);
-  } catch (error) {
-    return res.status(HTTP_OK_STATUS).json([]);
-  }
+app.get('/talker', (_req, res) => {
+  const talkerData = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
+
+  if (talkerData.length === 0) return res.status(HTTP_OK_STATUS).json([]);
+  res.status(HTTP_OK_STATUS).json(talkerData);
 });
 
 app.listen(PORT, () => {
