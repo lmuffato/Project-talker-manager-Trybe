956da6447ebfb1dfb7ae3d1ae94706ba34dd2535
Erick-Marinho <erickgaves@gmail.com>
2021-09-08 11:53:14 -0300

    cumprindo requisito 01

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+const data = require('./talker.json');
+
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (req, res) => {
+  if (!data) {
+    return res.status(HTTP_OK_STATUS).json([]);
+  }
+  res.status(HTTP_OK_STATUS).send(data);
 });
 
 app.listen(PORT, () => {
