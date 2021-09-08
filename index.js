52a94bd330a5aa7986c1f2e13fd0520f45d0d170
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-09-08 14:13:54 -0300

    requisito 3

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
-const crypto = require('crypto');
+// const crypto = require('crypto');
 
-const { validateEmail, validatePassword } = require('./validation.js');
+const { validateEmail, validatePassword, validateToken, validateName, validateAge, 
+validateData, validateRate, validateTalk } = require('./validation.js');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
-
-// requisito 2
-app.get('/talker/:id', async (req, res) => {
-  const { id } = req.params;
-  
-  const data = await fs.readFile('./talker.json', 'utf8');
-  const talker = JSON.parse(data);
-
-  const talkerId = talker.find((item) => item.id === Number(id));
-  // console.log(talkerId);
-
-  if (!talkerId) {
-    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-  }
-
-  res.status(HTTP_OK_STATUS).json(talkerId);
-});
 
 // requisito 1
 app.get('/talker', async (_req, res) => {
@@ -36,18 +20,46 @@ app.get('/talker', async (_req, res) => {
   res.status(HTTP_OK_STATUS).json(talker);
 });
 
+// requisito 2
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  
+  const data = await fs.readFile('./talker.json', 'utf8');
+  const talker = JSON.parse(data);
+
+  const talkerId = talker.find((item) => item.id === Number(id));
+
+  if (!talkerId) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(HTTP_OK_STATUS).json(talkerId);
+});
+
 // requisito 3
 app.post('/login', validateEmail, validatePassword, (_req, res) => {
-  const token = crypto.randomBytes(16).toString('hex'); // crio um numero aleatorio de 16 caracteres, tive ajuda do Pedro Ramos durante o plantão
-  console.log(token);
+  // const tokenNumber = crypto.randomBytes(16).toString('hex'); // crio um numero aleatorio de 16 caracteres, tive ajuda do Pedro Ramos durante o plantão
+  // console.log(tokenNumber);
 
-  res.status(HTTP_OK_STATUS).json(token);
+  res.status(HTTP_OK_STATUS).json({ token: '7mqaVRXJSp886CGr' });
 });
 
 // // requisito 4
-// app.post('/taker', validateToken, validateEmail, validatePassword, (_req, res) => {
+app.post('/taker', validateToken, validateName, validateAge, 
+validateData, validateRate, validateTalk, async (req, res) => {
+  const { name, age, talk } = req.body;
   
-// });
+  const data = await fs.readFile('./talker.json', 'utf8');
+  const talkerData = JSON.parse(data);
+
+  talkerData.push({ name, age, talk });
+
+  fs.writeFile('./talker.json', talkerData);
+
+  // leio o arquivo talker.json, transformo em json e adiciono um novo objeto, depois pego esse arquivo atualizado e altero no talker.jdon com o writeFile
+
+  res.status(201).json({ name, age, talk });
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
--- validation.js
+++ validation.js
@@ -1,12 +1,3 @@
-const validateToken = (req, res, next) => {
-  const { token } = req.headers;
-  if (token.length < 17) {
-    res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-  }
-
-  next();
-};
-
 const validateEmail = (req, res, next) => {
   const { email } = req.body;
 
@@ -14,14 +5,14 @@ const validateEmail = (req, res, next) => {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailvalid);
   }
   
-  const valid = EmailIsValid(email);
+  const valid = EmailIsValid(email); // retorna true or false
 
   if (!email || email === '') {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
   
   if (!valid) {
-    return res.status(404).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   
   next();
@@ -30,15 +21,95 @@ const validateEmail = (req, res, next) => {
 const validatePassword = (req, res, next) => {
   const { password } = req.body;
 
-  if (!password || password === ' ') {
-    res.status().json({ message: 'O campo "password" é obrigatório' });
+  if (!password || password === '') {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
 
-  if (password.length < 7) {
-    res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
 
   next();
 };
 
-module.exports = { validateToken, validateEmail, validatePassword };
+const validateToken = (req, res, next) => {
+  const { token } = req.headers;
+  
+  if (!token) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+
+  if (token.length !== 16) {
+    res.status(400).json({ message: 'Token inválido' });
+  }
+
+  next();
+};
+
+const validateName = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name || name === '') {
+    res.status(400).json({ message: 'O campo name é obrigatório' });
+  }
+
+  if (name.length < 3) {
+    res.status(400).json({ message: 'O name deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+const validateAge = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age || age === '') {
+    res.status(400).json({ message: 'O campo age é obrigatório' });
+  }
+
+  if (age < 18) {
+    res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+const validateData = (req, res, next) => {
+  const { talk } = req.body;
+  const { date } = talk;
+
+  if (date) {
+    res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+const validateRate = (req, res, next) => {
+  const { talk } = req.body;
+  const { rate } = talk;
+
+  if (rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
+const validateTalk = (req, res, next) => {
+  const { talk } = req.body;
+  const { rate, date } = talk;
+
+  if (!talk && (rate === 0 || date === '')) {
+    res.status(400).json({ message: 'O campo age é obrigatório' });
+  }
+  next();
+};
+
+module.exports = { 
+  validateEmail, 
+  validatePassword, 
+  validateToken, 
+  validateName, 
+  validateAge, 
+  validateData, 
+  validateRate, 
+  validateTalk,
+};
