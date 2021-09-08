f03d340a4614899586e89d74f05d0207463b37a0
Perycles reis <perycles_reis@hotmail.com>
2021-09-08 14:53:55 -0300

    desafio 2

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const NOT_FOUND = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -18,6 +19,19 @@ app.get('/talker', (_req, res) => {
 
   if (talkerData.length === 0) return res.status(HTTP_OK_STATUS).json([]);
   res.status(HTTP_OK_STATUS).json(talkerData);
+});
+
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+
+  const talkerData = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
+  const dataPerson = talkerData.find((person) => person.id === parseInt(id, 10));
+
+  if (dataPerson === undefined) {
+    return res.status(NOT_FOUND)
+      .json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(HTTP_OK_STATUS).json(dataPerson);
 });
 
 app.listen(PORT, () => {
