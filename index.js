083856cdee244d590a36484acf4891fba9fc5194
bob-mw <wend3llsouza@gmail.com>
2021-09-09 06:35:35 -0300

    :package: post Login

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const HTTP_NOTFOUND_STATUS = 404;
+const HTTP_NOTFOUND = 400;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -33,4 +34,23 @@ app.get('/talker/:id', async (req, res) => {
   }
 
   res.status(HTTP_NOTFOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
+app.post('/login', (req, res) => {
+  const regex = /\S+@\S+\.\S+/;
+  const { email, password } = req.body;
+  const emailTest = regex.test(email);
+  if (!email) {
+    res.status(HTTP_NOTFOUND).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!emailTest) {
+    res.status(HTTP_NOTFOUND).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    res.status(HTTP_NOTFOUND).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    res.status(HTTP_NOTFOUND).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  res.status(HTTP_OK_STATUS).json({ token: '7mqaVRXJSp886CGr' });
 });
