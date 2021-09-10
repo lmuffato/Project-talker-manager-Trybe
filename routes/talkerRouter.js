93b176fb57d9824e4b140455e4f205e0efe0d280
Debora Passos <deborinhapassos@gmail.com>
2021-09-10 18:30:39 -0300

    Ainda 4

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const express = require('express');
+const talkersList = require('../talker.json');
 
 const router = express.Router();
 
@@ -7,14 +9,26 @@ const getTalkers = require('./getTalkers');
 const nameValidation = require('./nameValidation');
 const ageValidation = require('./ageValidation');
 const tokenValidation = require('./tokenValidation');
+const { rateValidation, watValidation } = require('./talkValidation');
+
+const validations = [nameValidation, ageValidation, tokenValidation, rateValidation, watValidation];
 
 router.get('/', getTalkers, getTalkersId);
-router.post('/', nameValidation, ageValidation, tokenValidation);
+router.post('/', validations, async (req, res) => {
+  const { name, age, talk: { watchedAt, rate } } = req.body;
+  const talkers = getTalkers();
+  const newTalker = {
+    id: talkers.length + 1,
+    name,
+    age: parseInt(age, 10),
+    talk: {
+      watchedAt,
+      rate: parseInt(rate, 10),
+    },
+  };
+  const addTalker = talkers.push(newTalker);
+  await fs.writeFile(talkersList, JSON.stringify(addTalker));
+  return res.status(201).json(newTalker);
+});
 
-module.exports = {
-  getTalkersId,
-  getTalkers,
-  nameValidation,
-  ageValidation,
-  tokenValidation,
-};
+module.exports = router;
