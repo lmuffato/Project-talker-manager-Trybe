117b44b1832e3c0312d0daeff95315c0e149bd08
lmuffato <lmiremuffato@gmail.com>
2021-09-11 01:02:41 -0300

    requisito 5

--- DFuncoesDeTest.js
+++ DFuncoesDeTest.js
@@ -1,5 +1,5 @@
 // Aquivo para ambiente de teste, será deletado no final do projeto
-
+/*
 const myArray = [
   { name: 'Henrique Albuquerque', age: 62, id: 1, talk: { watchedAt: '23/10/2020', rate: 5 } },
   { name: 'Heloísa Albuquerque', age: 67, id: 2, talk: { watchedAt: '23/10/2020', rate: 5 } },
@@ -36,3 +36,4 @@ console.log(talkers);
 
 delete dataToUpdateWithId.id;
 console.log(dataToUpdateWithId);
+*/
