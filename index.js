f5a3e575c72013504c722a3f3836f74c683d4e43
lmuffato <lmiremuffato@gmail.com>
2021-09-11 20:08:17 -0300

    requisito 7

--- index.js
+++ index.js
@@ -53,16 +53,6 @@ async () => { });
 //   }
 // });
 
-// GET - Rota para acessar o conteúdo de talkers.json
-app.get('/talker', async (_request, response) => {
-  const talkers = await getTalkers();
-  return response.status(200).json(talkers);
-}); 
-/* REQUISIÇÃO
-// http GET :3000/talker           // HTTPIE;   (ok)
-// http://localhost:3000/talker    // navegador (ok);
-*/
-
 // GET - Rota para acessar o conteúdo de talkers.json filtrando por id
 app.get('/talker/:id', async (request, response) => {
   const { id } = request.params;
@@ -80,36 +70,6 @@ app.get('/talker/:id', async (request, response) => {
 // http GET :3000/talker/5          // HTTPIE (mensagem de erro);
 */
 
-// POST - Rota para validar um login e gerar um token;
-app.post('/login', emailValidation, passwordValidation, async (request, response) => {
-  const token = await crypto.randomBytes(8).toString('hex'); // gera um token de 6 caracteres
-  return response.status(200).json({ token: `${token}` });
-});
-/* REQUISIÇÃO
-echo '{"email":"email@email.com", "password":"123456789" }' | http POST :3000/login  // (ok) retorna o token 
-http POST :3000/login email='email@email.com' password='123456789'                   // (ok) retorna o token 
-http POST :3000/login email='' password='123456789'                                  // (error)
-http POST :3000/login email='email@email.com' password=''                            // (error)
-http POST :3000/login email='' password=''                                           // (error)
-*/
-
-// POST - Rota para cadastrar novos talkers
-app.post('/talker',
-tokenValidation,
-nameValidation,
-ageValidation,
-talkValidation,
-// watchedAtValidation,
-// rateValidation,
-registerTalker,
-async () => {});
-/* REQUISIÇÃO
-Requisição complexa no httpie - objeto de vários níveis no body e headers
-echo '{"name": "Danielle Santos", "age": 56, "talk": { "watchedAt": "22/10/2019", "rate": 5 } }' | http POST :3000/talker authorization:"375c3a2e0051b630"
-
-echo '{"name": "Miley Cyrus", "age": 27, "talk": { "watchedAt": "25/09/2020", "rate": 4 } }' | http POST :3000/talker authorization:"375c3a2e0051b630"
-*/
-
 // PUT - Rota para alterar os talkers
 app.put('/talker/:id',
 tokenValidation,
@@ -119,9 +79,7 @@ ageValidation,
 // rateValidation,
 talkValidation,
 updateTalker,
-async () => {
-
-});
+async () => { });
 /* REQUISIÇÃO
 Requisição complexa no httpie - objeto de vários níveis no body e headers
 echo '{"name": "Danielle Santos", "age": 56, "talk": { "watchedAt": "22/10/2019", "rate": 4 } }' | http PUT :3000/talker/4 authorization:"375c3a2e0051b630"         // (ok)
@@ -139,6 +97,45 @@ async () => { });
 /* REQUISIÇÃO
 Requisição complexa no httpie - objeto de vários níveis no body e headers
 http DELETE :3000/talker/4 authorization:"375c3a2e0051b630"         // (ok)
+*/
+
+// POST - Rota para cadastrar novos talkers
+app.post('/talker',
+tokenValidation,
+nameValidation,
+ageValidation,
+talkValidation,
+// watchedAtValidation,
+// rateValidation,
+registerTalker,
+async () => {});
+/* REQUISIÇÃO
+Requisição complexa no httpie - objeto de vários níveis no body e headers
+echo '{"name": "Danielle Santos", "age": 56, "talk": { "watchedAt": "22/10/2019", "rate": 5 } }' | http POST :3000/talker authorization:"375c3a2e0051b630"
+echo '{"name": "Miley Cyrus", "age": 27, "talk": { "watchedAt": "25/09/2020", "rate": 4 } }' | http POST :3000/talker authorization:"375c3a2e0051b630"
+*/
+
+// GET - Rota para acessar o conteúdo de talkers.json
+app.get('/talker', async (_request, response) => {
+  const talkers = await getTalkers();
+  return response.status(200).json(talkers);
+}); 
+/* REQUISIÇÃO
+// http GET :3000/talker           // HTTPIE;   (ok)
+// http://localhost:3000/talker    // navegador (ok);
+*/
+
+// POST - Rota para validar um login e gerar um token;
+app.post('/login', emailValidation, passwordValidation, async (request, response) => {
+  const token = await crypto.randomBytes(8).toString('hex'); // gera um token de 6 caracteres
+  return response.status(200).json({ token: `${token}` });
+});
+/* REQUISIÇÃO
+echo '{"email":"email@email.com", "password":"123456789" }' | http POST :3000/login  // (ok) retorna o token 
+http POST :3000/login email='email@email.com' password='123456789'                   // (ok) retorna o token 
+http POST :3000/login email='' password='123456789'                                  // (error)
+http POST :3000/login email='email@email.com' password=''                            // (error)
+http POST :3000/login email='' password=''                                           // (error)
 */
 
 // GET - Rota para encontrar termos por search
