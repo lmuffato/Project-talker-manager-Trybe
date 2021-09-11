2ca5f7944c304e5503e3cd63626deeb6ee9ae9ed
lmuffato <lmiremuffato@gmail.com>
2021-09-11 20:00:49 -0300

    requisito 7

--- middlewares/searchTalker.js
+++ middlewares/searchTalker.js
@@ -2,11 +2,15 @@ const { getTalkers } = require('../functions/getTalkers');
 
 // Middleware para editar as informações de um talker
 const searchTalker = async (request, response) => {
+  try {
   const talkersDatabase = await getTalkers();
   const { q } = await request.query; // Parâmetro da query
-  const talkersFiltred = await talkersDatabase.filter((talker) => String(talker.name).includes(q));
-  if (!q) { return response.status(200).json(talkersDatabase).end(); }
-  return response.status(200).json(talkersFiltred).end(); // Envia a confirmação
+  const filtredTalkers = await talkersDatabase.filter((talker) => talker.name.toLowerCase()
+    .includes(q.toLowerCase()));
+  return response.status(200).json(filtredTalkers).end(); // Envia a confirmação
+  } catch (error) {
+    return response.status(400).json({ message: error.message });
+  }
 };
 
 module.exports = { searchTalker };
