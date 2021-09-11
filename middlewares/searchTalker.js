88a797d97eb1e9dfbe4864104bdcd46e4b9e8658
lmuffato <lmiremuffato@gmail.com>
2021-09-11 03:57:45 -0300

    requisito 7

--- middlewares/searchTalker.js
+++ middlewares/searchTalker.js
@@ -5,7 +5,7 @@ const searchTalker = async (request, response) => {
   const talkersDatabase = await getTalkers();
   const { q } = await request.query; // Parâmetro da query
   const talkersFiltred = await talkersDatabase.filter((talker) => String(talker.name).includes(q));
-  if (!q || !talkersFiltred) { return response.status(200).json(talkersDatabase).end(); }
+  if (!q) { return response.status(200).json(talkersDatabase).end(); }
   return response.status(200).json(talkersFiltred).end(); // Envia a confirmação
 };
 
