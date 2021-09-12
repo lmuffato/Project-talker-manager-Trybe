d04aa8341c423de3d1f8cfd42ae986e75a405289
ScriptCamilo <script.camilo@gmail.com>
2021-09-11 23:08:08 -0300

    feat: name validation created

--- utils/validations.js
+++ utils/validations.js
@@ -59,8 +59,27 @@ function isTokenValid(req, res, next) {
   next();
 }
 
+function isNameValid(req, res, next) {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(HTTP_BAD_REQUEST).json({
+      message: 'O campo "name" é obrigatório',
+    });
+  }
+
+  if (name.length < 3) {
+    return res.status(HTTP_BAD_REQUEST).json({
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    });
+  }
+
+  next();
+}
+
 module.exports = {
   isEmailValid,
   isPasswordValid,
   isTokenValid,
+  isNameValid,
 };
