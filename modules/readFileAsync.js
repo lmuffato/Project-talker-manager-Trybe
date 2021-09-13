e6d099ce80c28f0ec82f64de47f0f3829ed12b5e
Erick-Marinho <erickgaves@gmail.com>
2021-09-13 15:35:30 -0300

    Redefinindo nome do argumento da função

--- modules/readFileAsync.js
+++ modules/readFileAsync.js
@@ -1,8 +1,8 @@
 const fs = require('fs').promises;
 
-module.exports = async (filePath) => {
+module.exports = async (fileName) => {
   try {
-    const data = await fs.readFile(filePath);
+    const data = await fs.readFile(fileName);
     return data.toString();
   } catch (error) {
     return console.error(`Got an error trying to read the file: ${error.message}`);
