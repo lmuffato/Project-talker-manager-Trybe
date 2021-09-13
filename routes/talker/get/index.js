cc0b5a652faffc0e53b08b53106120c389d31f86
ScriptCamilo <script.camilo@gmail.com>
2021-09-13 11:19:04 -0300

    feat: query search created

--- routes/talker/get/index.js
+++ routes/talker/get/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { readFile } = require('../../../utils/handleFile');
-const { isIdValid } = require('../../../utils/validations');
+const { isIdValid, isTokenValid } = require('../../../utils/validations');
 const { HTTP_OK_STATUS } = require('../../../utils/serverStatus');
 
 router.get('/', (_req, res, next) => {
@@ -11,6 +11,26 @@ router.get('/', (_req, res, next) => {
 
     res.status(HTTP_OK_STATUS).json(data);
   } catch (err) {   
+    next(err);
+  }
+});
+
+router.get('/search', isTokenValid, (req, res, next) => {
+  try {
+    const { query: { q } } = req;
+    const data = readFile();
+    const search = data.filter(({ name }) => name.toLowerCase().includes(q.toLowerCase()));
+
+    if (!q) {
+      return res.status(HTTP_OK_STATUS).json(data);
+    }
+
+    if (!search.length) {
+      return res.status(HTTP_OK_STATUS).json([]);
+    }
+
+    res.status(HTTP_OK_STATUS).json(search);
+  } catch (err) {
     next(err);
   }
 });
