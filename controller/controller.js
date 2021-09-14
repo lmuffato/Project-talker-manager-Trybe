597813036f1fe8b93496ee59c4f24e18d4ade6bb
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-09-14 18:29:56 -0300

    starting requirements 6

--- controller/controller.js
+++ controller/controller.js
@@ -19,9 +19,26 @@ async function querySearch(req, res) {
 }
 
 async function queryPush(req, res) {
-  const { name, age, talk } = req.body;
-  talkersList.push({ name, age, talk });
+  const { id, name, age, talk } = req.body;
+  talkersList.push({ id, name, age, talk });
   res.status(HTTP_OK_CREATED).json(req.body);
+}
+
+async function queryChange(req, res) {
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+  const index = talkersList.findIndex((talker) => talker.id === +id);
+  if (index === -1) return res.status(404).json({ message: 'id not found' });
+  talkersList[index] = { id, name, age, talk };
+  res.status(204).end();
+}
+
+async function queryDelete(req, res) {
+  const { id } = req.params;
+  const index = talkersList.findIndex((talker) => talker.id === +id);
+  if (index === -1) return res.status(404).json({ message: 'id not found' });
+  talkersList.splice(index, 1);
+  res.status(204).end();
 }
 
 module.exports = {
@@ -29,4 +46,6 @@ module.exports = {
   queryId,
   fullQuery,
   queryPush,
+  queryChange,
+  queryDelete,
 };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { querySearch, queryId, fullQuery, queryPush } = require('./controller/controller');
+const { querySearch, queryId, fullQuery, queryPush, queryChange, queryDelete } = require('./controller/controller');
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,6 +17,8 @@ app.get('/talker', fullQuery);
 app.get('/talker/search', querySearch);
 app.get('/talker/:id', queryId);
 app.post('/talker', queryPush);
+app.put('/talker/:id', queryChange);
+app.delete('/talker/:id', queryDelete);
 
 app.listen(PORT, () => {
   console.log('Online');
