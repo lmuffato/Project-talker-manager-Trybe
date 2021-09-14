98a6c08fd102080b0d7e1d217b4fc7c76d3a5319
Southier <patrick.southier@hotmail.com>
2021-09-13 23:52:45 -0300

    feat: creates the /talker GET endpoint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const talkers = require('./talker.json');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,12 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  if (Array.isArray(talkers)) {
+    res.status(200).json(talkers);
+  }
 });
 
 app.listen(PORT, () => {
