942e58309b49fa60dc62cd267d6f0efc789a8a3e
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-09-14 16:41:00 -0300

    2nd requirement

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ app.get('/', (_request, response) => {
 
 app.get('/talker', (_req, res) => res.status(HTTP_OK_STATUS).json(talkersList));
 
+app.get('/talker/:id', (req, res) => {
+ const { id } = req.params;
+ const talkerFind = talkersList.find((talker) => talker.id === +id);
+ if (!talkerFind) res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(HTTP_OK_STATUS).json(talkerFind);
+ });
+
 app.listen(PORT, () => {
   console.log('Online');
 });
