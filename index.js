83c649d8fd02927a79f407305d42bb81321a8d3b
Southier <patrick.southier@hotmail.com>
2021-09-14 18:58:17 -0300

    refactor: creates middlewares to deal with POST /login endpoint validations

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const auth = require('./middlewares/authentication');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const HTTP_BAD_REQUEST = 400;
 const HTTP_NOT_FOUND = 404;
 const PORT = '3000';
-
-const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
 
 // Special Thanks to Adelino Junior T10-A, who helped me to solve the problem with the GET '/talker' endpoint and its logics
 
@@ -43,24 +41,7 @@ app.get('/talker/:id', async (req, res) => {
   res.status(HTTP_OK_STATUS).json(talker);
 });
 
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email) {
-    return res.status(HTTP_BAD_REQUEST).json({ message: 'O campo "email" é obrigatório' });
-  }
-  if (!regexEmail.test(email)) {
-    return res.status(HTTP_BAD_REQUEST)
-      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  }
-  if (!password) {
-    return res.status(HTTP_BAD_REQUEST)
-      .json({ message: 'O campo "password" é obrigatório' });
-  }
-  if (password.length < 6) {
-    return res.status(HTTP_BAD_REQUEST)
-      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-  }
+app.post('/login', auth.validateEmail, auth.validatePassword, async (req, res) => {
   res.status(HTTP_OK_STATUS).json({ token: '7mqaVRXJSp886CGr' });
 });
 
---
+++ middlewares/authentication.js
@@ -0,0 +1,33 @@
+const HTTP_BAD_REQUEST = 400;
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+  const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+
+  if (!email) {
+   return res.status(HTTP_BAD_REQUEST).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!regexEmail.test(email)) {
+   return res.status(HTTP_BAD_REQUEST)
+     .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+
+  if (!password) {
+    return res.status(HTTP_BAD_REQUEST)
+      .json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(HTTP_BAD_REQUEST)
+      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
+module.exports = {
+  validateEmail,
+  validatePassword,
+};
