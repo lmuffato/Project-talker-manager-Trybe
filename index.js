c12083fd15fa6bb98c5f16e59f77e60e4e733b82
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-09-14 18:44:42 -0300

    fixed linter

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { querySearch, queryId, fullQuery, queryPush, queryChange, queryDelete } = require('./controller/controller');
+const { querySearch, queryId, fullQuery, queryPush, queryChange, 
+  queryDelete } = require('./controller/controller');
 
 const app = express();
 app.use(bodyParser.json());
