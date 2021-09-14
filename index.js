38a5d655ac267eeba2b6a954eca9e5486cef2188
Southier <patrick.southier@hotmail.com>
2021-09-14 18:37:45 -0300

    feat: creates POST /login endpoint

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_BAD_REQUEST = 400;
 const HTTP_NOT_FOUND = 404;
 const PORT = '3000';
+
+const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
 
 // Special Thanks to Adelino Junior T10-A, who helped me to solve the problem with the GET '/talker' endpoint and its logics
 
@@ -38,6 +41,27 @@ app.get('/talker/:id', async (req, res) => {
     return res.status(HTTP_NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
   }
   res.status(HTTP_OK_STATUS).json(talker);
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(HTTP_BAD_REQUEST).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!regexEmail.test(email)) {
+    return res.status(HTTP_BAD_REQUEST)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(HTTP_BAD_REQUEST)
+      .json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(HTTP_BAD_REQUEST)
+      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  res.status(HTTP_OK_STATUS).json({ token: '7mqaVRXJSp886CGr' });
 });
 
 app.listen(PORT, () => {
