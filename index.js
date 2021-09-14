476449c67ff3001ae02b0c7f0b2e32c17cd55c5a
Erick-Marinho <erickgaves@gmail.com>
2021-09-13 23:07:10 -0300

    inserindo metodo post, importando pacote crypto-extra, redefinindo nomeclatura de funções e variáveis tendo em vista a criação de um módulo.

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const readFileAsync = require('./modules');
+const crypto = require('crypto-extra');
+const myModule = require('./modules');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 const FILE_NAME = './talker.json';
-
+// const isEmail = require('./modules');
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
 app.get('/talker', async (req, res) => {
-  const data = await readFileAsync(FILE_NAME);
+  const data = await myModule.readFileAsync(FILE_NAME);
   if (!data) {
     return res.status(HTTP_OK_STATUS).json([]);
   }
@@ -23,7 +25,7 @@ app.get('/talker', async (req, res) => {
 });
 
 app.get('/talker/:id', async (req, res) => {
-  const data = await readFileAsync(FILE_NAME);
+  const data = await myModule.readFileAsync(FILE_NAME);
   const talkersId = await Number(req.params.id);
   const getTalker = await JSON.parse(data).find((talker) => talker.id === talkersId);
 
@@ -32,6 +34,21 @@ app.get('/talker/:id', async (req, res) => {
   }
 
   res.status(HTTP_OK_STATUS).send(getTalker);
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.query;
+  const token = crypto.randomKey(16);
+  const validateEmail = myModule.validateEmail(email);
+  const validatePassword = myModule.validatePassword(password);
+  if (validateEmail) {
+    return res.status(400).json(validateEmail);
+  } 
+  if (validatePassword) {
+    return res.status(400).json(validatePassword);
+  }
+  
+  res.status(HTTP_OK_STATUS).json({ token, email, password });
 });
 
 app.listen(PORT, () => {
