87816e5b0126ec0bf7bfb5986ef82d81f1288d2c
Southier <patrick.southier@hotmail.com>
2021-09-14 17:48:04 -0300

    feat: creates GET /talker/id endpoint with id as a parameter

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND = 404;
 const PORT = '3000';
 
 // Special Thanks to Adelino Junior T10-A, who helped me to solve the problem with the GET '/talker' endpoint and its logics
@@ -21,11 +22,22 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/talker', async (_req, res) => {
-  const talkerPerson = await readFile();
-  if (talkerPerson.length === 0) {
+  const talkers = await readFile();
+  if (talkers.length === 0) {
     return res.status(HTTP_OK_STATUS).json([]);
   }
-  res.status(HTTP_OK_STATUS).json(talkerPerson);
+  res.status(HTTP_OK_STATUS).json(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFile();
+  const talker = talkers.find((t) => t.id === parseInt(id, 10));
+
+  if (!talker) {
+    return res.status(HTTP_NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(HTTP_OK_STATUS).json(talker);
 });
 
 app.listen(PORT, () => {
