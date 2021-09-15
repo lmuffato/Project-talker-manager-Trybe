6869ac47125a10819f309ff920e3d08b2428e7d8
Southier <patrick.southier@hotmail.com>
2021-09-15 02:39:11 -0300

    refactor: creates a httpStatus file at helper folder and improve the validateLogin file spacements

---
+++ helper/httpStatus.js
@@ -0,0 +1,13 @@
+const OK_STATUS = 200;
+const CREATED = 201;
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+const NOT_FOUND = 404;
+
+module.exports = {
+  OK_STATUS,
+  CREATED,
+  BAD_REQUEST,
+  UNAUTHORIZED,
+  NOT_FOUND,
+};
--- middlewares/validateLogin.js
+++ middlewares/validateLogin.js
@@ -1,15 +1,19 @@
-const HTTP_BAD_REQUEST = 400;
+const http = require('../helper/httpStatus');
+
 const validateEmail = (req, res, next) => {
   const { email } = req.body;
   const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
 
   if (!email) {
-   return res.status(HTTP_BAD_REQUEST).json({ message: 'O campo "email" é obrigatório' });
+   return res.status(http.BAD_REQUEST)
+    .json({ message: 'O campo "email" é obrigatório' });
   }
+
   if (!regexEmail.test(email)) {
-   return res.status(HTTP_BAD_REQUEST)
+   return res.status(http.BAD_REQUEST)
      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
+
   next();
 };
 
@@ -17,13 +21,15 @@ const validatePassword = (req, res, next) => {
   const { password } = req.body;
 
   if (!password) {
-    return res.status(HTTP_BAD_REQUEST)
+    return res.status(http.BAD_REQUEST)
       .json({ message: 'O campo "password" é obrigatório' });
   }
+
   if (password.length < 6) {
-    return res.status(HTTP_BAD_REQUEST)
+    return res.status(http.BAD_REQUEST)
       .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
+
   next();
 };
 
