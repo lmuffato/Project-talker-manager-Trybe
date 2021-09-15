24dc77457622039b152e00d0cc66b59ee21e0369
jlfagundes <jlfagundes@gmail.com>
2021-09-15 12:32:09 -0300

    feat: initial structure base EndPoint /talker

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = '3000';
+const PORT = '3001';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', (_req, res) => res.status(200).send({ message: 'hello word!' }));
 
 app.listen(PORT, () => {
   console.log('Online');
