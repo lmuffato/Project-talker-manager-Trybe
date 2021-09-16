c91d7b7a5b4fde1fade4fbf8965bd2873c1f599c
Ederson S <edersonrodrigo31@gmail.com>
2021-09-16 14:48:15 -0300

    feat: requisito 2

--- routes/talker.js
+++ routes/talker.js
@@ -3,10 +3,21 @@ const fs = require('fs').promises;
 
 const talkerRouter = express.Router();
 const HTTP_OK_STATUS = 200;
+const NOT_FOUND_STATUS = 404;
 
 talkerRouter.get('/', async (_request, response) => {
   const data = await fs.readFile('talker.json');
   response.status(HTTP_OK_STATUS).send(JSON.parse(data));
+});
+
+talkerRouter.get('/:id', async (_request, response) => {
+  const data = await fs.readFile('talker.json');
+  const jsonData = JSON.parse(data);
+  const findUser = jsonData.find((user) => user.id === Number(_request.params.id));
+  if (!findUser) {
+    response.status(NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  response.status(HTTP_OK_STATUS).send(findUser);
 });
 
 module.exports = talkerRouter;
