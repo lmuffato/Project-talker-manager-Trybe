e07750f2c7c6fac4a5f479b6cc753e32c0ad7706
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2021-09-16 17:47:58 -0300

    Req 1, finalmente!

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,13 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const data = await fs.readFile('./talker.json', 'utf-8');
+  const fetchData = await JSON.parse(data);
+
+  res.status(HTTP_OK_STATUS).json(fetchData);
 });
 
 app.listen(PORT, () => {
