4328aed5de8488a94313b13c67b5326a238e42ec
Ederson S <edersonrodrigo31@gmail.com>
2021-09-16 13:24:00 -0300

    feat: requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,17 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_request, response) => {
+  fs.readFile('talker.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Não foi possível ler o arquivo ${'talker.json'}\n Erro: ${err}`);
+      response.status(400).send();
+      process.exit(1);
+    }
+    response.status(HTTP_OK_STATUS).send(data);
+  });
 });
 
 app.listen(PORT, () => {
