505d35111fe7bfa10755ad58153ca384a8ea8c31
Erick-Marinho <erickgaves@gmail.com>
2021-09-17 01:35:38 -0300

    Inserindo novo modulo

--- modules/index.js
+++ modules/index.js
@@ -1,9 +1,11 @@
 const readFileAsync = require('./readFileAsync');
+const writeFileAsync = require('./writeFileAsync');
 const validateEmail = require('./validateEmail');
 const validatePassword = require('./validatePassword');
 
 module.exports = {
   readFileAsync,
+  writeFileAsync,
   validateEmail,
   validatePassword,
 };
