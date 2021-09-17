a2953ea4959ddba81e7e39927c63147b00af78b5
lucas-chave <lcchavesr150@gmail.com>
2021-09-17 10:13:08 -0300

    requirement 4, adding talker field validation

--- addTalker.js
+++ addTalker.js
@@ -70,10 +70,26 @@ const validateRateTalker = (req, res, next) => {
   next();
 };
 
+const validateTalker = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (!talk) {
+    return (res
+      .status(400)
+        .json(
+          { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
+        )
+    );
+  }
+
+  next();
+};
+
 router.post('/',
   validateTokenTalker,
   validateNameTalker,
   validateAgeTalker,
+  validateTalker,
   validateWatchedAtTalker,
   validateRateTalker,
   async (req, res) => {
