48c9a3e041b1ddd5b4ef5f3514481581171a382e
Erick-Marinho <erickgaves@gmail.com>
2021-09-17 01:35:01 -0300

    Criando método POST

--- routers/talkers.js
+++ routers/talkers.js
@@ -6,12 +6,12 @@ const HTTP_OK_STATUS = 200;
 const HTTP_BAD_REQUEST = 404;
 const FILE_NAME = './talker.json';
 
-talkers.get('', async (req, res) => {
+talkers.get('', async (_req, res) => {
   const data = await myModule.readFileAsync(FILE_NAME);
   if (!data) {
     return res.status(HTTP_OK_STATUS).json([]);
   }
-  res.status(HTTP_OK_STATUS).send(JSON.parse(data));
+  res.status(HTTP_OK_STATUS).send(data);
 });
 
 talkers.get('/:id', async (req, res) => {
@@ -24,6 +24,22 @@ talkers.get('/:id', async (req, res) => {
   }
 
   res.status(HTTP_OK_STATUS).send(getTalker);
+});
+
+talkers.post('', async (req, res) => {
+  const { name, age, talk } = req.body;
+  const data = await myModule.readFileAsync(FILE_NAME);
+  const newTalk = {
+    name,
+    age,
+    talk: {
+      wathedAt: talk.watchedAt,
+      rate: talk.rate,
+    },
+  };
+  await data.push(newTalk);
+  const result = await myModule.writeFileAsync(FILE_NAME, JSON.stringify(data));
+  res.status(201).json(result); 
 });
 
 module.exports = talkers;
