c1ec7a3cbbb1ca0a33cd024d769a27d0a1841e3f
Erick-Marinho <erickgaves@gmail.com>
2021-09-17 01:36:21 -0300

    Alterando funcao para manipular dados

--- modules/readFileAsync.js
+++ modules/readFileAsync.js
@@ -2,8 +2,8 @@ const fs = require('fs').promises;
 
 module.exports = async (fileName) => {
   try {
-    const data = await fs.readFile(fileName);
-    return data.toString();
+    const data = await fs.readFile(fileName, 'utf-8');
+    return JSON.parse(data);
   } catch (error) {
     return console.error(`Got an error trying to read the file: ${error.message}`);
   }
