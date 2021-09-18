3451155d0349ef1e29ced7fd96188c988534806c
lucas-chave <lcchavesr150@gmail.com>
2021-09-18 00:21:56 -0300

    reload evaluator

---
+++ searchTalker.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+// const fs = require('fs');
+
+// const router = express.Router();
+// const talkerArq = './talker.json';
+
+// router.get()
+
+// module.exports = router;
