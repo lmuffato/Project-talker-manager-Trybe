f8a272950ca289c5eb36a5143556416bd7f770c4
marcela <marcela9908@gmail.com>
2021-09-18 15:33:52 -0300

    Requisito 3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,6 +9,17 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const HTTP_NOT_FOUND = 404;
 const PORT = '3000';
+const HTTP_BAD_REQUEST = 400;
+
+function verificaEmail(email) {
+  const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+  return emailRegex.test(email);
+}
+
+function geraToken() {
+  return crypto.randomBytes(8)
+ .toString('hex');
+}
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -39,6 +51,28 @@ app.get('/talker/:id', (request, response) => {
   return talkerId
   ? response.status(HTTP_OK_STATUS).json(talkerId)
   : response.status(HTTP_NOT_FOUND).json(messageError);
+});
+
+// Requisito 3
+app.post('/login', (request, response) => {
+  const token = geraToken();
+  const { email, password } = request.body;
+  if (email === undefined) {
+    return response.status(HTTP_BAD_REQUEST).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (verificaEmail(email) === false) {
+   return response.status(HTTP_BAD_REQUEST)
+    .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (password === undefined) {
+   return response.status(HTTP_BAD_REQUEST)
+    .json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.toString().length < 6) {
+   return response.status(HTTP_BAD_REQUEST)
+    .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+ return response.status(HTTP_OK_STATUS).json({ token });
 });
 
 app.listen(PORT, () => {
