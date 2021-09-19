769aefde837aa416ad6c9844cd3ebf0f3de3a287
Erick-Marinho <erickgaves@gmail.com>
2021-09-19 20:34:03 -0300

    Criando index para exportação de tarefas

---
+++ middlewares/tasks/index.js
@@ -0,0 +1,17 @@
+const getAllTalkers = require('./getAllTalkers');
+const getTalkerId = require('./getTalkerId');
+const searchTalkers = require('./searchTalkers');
+const createTalker = require('./createTalker');
+const editTalker = require('./editTalker');
+const deleteTalker = require('./deleteTalker');
+const createLogin = require('./createLogin');
+
+module.exports = {
+  getAllTalkers,
+  getTalkerId,
+  searchTalkers,
+  createTalker,
+  editTalker,
+  deleteTalker,
+  createLogin,
+};
