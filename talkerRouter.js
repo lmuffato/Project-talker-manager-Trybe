49fdb4f2e34f0fb4dbc55dc77ed3df31e66066e4
EdmilsonBernardo <ebedmilson48@gmail.com>
2021-09-19 17:37:56 -0300

    fix: requirement 4...2x

--- talkerRouter.js
+++ talkerRouter.js
@@ -11,8 +11,8 @@ const validateToken = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (!token) res.status(401).json({ message: 'Token não encontrado' });
-  if (token !== 16) res.status(401).json({ message: 'Token inválido' });
-  req.token = token;
+  if (token.length !== 16) res.status(401).json({ message: 'Token inválido' });
+
   next();
 };
 
@@ -20,7 +20,7 @@ const validateName = (req, res, next) => {
   const { name } = req.body;
 
   if (!name) res.status(400).json({ message: 'O campo "name" é obrigatório' });
-  if (name.lenght < 3) {
+  if (name.length < 3) {
     return res.status(400).json({
       message: 'O "name" deve ter pelo menos 3 caracteres',
     });
@@ -33,7 +33,7 @@ const validateAge = (req, res, next) => {
   const { age } = req.body;
 
   if (!age) res.status(400).json({ message: 'O campo "age" é obrigatório' });
-  if (age < 18) {
+  if (!Number.isInteger(age) || age < 18) {
     return res.status(400).json({
       message: 'A pessoa palestrante deve ser maior de idade',
     });
@@ -45,7 +45,7 @@ const validateAge = (req, res, next) => {
 const ValidateDate = (req, res, next) => {
   const { talk } = req.body;
 
-  if (!moment(talk.watchedAt, 'DD/MM/YYYY').isValid()) {
+  if (!moment(talk.watchedAt, 'DD/MM/YYYY', true).isValid()) {
     return res.status(400).json({
       message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
     });
@@ -63,7 +63,7 @@ const validateRate = (req, res, next) => {
     });
   }
 
-  if (talk.rate < 1 && talk.rate > 5) {
+  if (talk.rate < 1 || talk.rate > 5) {
     return res.status(400).json({
       message: 'O campo "rate" deve ser um inteiro de 1 à 5',
     });
@@ -109,17 +109,17 @@ talkerRouter.post('/',
   async (req, res) => {
     const { name, age, talk } = req.body;
     const data = await fs.readFile('./talker.json', 'utf-8');
-    const fetchData = await JSON.parse(data);
-    const newData = {
-      id: fetchData.length + 1,
+    const talkers = await JSON.parse(data);
+    const newTalker = {
+      id: talkers.length + 1,
       name,
       age,
       talk,
     };
-    fetchData.push(newData);
-    await fs.writeFile('./talker.json', JSON.stringify(fetchData));
+    talkers.push(newTalker);
+    await fs.writeFile('./talker.json', JSON.stringify(talkers));
   
-    return res.status(201).json(newData);
+    return res.status(201).json(newTalker);
   });
 
 module.exports = talkerRouter;
