366bbf84ad2e3e62a0d5324ff86f95f0c1953494
Erick-Marinho <erickgaves@gmail.com>
2021-09-19 20:29:16 -0300

    Realocação de arquivos em diretorio específico

--- middlewares/validateTalk.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-  const { talk } = req.body;
-  if (!talk || talk.watchedAt === undefined || talk.rate === undefined) {
-    return res.status(400).send({ 
-      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
-    });
-  }
-  next();
-};
