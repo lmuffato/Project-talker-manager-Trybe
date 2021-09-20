53585444d4219e27bb599b24002e4f696aca5edb
EdmilsonBernardo <ebedmilson48@gmail.com>
2021-09-20 11:10:07 -0300

    fix: errors

--- talkerRouter.js
+++ talkerRouter.js
@@ -12,8 +12,8 @@ const getData = async () => fs.readFile(talkersFile, 'utf-8')
 const validateToken = (req, res, next) => {
   const token = req.headers.authorization;
 
-  if (!token) res.status(401).json({ message: 'Token não encontrado' });
-  if (token.length !== 16) res.status(401).json({ message: 'Token inválido' });
+  if (!token) return res.status(401).json({ message: 'Token não encontrado' });
+  if (token.length !== 16) return res.status(401).json({ message: 'Token inválido' });
 
   next();
 };
@@ -21,7 +21,7 @@ const validateToken = (req, res, next) => {
 const validateName = (req, res, next) => {
   const { name } = req.body;
 
-  if (!name) res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
   if (name.length < 3) {
     return res.status(400).json({
       message: 'O "name" deve ter pelo menos 3 caracteres',
@@ -34,7 +34,7 @@ const validateName = (req, res, next) => {
 const validateAge = (req, res, next) => {
   const { age } = req.body;
 
-  if (!age) res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
   if (!Number.isInteger(age) || age < 18) {
     return res.status(400).json({
       message: 'A pessoa palestrante deve ser maior de idade',
@@ -95,7 +95,9 @@ talkerRouter.get('/search', validateToken, async (req, res) => {
   const { q: term } = req.query;
   const talkers = await getData();
 
-  if (!term || term === '') res.status(200).json(talkers);
+  if (!term || term === '') {
+    return res.status(200).json(talkers);
+  }
 
   const results = talkers.filter((talker) => talker.name.includes(term));
 
