25a41f7634e9bd3c542cf88272d4fbfbc09c8818
Erick-Marinho <erickgaves@gmail.com>
2021-09-20 13:14:13 -0300

    Atribuindo desestruturação para acessar método validateToken

--- routers/talkers.js
+++ routers/talkers.js
@@ -3,12 +3,13 @@ const tasks = require('../middlewares/tasks');
 const validations = require('../middlewares/validations');
 
 const talkers = express.Router();
+const [validateToken] = validations;
 
 talkers.get('/', tasks.getAllTalkers);
-talkers.get('/search', validations[0]);
+talkers.get('/search', validateToken);
 talkers.get('/:id', tasks.getTalkerId);
 talkers.post('/', ...validations, tasks.createTalker);
 talkers.put('/:id', ...validations, tasks.editTalker);
-talkers.delete('/:id', validations[0], tasks.deleteTalker);
+talkers.delete('/:id', validateToken, tasks.deleteTalker);
 
 module.exports = talkers;
