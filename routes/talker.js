dd3703d83384b57c6e0823635df84ff251dc5c2e
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-09-21 09:44:43 -0300

    requisito 5

--- routes/talker.js
+++ routes/talker.js
@@ -30,16 +30,36 @@ validateWatched, async (req, res) => {
   const { name, age, talk: { watchedAt, rate } } = req.body;
   const talkers = await manageTalkersFile();
   const newTalker = {
-  id: (talkers.length + 1),
-  name,
-  age: parseInt(age, 10),
-  talk: {
-    watchedAt,
-    rate: parseInt(rate, 10),
+    id: (talkers.length + 1),
+    name,
+    age: parseInt(age, 10),
+    talk: {
+      watchedAt,
+      rate: parseInt(rate, 10),
   } };
   talkers.push(newTalker);
   await fs.writeFile('./talker.json', JSON.stringify(talkers));
   res.status(CREATE).json(newTalker);
+});
+
+router.put('/:id', 
+validateToken,
+validateAge,
+validateTalk,
+validateName,
+validateRate,
+validateWatched, async (req, res) => {
+  const { id } = req.params;
+  const { name, age, talk: { watchedAt, rate } } = req.body;
+  const talkers = await manageTalkersFile();
+  const talkerIndex = talkers.findIndex((talker) => talker.id === parseInt(id, 10));
+  if (talkers[talkerIndex] !== -1) {
+    talkers[talkerIndex] = { ...talkers[talkerIndex], name, age, talk: { watchedAt, rate } };
+    const filterOutdatedInfo = talkers.filter((talker) => talker.id !== parseInt(id, 10));
+    filterOutdatedInfo.push(talkers[talkerIndex]);
+    await fs.writeFile('./talker.json', JSON.stringify(filterOutdatedInfo), 'utf-8');
+    res.status(OK).json(talkers[talkerIndex]);
+  }
 });
 
 module.exports = router;
--- utils/validateRate.js
+++ utils/validateRate.js
@@ -4,10 +4,10 @@ const { BAD_REQUEST } = require('./status');
 const validateRate = (req, res, next) => {
   const { talk } = req.body;
   const { rate } = talk;
-  if (!rate || rate === undefined) return res.status(BAD_REQUEST).json({ message: TALK });
   if (rate < 1 || rate > 5) {
     return res.status(BAD_REQUEST).json({ message: RATE });
   }
+  if (!rate || rate === undefined) return res.status(BAD_REQUEST).json({ message: TALK });
   next();
 };
 
