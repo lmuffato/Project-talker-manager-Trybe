8403c5766dfac2fe46c3683332636503fdf9c707
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-21 16:31:39 -0400

    feat: route /login w/validations

--- routes/talker.js
+++ routes/talker.js
@@ -3,13 +3,16 @@ const STATUS = require('../status/http_status');
 const middle = require('../middlewares/talker');
 
 const router = express.Router();
+const { getAllTalker, getSortedTalker, validateEmail, validatePassword, generateToken } = middle;
 
 router.get('/', (_req, res) => {
     res.status(STATUS.SUCCESS.OK).send('Hello World 🚀👩‍🚀');
 });
 
-router.get('/talker', middle.getAllTalker);
+router.get('/talker', getAllTalker);
 
-router.get('/talker/:id', middle.getSortedTalker);
+router.get('/talker/:id', getSortedTalker);
+
+router.post('/login', validateEmail, validatePassword, generateToken);
 
 module.exports = router;
