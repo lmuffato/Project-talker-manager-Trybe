7eb9a8edecb3303293e7821a49eafc10adc3ded2
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-21 16:08:47 -0400

    feat: middlewares to validate email, password and generate token

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/talker');
-const STATUS = require('./status/http_status');
 
 const app = express();
 app.use(bodyParser.json());
--- middlewares/talker.js
+++ middlewares/talker.js
@@ -2,6 +2,8 @@ const fs = require('fs').promises;
 
 const STATUS = require('../status/http_status');
 
+const gen = () => Math.random().toString(36).substr(4);
+
 const convertFromJSON = async () => {
   const rawTalkers = (await fs.readFile('./talker.json')).toString('utf-8');
   const talkers = JSON.parse(rawTalkers);
@@ -24,7 +26,44 @@ const getSortedTalker = async (req, res, next) => {
   }
 };
 
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+  const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!email) {
+    res.status(STATUS.ERROR.BAD_REQUEST).send({
+      message: 'O campo "email" é obrigatório',
+    });
+  } else if (!validation.test(email)) {
+    res.status(STATUS.ERROR.BAD_REQUEST).send({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    res.status(STATUS.ERROR.BAD_REQUEST).send({
+      message: 'O campo "password" é obrigatório',
+    });
+  } else if (password < 6) {
+    res.status(STATUS.ERROR.BAD_REQUEST).send({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
+  }
+  next();
+};
+
+const generateToken = (_req, res) => {
+  const token = `${gen()}${gen()}`;
+  res.status(STATUS.SUCCESS.OK).send({ token });
+};
+
 module.exports = {
     getAllTalker,
     getSortedTalker,
+    validateEmail,
+    validatePassword,
+    generateToken,
 };
