90de568093d59fb464c7b94452d7a410ce36e74c
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-09-20 21:21:02 -0300

    requisito 2

---
+++ endpoints/get-talker-id.js
@@ -0,0 +1,16 @@
+const manageTalkers = require('../utils/manageTalkers');
+const { OK, NOT_FOUND } = require('../utils/status');
+
+const getTalkerById = async (req, res) => {
+  const { id } = req.params;
+
+  const getTalkers = await manageTalkers();
+
+  const soughtTalker = getTalkers.find((talker) => talker.id === parseInt(id, 10));
+
+  if (soughtTalker) return res.status(OK).json(soughtTalker);
+
+  return res.status(NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
+};
+
+module.exports = getTalkerById;
---
+++ utils/status.js
@@ -0,0 +1,9 @@
+const OK = 200;
+const CREATE = 201;
+const NOT_FOUND = 404;
+
+module.exports = {
+  OK,
+  CREATE,
+  NOT_FOUND,
+};
