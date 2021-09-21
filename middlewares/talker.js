edf5ce8a17a2127149642168b6e8cdbd45bb258d
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-21 15:15:44 -0400

    feat: filter talker by id middleware

--- middlewares/talker.js
+++ middlewares/talker.js
@@ -2,11 +2,28 @@ const fs = require('fs').promises;
 
 const STATUS = require('../status/http_status');
 
+const convertFromJSON = async () => {
+  const rawTalkers = (await fs.readFile('./talker.json')).toString('utf-8');
+  const talkers = JSON.parse(rawTalkers);
+  return talkers;
+};
+
 const getAllTalker = async (_req, res) => {
-  const talker = (await fs.readFile('./talker.json')).toString('utf-8');
+  const talker = await convertFromJSON();
   res.status(STATUS.SUCCESS.OK).send(talker);
+};
+
+const getSortedTalker = async (req, res) => {
+  const talkers = await convertFromJSON();
+  const { id } = req.params;
+  const filterdTalker = talkers.filter((talker) => talker.id === id);
+  if (filterdTalker.length === 0) {
+    res.status(STATUS.SUCCESS.NO_CONTENT).send('Nenhum resultado encontrado');
+  }
+  res.status(STATUS.SUCCESS.OK).send(filterdTalker);
 };
 
 module.exports = {
     getAllTalker,
+    getSortedTalker,
 };
