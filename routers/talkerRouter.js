1bfd35cacc7e2391c8e76e96ee8cac745af9be76
Junior Portela <portelajr14@gmail.com>
2021-09-22 02:26:57 -0300

    req2: endpoint com parametro id entrega arquivo filtrado

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -5,6 +5,7 @@ const fs = require('fs').promises;
 // const talker = require('../talker.json');
 
 const STATUS_OK = 200;
+const NOT_FOUND = 400;
 
 const getAll = async () => {
   const allTalkers = await fs.readFile('./talker.json', 'utf-8');
@@ -13,11 +14,22 @@ const getAll = async () => {
   return JSON.parse(allTalkers);
 };
 
+const getTalkerById = async (id) => {
+  const allTalkers = await getAll();
+  const talkerById = allTalkers.find((t) => t.id === parseInt(id, Number));
+  return talkerById; 
+};
+
 router.get('/', async (_req, res) => {
   const talkers = await getAll();
   res.status(STATUS_OK).json(talkers);
 });
 
-router.get('/:id', async (req, res) => {});
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const getTalker = await getTalkerById(id);
+  if (!getTalker) res.status(NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(STATUS_OK).json(getTalker);
+});
 
 module.exports = router;
