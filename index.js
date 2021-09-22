546def48cc17fa1a4d2784a5a38a48db5b55af4b
Marcos Paulo da Silva Albuquerque <li0000@li0000.mpdsa>
2021-09-22 07:00:33 -0300

    error connect in local

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
 
+const fileTalker = fs.readFileSync('./talker.json');
+const HTTP_OK_STATUS = 200;
+const PORT = 3000;
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
-const HTTP_OK_STATUS = 200;
-const PORT = '3000';
+app.listen(PORT, () => {
+  console.log('Online');
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
+app.get('/talker', (req, res) => {
+  res.status(HTTP_OK_STATUS).send(fileTalker);
 });
+
+// app.get('/talker/:id', (req, res) => {
+//   // const { id } = req.params;
+  
+//   const test = fs.readFileSync('./talker.json');
+//   const result = test.toString();
+//   const resultJSON = result;
+//   res.status(HTTP_OK_STATUS).send(resultJSON);
+// });
