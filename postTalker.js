b0ec1163cf6e5bbe3233cf4a2e0c3eddfdeb747c
jlfagundes <jlfagundes@gmail.com>
2021-09-22 00:07:32 -0300

    fix: check rate

--- postTalker.js
+++ postTalker.js
@@ -67,7 +67,7 @@ const checkWatchedAt = (req, res, next) => {
 
 const checkRate = (req, res, next) => {
   const { talk } = req.body;
-  if (talk.rate > 1 || talk.rate < 5) {
+  if (talk.rate < 1 || talk.rate > 5) {
     return res.status(HTTP_BAD_REQUEST_STATUS)
       .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
