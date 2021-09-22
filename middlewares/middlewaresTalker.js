e62b83935e0b206ecb208b078f6052ed65e8f10a
Junior Portela <portelajr14@gmail.com>
2021-09-22 05:01:28 -0300

    fix: ordem  middeware validateRate

--- middlewares/middlewaresTalker.js
+++ middlewares/middlewaresTalker.js
@@ -53,15 +53,17 @@ const validateTalk = (req, res, next) => {
   
 const validateRate = (req, res, next) => {
   const { talk: { rate } } = req.body;
+
+  if (rate < 1 || rate > 5) {
+    res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
   if (!rate) {
     res.status(400).json(
       { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
     );
   }
 
-  if (rate < 1 || rate > 5) {
-    res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-  }
   next();
 };
 
