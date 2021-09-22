90ec5a9160f56cd5c6286a7885d7e8b9683dbd79
Marcos Albuquerque <mpdsa20@gmail.com>
2021-09-22 09:17:45 -0300

    Organizando Endpoints + Req 1

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
 
-const app = express();
-app.use(bodyParser.json());
-
+const fileTalker = fs.readFileSync('./talker.json');
+const talkerJSON = JSON.parse(fileTalker);
+const PORT = process.env.PORT || 3000;
 const HTTP_OK_STATUS = 200;
-const PORT = '3000';
+const app = express();
 
-// não remova esse endpoint, e para o avaliador funcionar
+app.use(bodyParser.json());
+app.use(cors());
+app.listen(PORT, () => console.log('Online na porta: %s', PORT));
+
+/**
+ * ENDPOINTS
+ */
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
+app.get('/talker', (req, res) => {
+  res.send(talkerJSON);
 });
+
+app.get('/talker/:id', () => {});
+
+app.post('/login', () => {});
+
+app.post('/talker', () => {});
+
+app.put('/talker/:id', () => {});
+
+app.delete('/talker/:id', () => {});
+
+app.get('/talker/search?q=searchTerm', () => {});
