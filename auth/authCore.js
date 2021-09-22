2c09f0ee250292d74cb6252ee6c8892b112321d6
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-21 21:42:18 -0400

    feat: authCore, saves generate token and save

---
+++ auth/authCore.js
@@ -0,0 +1,15 @@
+const fs = require('fs').promises;
+
+const gen = () => Math.random().toString(36).substr(4);
+
+const generateToken = async () => {
+  const newToken = `${gen()}${gen()}`;
+  const tokens = JSON.parse((await fs.readFile('./auth/validTokens.json')).toString('utf-8'));
+  tokens.push(newToken);
+  await fs.writeFile('./auth/validTokens.json', JSON.stringify(tokens));
+  return newToken;
+};
+
+module.exports = {
+    generateToken,
+}
