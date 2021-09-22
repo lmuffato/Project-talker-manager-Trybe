fa446dab98bd70bedb8dabf2f172199b16604a00
marcela <marcela9908@gmail.com>
2021-09-22 08:07:26 -0300

    Ultimos requisitos

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const crypto = require('crypto');
 const path = require('path');
-const joi = require('joi');
+const { writeFileSync } = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,7 +11,6 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const HTTP_NOT_FOUND = 404;
 const HTTP_BAD_REQUEST = 400;
-const HTTP_CREATED = 201;
 const PORT = '3000';
 
 const caminhoTalker = path.resolve(__dirname, './', 'talker.json');
@@ -23,10 +22,10 @@ function verificaEmail(email) {
 
 function geraToken() {
   return crypto.randomBytes(8)
- .toString('hex');
+    .toString('hex');
 }
 
-const validaToken = (request, response, next) => {
+const validateToken = (request, response, next) => {
   const { authorization } = request.headers;
   if (!authorization) {
     return response.status(401).json({ message: 'Token não encontrado' });
@@ -37,50 +36,70 @@ const validaToken = (request, response, next) => {
   next();
 };
 
-const verificaTalker = joi
-  .object({
-    name: joi
-      .string()
-      .min(3)
-      .messages({ 'string.min': 'O "name" deve ter pelo menos 3 caracteres' })
-      .required(),
-    age: joi
-      .number()
-      .integer()
-      .min(18)
-      .message('A pessoa palestrante deve ser maior de idade')
-      .required(),
-    talk: joi
-      .object({
-        watchedAt: joi
-          .string()
-          .pattern(/^\d{2}\/\d{2}\/\d{4}$/)
-          .message('O campo "watchedAt" deve ter o formato "dd/mm/aaaa"')
-          .required(),
-        rate: joi
-          .number()
-          .integer()
-          .min(1)
-          .max(5)
-          .messages({
-            'number.min': 'O campo "rate" deve ser um inteiro de 1 à 5',
-            'number.max': 'O campo "rate" deve ser um inteiro de 1 à 5',
-          })
-          .required(),
-      })
-      .required()
-      .messages({
-        'any.required':
-          'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
-      }),
-  })
-  .messages({
-    'any.required': 'O campo {#label} é obrigatório',
-  }); 
+const validateName = (req, res, next) => {
+  const { name } = req.body;
+  if (name === undefined || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+const validateAge = (req, res, next) => {
+  const { age } = req.body;
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+const validateTalk = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk || talk.rate === undefined || !talk.watchedAt) {
+    return res.status(400).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt"'
+        + ' e "rate" não podem ser vazios',
+    });
+  }
+  next();
+};
+
+const validateWatchedAt = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+  const re = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
+  if (!re.test(watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+};
+
+const validateRate = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+  if (rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(HTTP_OK_STATUS).json();
+});
+
+// Requisito 7
+app.get('/talker/search', validateToken, async (request, response) => {
+  const { query: { q } } = request;
+  const content = await fs.readFile(caminhoTalker, 'utf-8');
+  const talkers = JSON.parse(content);
+  const talkersFilters = talkers.filter((e) => e.name.toUpperCase().includes(q.toUpperCase()));
+  if (q === undefined || q === '') {
+   return response.status(HTTP_OK_STATUS).json(talkers);
+  }
+  return response.status(HTTP_OK_STATUS)
+  .json(talkersFilters);
 });
 
 // Requisito 1
@@ -92,9 +111,9 @@ app.get('/talker', (_request, response) => {
     response.status(HTTP_OK_STATUS).json([]);
   }
 
-/*   if (data === []) {
-    response.status(HTTP_OK_STATUS).json([]);
-  }response.status(HTTP_OK_STATUS).json(data); */
+  /*   if (data === []) {
+      response.status(HTTP_OK_STATUS).json([]);
+    }response.status(HTTP_OK_STATUS).json(data); */
 });
 
 // Requisito 2
@@ -103,11 +122,11 @@ app.get('/talker/:id', (request, response) => {
   const data = JSON.parse(fs.readFileSync('talker.json', 'utf8'));
   const { id } = request.params;
   const talkerId = data.find((talker) => talker.id === Number(id));
-   const messageError = { message: 'Pessoa palestrante não encontrada' };
-  
+  const messageError = { message: 'Pessoa palestrante não encontrada' };
+
   return talkerId
-  ? response.status(HTTP_OK_STATUS).json(talkerId)
-  : response.status(HTTP_NOT_FOUND).json(messageError);
+    ? response.status(HTTP_OK_STATUS).json(talkerId)
+    : response.status(HTTP_NOT_FOUND).json(messageError);
 });
 
 // Requisito 3
@@ -118,33 +137,61 @@ app.post('/login', (request, response) => {
     return response.status(HTTP_BAD_REQUEST).json({ message: 'O campo "email" é obrigatório' });
   }
   if (verificaEmail(email) === false) {
-   return response.status(HTTP_BAD_REQUEST)
-    .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    return response.status(HTTP_BAD_REQUEST)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   if (password === undefined) {
-   return response.status(HTTP_BAD_REQUEST)
-    .json({ message: 'O campo "password" é obrigatório' });
+    return response.status(HTTP_BAD_REQUEST)
+      .json({ message: 'O campo "password" é obrigatório' });
   }
   if (password.toString().length < 6) {
-   return response.status(HTTP_BAD_REQUEST)
-    .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    return response.status(HTTP_BAD_REQUEST)
+      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
- return response.status(HTTP_OK_STATUS).json({ token });
+  return response.status(HTTP_OK_STATUS).json({ token });
 });
 
 // Requisito 4
-app.post('/talker', validaToken, async (request, response) => {
+app.post('/talker',
+validateName, validateToken, validateTalk, validateAge, validateRate,
+  validateWatchedAt,
+  async (request, response) => {
+    const { body } = request;
+    const content = await fs.readFile(caminhoTalker, 'utf-8');
+    const talkers = JSON.parse(content);
+    const newTalker = { id: talkers[talkers.length - 1].id + 1, ...body };
+    const allTalkers = [...talkers, newTalker];
+    await fs.writeFile(caminhoTalker, JSON.stringify(allTalkers, null, 2), 'utf-8');
+    if (newTalker) {
+      return response.status(201).json(newTalker);
+    }
+  });
+
+  // Requisito 5
+app.put('/talker/:id', 
+validateName, validateToken, validateTalk, validateAge, validateRate,
+  validateWatchedAt,
+async (request, response) => {
+  const { id } = request.params;
   const { body } = request;
-  if (verificaTalker.validate(body).error) {
-  return response.status(HTTP_BAD_REQUEST)
-  .json({ message: verificaTalker.validate(body).error.details[0].message });
-  } 
   const content = await fs.readFile(caminhoTalker, 'utf-8');
-  const talkers = JSON.parse(content);
-  const newTalker = { id: talkers[talkers.length - 1].id + 1, ...body };
-  const allTalkers = [...talkers, newTalker];
-  await fs.writeFile(caminhoTalker, JSON.stringify(allTalkers, null, 2), 'utf-8');
-  return response.status(HTTP_CREATED).json(newTalker);
+  const talkerEdit = { id: Number(id), ...body };
+  const talkers = JSON.parse(content).map((e) => {
+ if (e.id !== Number(id)) return e; 
+return talkerEdit; 
+});
+   await fs.writeFile(caminhoTalker, JSON.stringify(talkers, null, 2), 'utf-8');
+   return response.status(HTTP_OK_STATUS).json(talkerEdit);
+});
+
+// Requisito 6
+app.delete('/talker/:id', validateToken, async (request, response) => {
+  const { id } = request.params;
+  const content = await fs.readFile(caminhoTalker, 'utf-8');
+  const talkers = JSON.parse(content).filter((e) => e.id !== Number(id));
+  await fs.writeFile(caminhoTalker, JSON.stringify(talkers, null, 2), 'utf-8');
+  return response.status(HTTP_OK_STATUS)
+  .json({ message: 'Pessoa palestrante deletada com sucesso' });
 });
 
 app.listen(PORT, () => {
