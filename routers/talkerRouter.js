7c50b2a8f009e02014a797893a45b5c664453321
Junior Portela <portelajr14@gmail.com>
2021-09-22 02:04:07 -0300

    req1: criar endpoint /talker retornando leitura arquivo

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -7,7 +7,7 @@ const fs = require('fs').promises;
 const STATUS_OK = 200;
 
 const getAll = async () => {
-  const allTalkers = await fs.readFile('../talker.json', 'utf-8');
+  const allTalkers = await fs.readFile('./talker.json', 'utf-8');
   if (allTalkers && allTalkers.length === 0) return JSON.parse([]);
 
   return JSON.parse(allTalkers);
@@ -17,5 +17,7 @@ router.get('/', async (_req, res) => {
   const talkers = await getAll();
   res.status(STATUS_OK).json(talkers);
 });
+
+router.get('/:id', async (req, res) => {});
 
 module.exports = router;
